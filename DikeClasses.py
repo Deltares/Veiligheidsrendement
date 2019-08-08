* minor changes for further implementation of Pathlib * fixed major bug in computation of volumes for soil reinforcement * many new functions for optimization & computing Pareto optimal solutions

DikeClasses.py
@@ -41,12 +41,14 @@ class DikeTraject:
             self.GeneralInfo['omegaStabilityInner'] = 0.04; self.GeneralInfo['aStabilityInner'] = 0.033; self.GeneralInfo['bStabilityInner'] = 50
             self.GeneralInfo['omegaOverflow'] = 0.24
 
-    def ReadAllTrajectInput(self, path, directory, traject, T, startyear, mechanisms=['Overflow', 'StabilityInner', 'Piping']):
+    def ReadAllTrajectInput(self, path, directory, T, startyear,traject='16-4', mechanisms=['Overflow',
+                                                                                           'StabilityInner',
+                                                                                     'Piping']):
         #Make a case directory and inside a figures and results directory if it doesnt exist yet
         if not path.joinpath(directory).is_dir():
-            directory.mkdir(parents=True, exist_ok=True)
-            directory.joinpath('figures').mkdir(parents=True, exist_ok=True)
-            directory.joinpath('results', 'investment_steps').mkdir(parents=True, exist_ok=True)
+            path.joinpath(directory).mkdir(parents=True, exist_ok=True)
+            path.joinpath(directory).joinpath('figures').mkdir(parents=True, exist_ok=True)
+            path.joinpath(directory).joinpath('results', 'investment_steps').mkdir(parents=True, exist_ok=True)
 
         # Routine to read the input for all sections based on the default input format.
         files = [i for i in path.glob("*DV*") if i.is_file()]
@@ -67,12 +69,13 @@ class DikeTraject:
                 self.Sections[i].Reliability.Mechanisms[j] = MechanismReliabilityCollection(j, self.Sections[i].MechanismData[j][1], years=T)
 
                 for k in self.Sections[i].Reliability.Mechanisms[j].Reliability.keys():
-                    self.Sections[i].Reliability.Mechanisms[j].Reliability[k].Input.fill_mechanism(path.joinpath(self.Sections[i].MechanismData[j][0]), calctype=self.Sections[i].MechanismData[j][1], mechanism=j)
+                    self.Sections[i].Reliability.Mechanisms[j].Reliability[k].Input.fill_mechanism(path.joinpath(
+                        self.Sections[i].MechanismData[j][0]), calctype=self.Sections[i].MechanismData[j][1], mechanism=j)
 
             #Make in the figures directory a Initial and Measures direcotry if they don't exist yet
-            if not path.joinpath(directory.joinpath('figures', self.Sections[i].name)).is_dir():
-                directory.joinpath('figures', self.Sections[i].name, 'Initial').mkdir(parents=True, exist_ok=True)
-                directory.joinpath('figures', self.Sections[i].name, 'Measures').mkdir(parents=True, exist_ok=True)
+            if not path.joinpath(directory).joinpath('figures', self.Sections[i].name).is_dir():
+                path.joinpath(directory).joinpath('figures', self.Sections[i].name, 'Initial').mkdir(parents=True, exist_ok=True)
+                path.joinpath(directory).joinpath('figures', self.Sections[i].name, 'Measures').mkdir(parents=True, exist_ok=True)
 
         self.GeneralInfo['Mechanisms'] = mechanisms
 
@@ -238,9 +241,9 @@ class DikeTraject:
                 # plt.xlim([np.min(cumlength), np.max(cumlength)])
                 if PATH != None:
                     if type == 'Assessment':
-                        plt.savefig(PATH.joinpath('figures', beta_or_prob + '_' + str(startyear + ii) + '_Assessment.png'), dpi=300, bbox_inches='tight', format='png')
+                        plt.savefig(PATH.joinpath(beta_or_prob + '_' + str(startyear + ii) + '_Assessment.png'), dpi=300, bbox_inches='tight', format='png')
                     else:
-                        plt.savefig(PATH.joinpath('figures', str(startyear + ii) + '_Step=' + str(type) + '_' + beta_or_prob + '.png'), dpi=300, bbox_inches='tight', format='png')
+                        plt.savefig(PATH.joinpath(str(startyear + ii) + '_Step=' + str(type) + '_' + beta_or_prob + '.png'), dpi=300, bbox_inches='tight', format='png')
                     plt.close()
                 else:
                     plt.show()
@@ -271,7 +274,7 @@ class DikeTraject:
                      'k--', label='Requirement')
             plt.legend()
             plt.title(i.name)
-            plt.savefig(directory + '\\' + i.name + '.png', bbox_inches='tight')
+            plt.savefig(directory.joinpath(i.name + '.png'), bbox_inches='tight')
             plt.close()
 
     def updateProbabilities(self,Probabilities,ChangedSection):
@@ -405,9 +408,25 @@ class MechanismReliabilityCollection:
             else:
                 self.Reliability[str(i)] = MechanismReliability(mechanism, type)
 
-    def generateLCRProfile(self, load, mechanism='Overflow', method='FORM', trajectinfo=None, conditionality = 'no'):
+    def generateInputfromDistributions(self, distributions, parameters = ['R', 'dR', 'S'], processes = ['dR']):
+        processIDs = []
+        for process in processes:
+            processIDs.append(parameters.index(process))
+
+        for i in self.Reliability:
+            self.Reliability[i].Input.fill_distributions(distributions,np.int32(i),processIDs,parameters)
+
+            pass
+
+    def generateLCRProfile(self, load=False, mechanism='Overflow', method='FORM', trajectinfo=None,
+                           conditionality = 'no'):
         # this function generates life-cycle reliability based on the years that have been calculated (so reliability in time)
-        [self.Reliability[i].calcReliability(self.Reliability[i].Input, load, mechanism=mechanism, method=method, year=float(i), TrajectInfo=trajectinfo) for i in self.Reliability.keys()]
+        if load:
+            [self.Reliability[i].calcReliability(self.Reliability[i].Input, load, mechanism=mechanism, method=method, year=float(i), TrajectInfo=trajectinfo) for i in self.Reliability.keys()]
+        else:
+            [self.Reliability[i].calcReliability(mechanism=mechanism, method=method,
+                                                 year=float(i), TrajectInfo=trajectinfo) for i in
+             self.Reliability.keys()]
         #NB: This should be extended with conditional failure probabilities
 
     def constructFragilityCurves(self,input,start = 5, step = 0.2):
@@ -483,6 +502,21 @@ class MechanismReliabilityCollection:
         if yscale == 'log':
             plt.yscale('log')
 
+    def drawAlphaBar(self,step = 5):
+        import matplotlib.ticker as ticker
+
+        alphas = np.array([])
+        firstKey = list(self.Reliability.keys())[0]
+        alphaDim = len(self.Reliability[firstKey].alpha_sq)
+        alphas = np.concatenate([self.Reliability[i].alpha_sq for i in self.Reliability.keys()])
+        alphas = np.reshape(alphas, (np.int(np.size(alphas) / alphaDim), alphaDim))
+        variableNames = list(self.Reliability[firstKey].Input.input.getDescription())
+        alphas = pd.DataFrame(alphas,columns = variableNames)
+        ax = alphas.plot.bar(stacked=True)
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(step))
+        plt.ylim([0,1])
+        plt.title(r'Influence coefficients $\alpha$ in time')
+        plt.show()
 class MechanismReliability:
     #This class contains evaluations of the reliability for a mechanism in a given year.
     def __init__(self, mechanism, type, copy_or_calculate='calculate'):
@@ -584,7 +618,7 @@ class MechanismReliability:
 
         self.type = 'FragilityCurve'
 
-    def calcReliability(self, strength, load, mechanism=None, method='FORM', year=0, TrajectInfo=None):
+    def calcReliability(self, strength = False, load = False, mechanism=None, method='FORM', year=0, TrajectInfo=None):
         #This routine calculates cross-sectional reliability indices based on different types of calculations.
         if self.type == 'Simple':
             if mechanism == 'StabilityInner':
@@ -643,53 +677,27 @@ class MechanismReliability:
             self.alpha_sq = alfas_sq
         elif self.type == 'Prob':
             #Probabilistic evaluation of a mechanism.
-            # make list of all random variables:
-            marginals, names = MarginalsforTimeDepReliability(strength, load=load, year=year, type='Probabilistic')
-            marginals = []
-            names = []
-
-            for i in strength.input.keys():
-                if i in strength.temporals:
-                    original = copy.deepcopy(strength.input[i])
-                    str_change = TemporalProcess(original, year)
-                    marginals.append(str_change)
-                else:
-                    marginals.append(strength.input[i])
-                names.append(i)
-            marginals.append(load.distribution)
-
-            marginals_orig = copy.deepcopy(marginals)
-            if hasattr(load, 'dist_change'):
-                original = copy.deepcopy(load.dist_change)
-                dist_change = TemporalProcess(original, year)
-                marginals = marginals_orig
-                marginals.append(dist_change)
-                dist = ot.ComposedDistribution(marginals)
-                descr = list(strength.input.keys())
-                descr.extend(('h', 'dh'))
-            else:
-                marginals = marginals_orig
-                dist = ot.ComposedDistribution(marginals)
-                descr = list(strength.input.keys())
-                descr.append('h')
-            dist.setDescription(descr)
             if mechanism == 'Piping':
                 zFunc = Mechanisms.zPipingTotal
             elif mechanism == 'Overflow':
                 zFunc = Mechanisms.zOverflow
+            elif mechanism == 'simpleLSF':
+                zFunc = Mechanisms.simpleLSF
             else:
                 raise ValueError('Unknown Z-function')
 
-            if hasattr(strength,'char_vals'):
+            if hasattr(self.Input,'char_vals'):
                 start_vals = []
-
                 for i in descr:
                     if i != 'h' and i != 'dh':
                         start_vals.append(strength.char_vals[i]) if i not in strength.temporals else start_vals.append(strength.char_vals[i] * year)
-
                 start_vals = addLoadCharVals(start_vals, load)
+            else:
+                start_vals = self.Input.input.getMean()
 
-            result, P, beta, alpha_sq = run_prob_calc(ot.PythonFunction(len(marginals), 1, zFunc), dist, method, startpoint=start_vals)
+            result, P, beta, alpha_sq = run_prob_calc(ot.PythonFunction(self.Input.input.getDimension(), 1, zFunc),
+                                                      self.Input.input,
+                                                      method, startpoint=start_vals)
             self.result = result
             self.Pf = P
             self.beta = beta
@@ -754,6 +762,18 @@ class MechanismInput:
     #Class for input of a mechanism
     def __init__(self,mechanism):
         self.mechanism = mechanism
+    def fill_distributions(self,distributions,t,processIDs,parameters):
+
+        dists = copy.deepcopy(distributions)
+        self.temporals = []
+        for j in processIDs:
+            if t > 0:
+                dists[j] = TemporalProcess(dists[j],t)
+            else:
+                dists[j] = ot.Dirac(0.0)
+            self.temporals.append(parameters[j])
+        self.input = ot.ComposedDistribution(dists)
+        self.input.setDescription(parameters)
 
     #This routine reads  input from an input sheet
     def fill_mechanism(self, input, type= 'csv', sheet=None, calctype = 'Prob',mechanism=None):

HelperFunctions.py
@@ -147,13 +147,13 @@ def createDir(directory):
 def DataAtShelve(dir, name, objects = None, mode = 'write'):
     if mode == 'write':
         #make shelf
-        my_shelf = shelve.open(dir + '\\' + name, 'n')
+        my_shelf = shelve.open(str(dir.joinpath(name)), 'n')
         for i in objects.keys():
             my_shelf[i] = objects[i]
         my_shelf.close()
     elif mode == 'read':
         # open shelf
-        my_shelf = shelve.open(dir + '\\' + name)
+        my_shelf = shelve.open(str(dir.joinpath(name)))
         keys = []
         for key in my_shelf:
             locals()[key]=my_shelf[key]
@@ -161,3 +161,40 @@ def DataAtShelve(dir, name, objects = None, mode = 'write'):
         my_shelf.close()
         if len(keys) == 1:
             return locals()[keys[0]]
+
+def IDtoName(ID, MeasureTable):
+    return MeasureTable.loc[MeasureTable['ID']==ID]['Name'].values[0]
+
+def flatten(l): #flatten a list
+  out = []
+  if len(l)>0:
+      for item in l:
+        if isinstance(item, (list, tuple)):
+          out.extend(flatten(item))
+        else:
+          out.append(item)
+  return out
+
+def pareto_frontier(Xs, Ys, maxX = True, maxY = True):
+    myList = sorted([[Xs[i], Ys[i]] for i in range(len(Xs))], reverse=maxX)
+    index_order = np.argsort(Xs)
+    p_front = [myList[0]]
+    index = [index_order[0]]
+    count = 1
+    for pair in myList[1:]:
+        if maxY:
+            if pair[1] >= p_front[-1][1]:
+                p_front.append(pair)
+                index.append(index_order[count])
+        else:
+            if pair[1] < p_front[-1][1]:
+                p_front.append(pair)
+                index.append(index_order[count])
+            elif pair[1] == p_front[-1][1]:
+                if pair[0] < p_front[-1][0]:
+                    p_front.append(pair)
+                    index.append(index_order[count])
+        count +=1
+    p_frontX = [pair[0] for pair in p_front]
+    p_frontY = [pair[1] for pair in p_front]
+    return p_frontX, p_frontY, index

ProbabilisticFunctions.py
@@ -173,7 +173,7 @@ class TableDist(ot.PythonDistribution):
             X.append(self.getRealization())
         return X
 
-def run_prob_calc(model,dist,method='FORM',startpoint=None):
+def run_prob_calc(model,dist,method='FORM',startpoint=False):
     vect = ot.RandomVector(dist)
     if method == 'MCS':
         model = ot.MemoizeFunction(model)
@@ -185,7 +185,7 @@ def run_prob_calc(model,dist,method='FORM',startpoint=None):
         solver.setMaximumAbsoluteError(1e-2)
         solver.setMaximumRelativeError(1e-2)
         solver.setMaximumIterationNumber(200)
-        if startpoint != None:
+        if startpoint:
             algo = ot.FORM(solver, event, startpoint)
         else:
             algo = ot.FORM(solver, event, dist.getMean())
@@ -300,12 +300,13 @@ def TemporalProcess(input, t,makePlot='off'):
         params = input.getParameter()
         mu = params[0] / params[1]
         var = params[0] / (params[1] ** 2)
-
-        input.setParameter(ot.GammaMuSigma()([mu*float(t), np.sqrt(var*float(t)),0]))
+        input.setParameter(ot.GammaMuSigma()([mu*float(t), np.sqrt(var)*float(t),0]))
         if makePlot=='on':
             gr = input.drawPDF()
             from openturns.viewer import View
-            View(gr)
+            view = View(gr)
+            view.show()
+
     elif input.getClassName() == 'Dirac':
         input.setParameter(input.getParameter()*t)
     return input

TestOptimization_main.py
@@ -1,8 +1,10 @@
 #IMPORT PACKAGES
 from DikeClasses import DikeTraject
-from HelperFunctions import DataAtShelve,getMeasureTable, replaceNames
+from HelperFunctions import DataAtShelve,getMeasureTable, replaceNames, pareto_frontier
 from StrategyEvaluation import Solutions, Strategy, SolveMIP
-
+from pathlib import Path
+import copy
+import pandas
 #GENERAL OPTIONS
 SHELVES = True
 
@@ -12,15 +14,16 @@ RUN_STEP_2 = False
 RUN_STEP_3 = True
 
 #INDICATE WHICH METHODS TO EVALUATE
-RUN_HEURISTIC              = False
-RUN_MULTIINTEGER           = False
+RUN_HEURISTIC              = True
+RUN_MIXEDINTEGER           = True
 RUN_PARETOFRONTIER         = True
-
+READ_MIXEDINTEGER          = True
 #INDICATE THE CASE TO USE
-CASE = 'Basic'
+CASE = 'Basic_NoDiaphragm'
 
 #STEP 0: READ GENERAL INPUT
-PATH = r'd:\wouterjanklerk\My Documents\00_PhDgeneral\03_Cases\01_Rivierenland SAFE\WJKlerk\SAFE\data\PaperOptimization\Basic_v2'
+PATH = Path(r'd:\wouterjanklerk\My Documents\00_PhDgeneral\03_Cases\01_Rivierenland ' \
+         'SAFE\WJKlerk\SAFE\data\PaperOptimization\Basic')
 LANGUAGE = 'EN'
 MECHANISMS = ['Overflow', 'StabilityInner', 'Piping']
 T = [0, 19, 20, 50, 75, 100]
@@ -32,10 +35,10 @@ TrajectObject.ReadAllTrajectInput(PATH,CASE,T,STARTYEAR)
 if RUN_STEP_1:
     TrajectObject.runFullAssessment()
 
-    assessment_directory = PATH + '\\AssessmentFigures\\Initial'
+    assessment_directory = PATH.joinpath('AssessmentFigures').joinpath('Initial')
     TrajectObject.plotAssessmentResults(directory=assessment_directory)
 
-    assessment_directory = PATH + '\\AssessmentFigures'
+    assessment_directory = PATH.joinpath('AssessmentFigures')
     TrajectObject.plotReliabilityofDikeTraject(PATH=assessment_directory)
     print('Step 1: assessment finished')
     #store results:
@@ -45,13 +48,15 @@ else:
 
 #STEP 2: EVALUATE ALL MEASURES
 if RUN_STEP_2:
-    solutions_directory = PATH + '\\Solutions'
+    solutions_directory = PATH.joinpath('Solutions')
     SolutionsCollection = {}
     for i in TrajectObject.Sections:
         SolutionsCollection[i.name] = Solutions(i)
-        SolutionsCollection[i.name].fillSolutions(PATH + '\\' + i.name + '.xlsx')
-        SolutionsCollection[i.name].evaluateSolutions(i, TrajectObject.GeneralInfo, geometry_plot=False, plot_dir=solutions_directory + '\\' +i.name, preserve_slope = True)
-        SolutionsCollection[i.name].SolutionstoDataFrame(filtering='off')
+        SolutionsCollection[i.name].fillSolutions(PATH.joinpath(i.name + '.xlsx'))
+        SolutionsCollection[i.name].evaluateSolutions(i, TrajectObject.GeneralInfo, geometry_plot=False,
+                                                      plot_dir=solutions_directory.joinpath(i.name), preserve_slope =
+                                                      True)
+        SolutionsCollection[i.name].SolutionstoDataFrame(filtering='off',splitparams=True)
     print('Step 2: evaluation of measures finished')
     #store results:
     if SHELVES: DataAtShelve(dir = PATH, name = 'Step2Result.out', objects = {'SolutionsCollection': SolutionsCollection}, mode = 'write')
@@ -63,46 +68,142 @@ else:
 #STEP 3: FIND SOLUTIONS FOR ALL SELECTED METHODS
 if RUN_HEURISTIC:
     StrategyHeuristic = Strategy('Heuristic')
-    StrategyHeuristic.combine(TrajectObject, SolutionsCollection)
-    StrategyHeuristic.evaluate(TrajectObject, SolutionsCollection)
+    StrategyHeuristic.combine(TrajectObject, SolutionsCollection,splitparams=True)
+    StrategyHeuristic.evaluate(TrajectObject, SolutionsCollection,splitparams=True)
 
     StrategyHeuristic = replaceNames(StrategyHeuristic,SolutionsCollection)
-    StrategyHeuristic.plotBetaCosts(TrajectObject,path= PATH + '\\Solutions',typ='multi',
+    StrategyHeuristic.plotBetaCosts(TrajectObject,path= PATH.joinpath('Solutions'),typ='multi',
                                      fig_id=101, symbolmode='on',linecolor='b', labels='Heuristic',
                                      MeasureTable=getMeasureTable(SolutionsCollection),beta_or_prob='beta',outputcsv=True,last='yes')
     # write to csv's
     for i in StrategyHeuristic.options: StrategyHeuristic.options[i].to_csv(
-        PATH + '\\Solutions\\' + i + '_Options.csv')
-    StrategyHeuristic.TakenMeasures.to_csv(PATH + '\\TakenMeasures_Heuristic.csv')
+        PATH.joinpath('Solutions').joinpath(i + '_Options.csv'))
+    StrategyHeuristic.TakenMeasures.to_csv(PATH.joinpath(CASE).joinpath('TakenMeasures_Heuristic.csv'))
     if SHELVES: DataAtShelve(dir = PATH, name = 'HeuristicResult.out', objects = {'StrategyHeuristic': StrategyHeuristic}, mode = 'write')
-if RUN_MULTIINTEGER:
-    StrategyMultiInteger = Strategy('MultiInteger')
-    StrategyMultiInteger.combine(TrajectObject,SolutionsCollection)
-    StrategyMultiInteger.make_optimization_input(TrajectObject,SolutionsCollection)
-    MultiIntegerModel = StrategyMultiInteger.create_optimization_model()
-    MultiIntegerSolution = SolveMIP(MultiIntegerModel)
+if RUN_MIXEDINTEGER:
+    StrategyMixedInteger = Strategy('MixedInteger')
+    StrategyMixedInteger.combine(TrajectObject,SolutionsCollection,splitparams=True)
+    StrategyMixedInteger.make_optimization_input(TrajectObject,SolutionsCollection)
+    MixedIntegerModel = StrategyMixedInteger.create_optimization_model()
+    MixedIntegerSolution = SolveMIP(MixedIntegerModel)
 
-    StrategyMultiInteger.readResults(MultiIntegerModel)
-    # StrategyMultiInteger.checkConstraintSatisfaction(MultiIntegerModel)
-    if SHELVES: DataAtShelve(dir = PATH, name = 'MultiIntegerResult.out', objects = {'StrategyMultiInteger': StrategyMultiInteger}, mode = 'write')
+    # StrategyMixedInteger.readResults(MixedIntegerModel)
+    # StrategyMixedInteger.checkConstraintSatisfaction(MixedIntegerModel)
+    if SHELVES:
+        DataAtShelve(dir = PATH, name = 'MixedIntegerStrategy.out', objects = {'StrategyMixedInteger':
+                                                                            StrategyMixedInteger}, mode = 'write')
+        MixedIntegerResults = {}
+        MixedIntegerResults['Values']= MixedIntegerModel.solution.get_values()
+        MixedIntegerResults['Names']= MixedIntegerModel.variables.get_names()
+        MixedIntegerResults['ObjectiveValue'] = MixedIntegerModel.solution.get_objective_value()
+        MixedIntegerResults['Status'] = MixedIntegerModel.solution.get_status_string()
+
+        DataAtShelve(dir = PATH, name = 'MixedIntegerResults.out', objects = {'MixedIntegerResults': MixedIntegerResults},
+                     mode = 'write')
 
     print()
 
 if RUN_PARETOFRONTIER:
     StrategyParetoFrontier = Strategy('ParetoFrontier')
-    StrategyParetoFrontier.combine(TrajectObject,SolutionsCollection)
-    #first step:
-    StrategyParetoFrontier.filter('ParetoPerSection')
-    #filter out as many alternatives as possible based on the cost-risk ratio for each section
-    #second step:
-    #brute force all remaining alternatives
-    #third step:
-    #determine pareto front
+    StrategyParetoFrontier.combine(TrajectObject,SolutionsCollection,splitparams=True)
+
+
+    # StrategyParetoFrontier_unfiltered = copy.deepcopy(StrategyParetoFrontier)
+    # StrategyParetoFrontier_unfiltered.evaluate(TrajectObject,SolutionsCollection)
+
+    #with filtering:
+    StrategyParetoFrontier.filter(TrajectObject,'ParetoPerSection')
+    import cProfile
+    # cProfile.run('StrategyParetoFrontier.evaluate(TrajectObject,SolutionsCollection)')
+    StrategyParetoFrontier.evaluate(TrajectObject,SolutionsCollection)
+
 
     if SHELVES: DataAtShelve(dir = PATH, name = 'ParetoFrontierResult.out', objects = {'StrategyParetoFrontier': StrategyParetoFrontier}, mode = 'write')
     print()
-# if RUN_SIMULATEDANNEALING:
+# if RUN_SIMULATEDANNEALING: or DIJKSTRA SHORTEST PATH/UNIFORM COST SEARCH
 #     pass
 #     print()
 
 ## STEP 4: POSTPROCESSING OF RESULTS
+
+if READ_MIXEDINTEGER:
+    StrategyMixedInteger = DataAtShelve(dir = PATH, name = 'MixedIntegerStrategy.out', mode = 'read')
+    MixedIntegerResults= DataAtShelve(dir = PATH, name = 'MixedIntegerResults.out', mode = 'read')
+    StrategyMixedInteger.readResults(MixedIntegerResults,dir = PATH.joinpath(CASE),MeasureTable=getMeasureTable(
+        SolutionsCollection))
+
+
+print()
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.figure(1000,figsize=(8,8))
+#plot the risk-cost relation:
+if 'StrategyParetoFrontier' in locals():
+    # [LCC_Pareto, TR_Pareto] = [StrategyParetoFrontier_unfiltered.LCC_combis, StrategyParetoFrontier_unfiltered.TotalRisk_combis]
+    # [LCC_ParetoFront,TR_ParetoFront,index_all] = pareto_frontier(LCC_Pareto,TR_Pareto,maxX=False,maxY=False)
+    # plt.plot(LCC_Pareto,TR_Pareto,linestyle = '',marker = 'o',color = 'plum',label='All Runs')
+    # plt.plot(LCC_ParetoFront,TR_ParetoFront,linestyle = '',marker = 's',color = 'indigo',label='Pareto Front')
+
+    [LCC_Pareto, TR_Pareto] = [StrategyParetoFrontier.LCC_combis, StrategyParetoFrontier.TotalRisk_combis]
+    [LCC_ParetoFront,TR_ParetoFront,index] = pareto_frontier(LCC_Pareto,TR_Pareto,maxX=False,maxY=False)
+    plt.plot(LCC_Pareto,TR_Pareto,linestyle = '',marker = 'o',color = 'lightgray',label='All Runs')
+    plt.plot(LCC_ParetoFront,TR_ParetoFront,linestyle = '-',marker = 'd',color = 'dimgray',label='Pareto Front')
+
+if 'StrategyHeuristic' in locals():
+    [TR_Heuristic, LCC_Heuristic] = StrategyHeuristic.determineRiskCostCurve(TrajectObject,PATH.joinpath(
+        CASE).joinpath('PfDumps'))
+    plt.plot(LCC_Heuristic,TR_Heuristic, '-or', label='Heuristic')
+
+if 'StrategyMixedInteger' in locals():
+    [TR_MixedInteger, LCC_MixedInteger] = StrategyMixedInteger.determineRiskCostCurve(TrajectObject,PATH.joinpath(
+        CASE).joinpath('PfDumps'))
+    plt.plot(LCC_MixedInteger,TR_MixedInteger,'ob',label='Mixed Integer')
+
+    #make radius line for optimum:
+    total = TR_MixedInteger+LCC_MixedInteger
+    x = np.arange(0,total,1)
+    y = total - x
+    plt.plot(x,y,linestyle=':',color='blue',label='Radius of MIP optimum')
+plt.ylabel('Total Risk')
+plt.xlabel('LCC')
+plt.yscale('log')
+plt.ylim((1e4,5e7))
+plt.xlim((0,1.2e8))
+# plt.xlim((0,2e7))
+plt.legend(loc=1)
+plt.title('Comparison of risk and cost for different methods')
+plt.savefig(PATH.joinpath(CASE).joinpath('ComparisonOfMethods_logscale.png'),dpi=300, bbox_inches='tight')
+# plt.savefig(PATH.joinpath(CASE).joinpath('ComparisonOfMethods.png'),dpi=300, bbox_inches='tight')
+plt.close()
+#plot with all TC of all draws in bar graph and the final solution of MIP and Heuristic:
+TC_Pareto = np.sort(np.add(LCC_Pareto,TR_Pareto))
+TC_ParetoFront = np.add(LCC_ParetoFront,TR_ParetoFront)
+TC_Heuristic =np.add(LCC_Heuristic,TR_Heuristic)
+TC_MIP = np.add(LCC_MixedInteger,TR_MixedInteger)
+
+plt.figure(1001,figsize=(8,6))
+plt.plot(range(0,len(TC_Pareto)),TC_Pareto,color='lightgray',label='individual runs')
+plt.axhline(y=TC_MIP,xmin=0,xmax=len(TC_Pareto),color='r',linestyle='--',label='Mixed Integer Optimum')
+plt.hlines(y=TC_Heuristic,xmin=0,xmax=len(TC_Pareto),color='g',linestyle='--',label='Greedy heuristic steps')
+plt.axhline(y=TC_Heuristic[-1],xmin=0,xmax=len(TC_Pareto),color='g',linestyle='-',label='Greedy heuristic '
+                                                                                                'Optimum')
+plt.xlim((0,len(TC_Pareto)))
+plt.legend(loc=2)
+plt.title('Comparison of total cost for different methods')
+plt.savefig(PATH.joinpath(CASE).joinpath('Methods.png'),dpi=300, bbox_inches='tight')
+print(TC_Heuristic)
+print(TC_MIP)
+
+
+#pseudocode:
+#if type = heuristic
+#loop over takenmeasures
+    #implemnet option
+    #calculate risk
+    #save (Total Risk, LCC)
+#elif type = MIP
+#implemnet options
+#calculate risk
+#save (total risk, lcc)
+#plot all points