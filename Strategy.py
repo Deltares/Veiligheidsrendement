Turned on the bundling of overflow measures. Fixed an error in the bundling where the list would be length zero if no more measures are available for increasing overflow safety at the weakest section.

Strategy.py
@@ -911,7 +911,7 @@ class GreedyStrategy(Strategy):
             overflow_bundle_index, BC_bundle = OverflowBundling(self,init_overflow_risk,
                                                                np.max(BC),
                                                                 measure_list,LifeCycleCost,traject)
-            BC_bundle=0
+            # BC_bundle=0
             #compute additional measures where we combine overflow measures, here we optimize a package, purely based
             # on overflow, and compute a general BC ratio that is a factor (factor cautious) higher than the max BC.
             #then in the selection of the measure we make a if-elif split with either the normal routine or an
@@ -1341,8 +1341,9 @@ class MixedIntegerStrategy(Strategy):
                         nlist = [1.0]*len(slist)
                         curconstraint = [slist,nlist]
                         C3.append(curconstraint)
+
                         del curconstraint, slist,nlist
-            print(str(sys.getsizeof(C3)) + ' bytes at t=' + str(t) + ' and n = ' + str(n))
+            print(str(sys.getsizeof(C3)) + ' bytes at t=' + str(t) + ' and n = ' + str(n+1))
 
         A = A + C3
         senseV = senseV + "L"*len(C3) # L means <=
@@ -1353,7 +1354,6 @@ class MixedIntegerStrategy(Strategy):
         print('binary constraints implemented in restriction of variables')
 
         # Add constraints to model:
-
         model.linear_constraints.add(lin_expr=A, senses=senseV, rhs=b)
 
         return model

StrategyEvaluation.py
@@ -402,22 +402,29 @@ def OverflowBundling(Strategy, init_overflow_risk, BCref,existing_investment,
         run_number += 1
 
     #take the final index from the list, where BC is max
-    final_index = counter_list[np.argmax(BC_list)]
+    if len(BC_list) > 0:
+        final_index = counter_list[np.argmax(BC_list)]
+        # convert measure_index to sh based on sorted_indices
+        sg_index = np.empty((len(traject.Sections),))
+        measure_index = np.zeros((np.size(LifeCycleCost, axis=0),), dtype=np.int32)
+        for i in range(0, len(measure_index)):
+            if final_index[i] != -1:  # a measure was taken
+                measure_index[i] = sorted_indices[i, final_index[i]]
+                sg_index[i] = sg_indices[i, final_index[i]]
+            else:  # no measure was taken
+                measure_index[i] = existing_investments[i, 0]
+                sg_index[i] = existing_investments[i, 1]
+
+        measure_index = np.append(measure_index, sg_index).reshape((2, len(traject.Sections))).T.astype(np.int32)
+        BC_out = np.max(BC_list)
+    else:
+        BC_out = 0
+        measure_index = []
+        print('Warning: no more measures for weakest overflow section')
 
 
-    #convert measure_index to sh based on sorted_indices
-    sg_index = np.empty((len(traject.Sections),))
-    measure_index = np.zeros((np.size(LifeCycleCost,axis=0),),dtype=np.int32)
-    for i in range(0,len(measure_index)):
-        if final_index[i] != -1: #a measure was taken
-            measure_index[i] = sorted_indices[i,final_index[i]]
-            sg_index[i] = sg_indices[i, final_index[i]]
-        else:   #no measure was taken
-            measure_index[i] = existing_investments[i,0]
-            sg_index[i] = existing_investments[i,1]
 
-    measure_index = np.append(measure_index,sg_index).reshape((2,len(traject.Sections))).T.astype(np.int32)
-    return measure_index, np.max(BC_list)
+    return measure_index, BC_out
 
 
 