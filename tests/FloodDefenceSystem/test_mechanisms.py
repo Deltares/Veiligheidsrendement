d4d40ff3237b5cf6205a67aaf1a11dff0f9145eb
Dennis Tang <dennis.tang@deltares.nl>
2023-02-21 11:15:42 +0100

    chore: Updated testfile:
    - Introduced testclass
    - Renamed file name to reflect the file it is testing

---
+++ tests/FloodDefenceSystem/test_mechanisms.py
@@ -0,0 +1,40 @@
+import pytest
+
+from src.FloodDefenceSystem.Mechanisms import LSF_heave, LSF_sellmeijer
+
+
+class TestMechanisms:
+    def test_LSF_heave():
+        # set tests data
+        r_exit, h, h_exit, d_cover, kwelscherm = (0.9, 6, 4, 2, 0)
+
+        # calculate
+        (g_h, i, i_c) = LSF_heave(r_exit, h, h_exit, d_cover, kwelscherm)
+
+        # assert
+        assert g_h == pytest.approx(-0.6)
+        assert i == pytest.approx(0.9)
+        assert i_c == pytest.approx(0.3)
+
+    def test_LSF_sellmeijer():
+        # set tests data
+        h, h_exit, d_cover, L, D, d70, k, mPiping = (
+            7,
+            4,
+            2,
+            50,
+            30,
+            0.00075,
+            1e-5,
+            1.0,
+        )
+
+        # calculate
+        (g_p, delta_h, delta_h_c) = LSF_sellmeijer(
+            h, h_exit, d_cover, L, D, d70, k, mPiping
+        )
+
+        # assert
+        assert g_p == pytest.approx(12.12568371313684)
+        assert delta_h == pytest.approx(2.4)
+        assert delta_h_c == pytest.approx(14.525683713136841)
