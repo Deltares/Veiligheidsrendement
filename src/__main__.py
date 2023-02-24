a37fa6653071203507abdef9ce7fce7e39cf44c2
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-02-24 20:52:24 +0100

    feat(__main__.py): Added endpoint for calling to the tool from CLI either locally or when installing through pip

---
+++ src/__main__.py
@@ -0,0 +1,37 @@
+import datetime
+import logging
+from pathlib import Path
+
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name="assessment", help="Validation of the model in the given directory.")
+@click.argument("model_directory", type=click.Path(exists=True), nargs=1)
+def run_step_assessment(**kwargs):
+    logging.info("Assess, {0}!".format(kwargs["name"]))
+
+
+@cli.command(
+    name="measures",
+    help="Measurement of all specified mechanisms in the model.",
+)
+@click.argument("model_directory", type=click.Path(exists=True), nargs=1)
+def run_step_measures(**kwargs):
+    logging.info("Measure, {0}!".format(kwargs["name"]))
+
+
+@cli.command(
+    name="optimization", help="Optimization of the model in the given directory."
+)
+@click.argument("model_directory", type=click.Path(exists=True), nargs=1)
+def run_step_optimization(**kwargs):
+    logging.info("Optimize, {0}!".format(kwargs["name"]))
+
+
+if __name__ == "__main__":
+    cli()
