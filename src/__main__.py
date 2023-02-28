1b3ded90d6625b76655265177df24320f5fac9a9
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-02-28 16:06:32 +0100

    chore(__main__.py): Added steps from test_integrated.py to the cli

--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,13 @@ from pathlib import Path
 
 import click
 
+from src.defaults.vrtool_config import VrtoolConfig
+from src.FloodDefenceSystem.DikeTraject import DikeTraject
+from src.run_workflows.measures_workflow.run_measures import RunMeasures
+from src.run_workflows.optimization_workflow.run_optimization import RunOptimization
+from src.run_workflows.safety_workflow.run_safety_assessment import RunSafetyAssessment
+from src.run_workflows.vrtool_plot_mode import VrToolPlotMode
+
 
 @click.group()
 def cli():
@@ -15,6 +22,16 @@ def cli():
 def run_step_assessment(**kwargs):
     logging.info("Assess, {0}!".format(kwargs["model_directory"]))
 
+    # Define VrToolConfig and Selected Traject
+    _vr_config = VrtoolConfig()
+    _selected_traject = DikeTraject.from_vr_config(_vr_config)
+
+    # Step 1. Safety assessment.
+    _safety_assessment = RunSafetyAssessment(plot_mode=VrToolPlotMode.STANDARD)
+    _safety_assessment.selected_traject = _selected_traject
+    _safety_assessment.vr_config = _vr_config
+    _safety_assessment.run()
+
 
 @cli.command(
     name="measures",
@@ -24,6 +41,16 @@ def run_step_assessment(**kwargs):
 def run_step_measures(**kwargs):
     logging.info("Measure, {0}!".format(kwargs["model_directory"]))
 
+    # Define VrToolConfig and Selected Traject
+    _vr_config = VrtoolConfig()
+    _selected_traject = DikeTraject.from_vr_config(_vr_config)
+
+    # Step 2. Measures.
+    _measures = RunMeasures(plot_mode=VrToolPlotMode.STANDARD)
+    _measures.selected_traject = _selected_traject
+    _measures.vr_config = _vr_config
+    _measures.run()
+
 
 @cli.command(
     name="optimization", help="Optimization of the model in the given directory."
@@ -32,6 +59,19 @@ def run_step_measures(**kwargs):
 def run_step_optimization(**kwargs):
     logging.info("Optimize, {0}!".format(kwargs["model_directory"]))
 
+    # Define VrToolConfig and Selected Traject
+    _vr_config = VrtoolConfig()
+    _selected_traject = DikeTraject.from_vr_config(_vr_config)
+
+    # Step 2. Measures.
+    _measures = RunMeasures(plot_mode=VrToolPlotMode.STANDARD)
+    _measures.selected_traject = _selected_traject
+    _measures.vr_config = _vr_config
+    _measures_result = _measures.run()
+    
+    # Step 3. Optimization.
+    _optimization = RunOptimization(_measures_result, plot_mode=_plot_mode)
+    _optimization.run()
 
 if __name__ == "__main__":
     cli()
