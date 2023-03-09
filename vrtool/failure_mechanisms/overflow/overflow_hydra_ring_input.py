7c047882838e79d23b2f6db3bf1c9986690e722d
Dennis Tang <dennis.tang@deltares.nl>
2023-03-09 14:08:21 +0100

    feature: added HydraRing input

---
+++ vrtool/failure_mechanisms/overflow/overflow_hydra_ring_input.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+from dataclasses import dataclass
+
+from vrtool.flood_defence_system.mechanism_input import MechanismInput
+
+
+@dataclass
+class OverflowHydraRingInput:
+    h_crest: float
+    d_crest: float
+    hc_beta: pd.DataFrame
+
+    @classmethod
+    def from_mechanism_input(
+        cls, mechanism_input: MechanismInput
+    ) -> OverflowHydraRingInput:
+        return cls(
+            h_crest=mechanism_input["h_crest"],
+            d_crest=mechanism_input["d_crest"],
+            hc_beta=mechanism_input["hc_beta"],
+        )
