c9cee36abcbec8c1b538d8c703692ab0de0de9ed
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-03-13 09:28:33 +0100

    test(test_common_functions.py): Removed unnecessary test case collector

--- tests/decision_making/measures/test_common_functions.py
+++ tests/decision_making/measures/test_common_functions.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import pandas as pd
 import pytest
 
@@ -5,40 +7,36 @@ from tests import test_data
 from vrtool.decision_making.measures.common_functions import determine_new_geometry
 
 
-def pytest_generate_tests(metafunc):
-    # called once per each test function
-    funcarglist = metafunc.cls.params[metafunc.function.__name__]
-    argnames = sorted(funcarglist[0])
-    metafunc.parametrize(
-        argnames, [[funcargs[name] for name in argnames] for funcargs in funcarglist]
-    )
-
-
 class TestCommonFunctions:
-    params = {
-        "test_new_geom": [
-            dict(dir="inward", dx=0.5, dy=10, ans1=37.8, ans2=13.06),
-            dict(dir="outward", dx=0, dy=20, ans1=91.23, ans2=0.0),
-        ],
-    }
 
-    def test_new_geom(self, dir, dx, dy, ans1, ans2):
-        geometry_change = (dx, dy)
-        max_berm_out = 20.0
-        test_sheet_new = test_data.joinpath("integrated_SAFE_16-3_small", "DV53.xlsx")
-        initial_new = pd.read_excel(
-            test_sheet_new,
+    @pytest.mark.parametrize("direction, dx, dy, expected_values",  
+        [
+            pytest.param("inward", 0.5, 10, (37.8, 13.06)),
+            pytest.param("outward", 0, 20, (91.23, 0.0)),
+        ])
+    def test_new_geom(self, direction: str, dx: float, dy: float, expected_values: tuple[float, float]):
+        # 1. Define test data.        
+        _traject_test_file = test_data.joinpath("integrated_SAFE_16-3_small", "DV53.xlsx")
+        assert _traject_test_file.exists(), "No test file found at {}".format(_traject_test_file)
+
+        _traject_test_data = pd.read_excel(
+            _traject_test_file,
             sheet_name="Geometry",
             index_col=0,
-        )  # New format with BIT BUT etc.
+        )
+
+        # 2. Run test.
         new_geometry_reinforced = determine_new_geometry(
-            geometry_change,
-            direction=dir,
-            max_berm_out=max_berm_out,
-            initial=initial_new,
+            (dx, dy),
+            direction=direction,
+            max_berm_out= 0.0,
+            initial=_traject_test_data,
             berm_height=2,
             geometry_plot=False,
         )
-        tol = 0.01
-        assert new_geometry_reinforced[1] == pytest.approx(ans1, tol)
-        assert new_geometry_reinforced[3] == pytest.approx(ans2, tol)
+
+        # 3. Verify expectations.
+        _tolerance = 0.01
+        _left_result, _right_result = expected_values
+        assert new_geometry_reinforced[1] == pytest.approx(_left_result, _tolerance)
+        assert new_geometry_reinforced[3] == pytest.approx(_right_result, _tolerance)
