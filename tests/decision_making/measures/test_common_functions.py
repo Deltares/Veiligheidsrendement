38058a377a6cc02a1ece726e80dbc17f089c1a82
Edwin Spee <edwin.spee@deltares.nl>
2023-04-20 13:27:09 +0200

    test: add no berm test to test_new_geom
    also reduce tolerance as it is relative

--- tests/decision_making/measures/test_common_functions.py
+++ tests/decision_making/measures/test_common_functions.py
@@ -9,18 +9,30 @@ from vrtool.decision_making.measures.common_functions import determine_new_geome
 
 class TestCommonFunctions:
     @pytest.mark.parametrize(
-        "direction, dx, dy, expected_values",
+        "sheetFileName, direction, dx, dy, expected_values",
         [
-            pytest.param("inward", 0.5, 10, (37.8, 13.06), id="Inward case"),
-            pytest.param("outward", 0, 20, (91.23, 0.0), id="Outward case"),
+            pytest.param(
+                "DV56.xlsx", "outward", 0, 20, (91.48, 0.0), id="No Berm case"
+            ),
+            pytest.param(
+                "DV53.xlsx", "inward", 0.5, 10, (37.8, 13.06), id="Inward case"
+            ),
+            pytest.param(
+                "DV53.xlsx", "outward", 0, 20, (91.23, 0.0), id="Outward case"
+            ),
         ],
     )
     def test_new_geom(
-        self, direction: str, dx: float, dy: float, expected_values: tuple[float, float]
+        self,
+        sheetFileName: str,
+        direction: str,
+        dx: float,
+        dy: float,
+        expected_values: tuple[float, float],
     ):
         # 1. Define test data.
         _traject_test_file = test_data.joinpath(
-            "integrated_SAFE_16-3_small", "DV53.xlsx"
+            "integrated_SAFE_16-3_small", sheetFileName
         )
         assert _traject_test_file.exists(), "No test file found at {}".format(
             _traject_test_file
@@ -43,7 +55,7 @@ class TestCommonFunctions:
         )
 
         # 3. Verify expectations.
-        _tolerance = 0.01
+        _tolerance = 0.001
         _reinforcement_1, _reinforcement_3 = expected_values
         assert _reinforced_geometry[1] == pytest.approx(_reinforcement_1, _tolerance)
         assert _reinforced_geometry[3] == pytest.approx(_reinforcement_3, _tolerance)
