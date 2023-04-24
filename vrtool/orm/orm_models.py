985131e47a0d1e983e60ff176cf7908b6d68259a
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-04-24 16:02:09 +0200

    chore(orm_models): Added relation DikeTraject 1 to many SectionData

--- vrtool/orm/orm_models.py
+++ vrtool/orm/orm_models.py
@@ -20,7 +20,26 @@ class BaseModel(Model):
     class Meta:
         database = vrtool_db
 
+
+class DikeTrajectInfo(BaseModel):
+    traject_name = CharField(max_length=_max_char_length)
+    omega_piping = FloatField(default=0.24)
+    omega_stability_inner = FloatField(0.04)
+    omega_overflow = FloatField(0.24)
+    a_piping = FloatField(default=float("nan"))
+    b_piping = FloatField(default=300)
+    a_stability_inner = FloatField(default = 0.033)
+    b_stability_inner = FloatField(default = 50)
+    beta_max = FloatField(default=float("nan"))
+    p_max = FloatField(default=float("nan"))
+    flood_damage = FloatField(default=float("nan"))
+    traject_length = FloatField(default=float("nan"))
+
+    class Meta:
+        table_name = _get_table_name(__qualname__)
+
 class SectionData(BaseModel):
+    dike_traject = ForeignKeyField(DikeTrajectInfo, backref="dike_sections")
     section_name = CharField(unique=True, max_length=_max_char_length)
     dijkpaal_start = CharField(null=True, max_length=_max_char_length)
     dijkpaal_end = CharField(null=True, max_length=_max_char_length)
@@ -185,23 +204,6 @@ class CustomMeasure(Measure):
     cost = FloatField(null=False)
     beta = FloatField(null=False)
     year = IntegerField(null=False)
-
-    class Meta:
-        table_name = _get_table_name(__qualname__)
-
-class DikeTrajectInfo(BaseModel):
-    traject_name = CharField(max_length=_max_char_length)
-    omega_piping = FloatField(default=0.24)
-    omega_stability_inner = FloatField(0.04)
-    omega_overflow = FloatField(0.24)
-    a_piping = FloatField(default=float("nan"))
-    b_piping = FloatField(default=300)
-    a_stability_inner = FloatField(default = 0.033)
-    b_stability_inner = FloatField(default = 50)
-    beta_max = FloatField(default=float("nan"))
-    p_max = FloatField(default=float("nan"))
-    flood_damage = FloatField(default=float("nan"))
-    traject_length = FloatField(default=float("nan"))
 
     class Meta:
         table_name = _get_table_name(__qualname__)
