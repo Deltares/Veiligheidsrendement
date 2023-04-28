3c64c407e05b933c24face14c5eb47da74c08fbc
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-04-28 16:22:25 +0200

    chore: Added importers for DikeTrajectInfo, DikeTraject and DikeSection

---
+++ tests/orm/io/importers/test_dike_traject_importer.py
@@ -0,0 +1,9 @@
+from vrtool.orm.io.importers.dike_traject_importer import DikeTrajectImporter
+from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
+
+class TestDikeTrajectInfoImporter:
+
+    def test_initialize(self):
+        _importer = DikeTrajectImporter()
+        assert isinstance(_importer, DikeTrajectImporter)
+        assert isinstance(_importer, OrmImporterProtocol)
---
+++ vrtool/orm/io/importers/dike_section_importer.py
@@ -0,0 +1,20 @@
+from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
+from vrtool.orm.models.section_data import SectionData
+from vrtool.orm.models.buildings import Buildings
+from vrtool.flood_defence_system.dike_section import DikeSection
+import pandas as pd
+
+class DikeSectionImporter(OrmImporterProtocol):
+
+    def _import_buildings_list(self, buildings_list: list[Buildings]) -> pd.DataFrame:
+        _buildings_data = [[_building.distance_from_toe, _building.number_of_buildings] for _building in buildings_list]
+        return pd.DataFrame(_buildings_data, columns=['distancefromtoe', 'cumulative'])
+
+    def import_orm(self, orm_model: SectionData) -> DikeSection:
+        _dike_section = DikeSection()
+        _dike_section.name = orm_model.section_name
+        _dike_section.houses = self._import_buildings_list(orm_model.buildings_list)
+        _dike_section.mechanism_data = {}
+        for _mechanism_per_section in orm_model.mechanisms_per_section:
+            _dike_section.mechanism_data[_mechanism_per_section.mechanism.name] = ()
+        return _dike_section
