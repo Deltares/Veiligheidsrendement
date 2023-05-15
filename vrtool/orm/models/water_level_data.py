ff96edf3ea9c8f1ed14b3944fe1980109955bbd7
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-15 11:55:54 +0200

    chore: Changed property to nullable

--- vrtool/orm/models/water_level_data.py
+++ vrtool/orm/models/water_level_data.py
@@ -6,7 +6,7 @@ from vrtool.orm.models.section_data import SectionData
 
 class WaterlevelData(OrmBaseModel):
     section_data = ForeignKeyField(SectionData, backref="water_level_data_list")
-    water_level_location_id = IntegerField(null=False)
+    water_level_location_id = IntegerField(null=True)
     year = IntegerField(null=False)
     water_level = FloatField(null=False)
     beta = FloatField(null=False)
