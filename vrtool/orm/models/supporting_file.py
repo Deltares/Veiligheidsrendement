220f5bf270a97ac9285e09205a4840cdc2161b00
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-15 20:35:38 +0200

    feat(supporting_file.py): Added new table and its own backreference

---
+++ vrtool/orm/models/supporting_file.py
@@ -0,0 +1,16 @@
+from vrtool.orm.models.orm_base_model import OrmBaseModel
+from peewee import CharField, ForeignKeyField
+from vrtool.orm.models.computation_scenario import ComputationScenario
+
+from vrtool.orm.models.orm_base_model import (
+    OrmBaseModel,
+    _get_table_name,
+    _max_char_length,
+)
+
+class SupportingFile(OrmBaseModel):
+    computation_scenario = ForeignKeyField(ComputationScenario, backref="supporting_files")
+    filename = CharField(max_length=_max_char_length)
+
+    class Meta:
+        table_name = _get_table_name(__qualname__)
