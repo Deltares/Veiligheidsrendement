58bf00717201a1279887fead5734a5e9b46275f6
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-16 13:07:08 +0200

    chore: Added float converter to prevent None values being set

--- vrtool/orm/io/importers/dike_traject_info_importer.py
+++ vrtool/orm/io/importers/dike_traject_info_importer.py
@@ -2,6 +2,7 @@ from vrtool.common.dike_traject_info import DikeTrajectInfo
 from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
 from vrtool.orm.models.dike_traject_info import DikeTrajectInfo as OrmDikeTrajectInfo
 
+from vrtool.orm.io.importers.converters import to_valid_float
 
 class DikeTrajectInfoImporter(OrmImporterProtocol):
     def import_orm(self, orm_model: OrmDikeTrajectInfo) -> DikeTrajectInfo:
@@ -13,12 +14,12 @@ class DikeTrajectInfoImporter(OrmImporterProtocol):
             omegaPiping=orm_model.omega_piping,
             omegaStabilityInner=orm_model.omega_stability_inner,
             omegaOverflow=orm_model.omega_overflow,
-            aPiping=orm_model.a_piping,
+            aPiping=to_valid_float(orm_model.a_piping),
             bPiping=orm_model.b_piping,
             aStabilityInner=orm_model.a_stability_inner,
             bStabilityInner=orm_model.b_stability_inner,
-            beta_max=orm_model.beta_max,
-            Pmax=orm_model.p_max,
-            FloodDamage=orm_model.flood_damage,
-            TrajectLength=orm_model.traject_length,
+            beta_max=to_valid_float(orm_model.beta_max),
+            Pmax=to_valid_float(orm_model.p_max),
+            FloodDamage=to_valid_float(orm_model.flood_damage),
+            TrajectLength=to_valid_float(orm_model.traject_length),
         )
