cbaf4c28b879c41dbb36c43292ee2f243c7e4645
MatthiasHauth <matthias.hauth@deltares.nl>
2023-05-17 12:05:48 +0200

    test/add test to check if the stability screen has been correctly added in func 'implement_berm_widening'

--- tests/decision_making/measures/test_common_functions.py
+++ tests/decision_making/measures/test_common_functions.py
@@ -2,9 +2,40 @@ from __future__ import annotations
 
 import pandas as pd
 import pytest
+from geolib import DStabilityModel
 
-from tests import test_data
-from vrtool.decision_making.measures.common_functions import determine_new_geometry
+from tests import test_data, test_results, test_externals
+from vrtool.decision_making.measures.common_functions import determine_new_geometry, implement_berm_widening
+
+_measure_input = _measure_input_test = {
+    "geometry": pd.DataFrame.from_dict(
+        {
+            "x": {
+                "BUT": -17.0,
+                "BUK": 0.0,
+                "BIK": 3.5,
+                "BBL": 25.0,
+                "EBL": 42.0,
+                "BIT": 47.0,
+                "BIT_0": 47.0,
+                "EXT": 3.5,
+            },
+            "z": {
+                "BUT": 4.996,
+                "BUK": 10.51,
+                "BIK": 10.51,
+                "BBL": 6.491,
+                "EBL": 5.694,
+                "BIT": 5.104,
+                "BIT_0": 5.104,
+                "EXT": 4.996,
+            },
+        }
+    ),
+    "dcrest": 0,
+    "dberm": 0,
+    "StabilityScreen": 'yes'
+}
 
 
 class TestCommonFunctions:
@@ -29,12 +60,12 @@ class TestCommonFunctions:
         ],
     )
     def test_new_geom(
-        self,
-        sheetFileName: str,
-        direction: str,
-        dx: float,
-        dy: float,
-        expected_values: tuple[float, float],
+            self,
+            sheetFileName: str,
+            direction: str,
+            dx: float,
+            dy: float,
+            expected_values: tuple[float, float],
     ):
         # 1. Define test data.
         _traject_test_file = test_data.joinpath(
@@ -65,3 +96,39 @@ class TestCommonFunctions:
         _reinforcement_1, _reinforcement_3 = expected_values
         assert _reinforced_geometry[1] == pytest.approx(_reinforcement_1, _tolerance)
         assert _reinforced_geometry[3] == pytest.approx(_reinforcement_3, _tolerance)
+
+    def test_implement_berm_widening_dstability_with_screen(self):
+
+        _berm_input = {
+            'STIXNAAM': test_data / "stix" / "RW001.+096_STBI_maatgevend_Segment_38005_1D1.stix",
+            'DStability_exe_path' : test_externals.joinpath("DStabilityConsole")
+        }
+        _path_intermediate_stix = test_results / "test_intermediate_stix"
+        implement_berm_widening(_berm_input,
+                                _measure_input,
+                                measure_parameters={},
+                                mechanism='StabilityInner',
+                                computation_type='DStability',
+                                path_intermediate_stix=_path_intermediate_stix,
+                                SFincrease=0.2,
+                                depth_screen=6.0)
+
+        _dstability_model = DStabilityModel()
+        _modified_stix_name = "RW001.+096_STBI_maatgevend_Segment_38005_1D1" + f"_dberm_{_measure_input['dberm']}_dcrest_{_measure_input['dcrest']}.stix"
+        _dstability_model.parse(_path_intermediate_stix / _modified_stix_name)
+
+        # Assert that
+        assert (
+            len(_dstability_model.datastructure.reinforcements) == 2
+        )
+        assert (
+            len(
+                _dstability_model.datastructure.reinforcements[
+                    0
+                ].ForbiddenLines
+            )
+            == 1
+        )
+
+
+
