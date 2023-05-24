74f7761dbc00e46515609c8d3847b48a9530fcac
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-24 10:22:03 +0200

    test: Improved test fixtures performance by using transactions and adding an empty database  to the test data

--- tests/orm/models/__init__.py
+++ tests/orm/models/__init__.py
@@ -1,23 +1,16 @@
-import shutil
-
 import pytest
 from peewee import SqliteDatabase
 
-from tests import test_results
-from vrtool.orm.orm_controllers import initialize_database
-
+from tests import test_data
+from vrtool.orm.orm_controllers import open_database
 
 @pytest.fixture(autouse=False)
-def empty_db_fixture(request: pytest.FixtureRequest):
-    _parts = request.node.nodeid.split("::")
-    _db_file = test_results / _parts[-2] / f"{_parts[-1]}_db.db"
-    if _db_file.exists():
-        _db_file.unlink()
-
-    _db = initialize_database(_db_file)
+def empty_db_fixture():
+    _db_file = test_data / "test_db" / f"empty_db.db"
+    _db = open_database(_db_file)
     assert isinstance(_db, SqliteDatabase)
 
-    yield _db
+    with _db.atomic() as transaction:
+        yield _db
+        transaction.rollback()
     _db.close()
-
-    shutil.rmtree(_db_file.parent)
