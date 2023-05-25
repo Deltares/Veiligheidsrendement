58fb98aacfc45951b15ce165c5c2e71801516398
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-25 16:59:06 +0200

    feat: Added missing Table MeasureParameters

--- vrtool/orm/io/importers/solutions_importer.py
+++ vrtool/orm/io/importers/solutions_importer.py
@@ -1,18 +1,21 @@
 from pathlib import Path
+from typing import Type
 import pandas as pd
+from vrtool.decision_making.measures.custom_measure import CustomMeasure
 from vrtool.decision_making.measures.measure_base import MeasureBase
 from vrtool.decision_making.measures import (
-    CustomMeasure,
     DiaphragmWallMeasure,
     SoilReinforcementMeasure,
     StabilityScreenMeasure,
     VerticalGeotextileMeasure,
 )
+from vrtool.decision_making.measures import CustomMeasure
 from vrtool.flood_defence_system.dike_section import DikeSection
 
 from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
 from vrtool.defaults.vrtool_config import VrtoolConfig
 from vrtool.orm.models.measure import Measure
+from vrtool.orm.models.custom_measure import CustomMeasure as OrmCustomMeasure
 from vrtool.orm.models.orm_base_model import OrmBaseModel
 from vrtool.orm.models.standard_measure import StandardMeasure
 
@@ -36,59 +39,63 @@ class MeasureImporter(OrmImporterProtocol):
         self.unit_costs = vrtool_config.unit_costs
 
 
-    def _set_standard_measure_values(self, measure: MeasureBase, orm_measure: StandardMeasure) -> None:
+    def _set_base_values(self, measure: MeasureBase):
         measure.config = self._vrtool_config
-        measure.crest_step = orm_measure.crest_step
         measure.berm_step = self.berm_step
         measure.t_0 = self.t_0
         measure.geometry_plot = self.geometry_plot
         measure.unit_costs = self.unit_costs
         measure.parameters = {}
-        measure.parameters["Direction"] = orm_measure.direction
-        measure.parameters["StabilityScreen"] = "yes" if orm_measure.stability_screen else "no"
-        measure.parameters["dcrest_min"] = None
-        measure.parameters["dcrest_max"] = orm_measure.max_crest_increase
-        measure.parameters["max_outward"] = orm_measure.max_outward_reinforcement
-        measure.parameters["max_inward"] = orm_measure.max_inward_reinforcement
-        measure.parameters["ID"] = orm_measure.get_id()
 
-    def _import_soil_reinforcement_measure(self, orm_measure: Measure) -> SoilReinforcementMeasure:
-        _measure = SoilReinforcementMeasure()
-        self._set_standard_measure_values(_measure, orm_measure.standard_measure)
-        _measure.parameters["Type"] = "Soil Reinforcement"
+    def _get_standard_measure(self, measure_type: Type[MeasureBase], orm_measure: StandardMeasure) -> MeasureBase:
+        _measure = measure_type()
+        self._set_base_values(_measure)
+        _measure.crest_step = orm_measure.crest_step
+        _measure.config = self._vrtool_config
+        _measure.crest_step = orm_measure.crest_step
+        _measure.berm_step = self.berm_step
+        _measure.t_0 = self.t_0
+        _measure.geometry_plot = self.geometry_plot
+        _measure.unit_costs = self.unit_costs
+        _measure.parameters = {}
+        _measure.parameters["Type"] = orm_measure.measure.measure_type.name
+        _measure.parameters["Direction"] = orm_measure.direction
+        _measure.parameters["StabilityScreen"] = "yes" if orm_measure.stability_screen else "no"
+        _measure.parameters["dcrest_min"] = None
+        _measure.parameters["dcrest_max"] = orm_measure.max_crest_increase
+        _measure.parameters["max_outward"] = orm_measure.max_outward_reinforcement
+        _measure.parameters["max_inward"] = orm_measure.max_inward_reinforcement
+        _measure.parameters["year"] = orm_measure.measure.year
+        _measure.parameters["P_solution"] = orm_measure.prob_of_solution_failure
+        _measure.parameters["Pf_solution"] = orm_measure.failure_probability_with_solution
+        _measure.parameters["ID"] = orm_measure.get_id()
+        return _measure
+
+    def _import_custom_measure(self, orm_measure: OrmCustomMeasure) -> CustomMeasure:
+        _measure = CustomMeasure()
+        self._set_base_values(_measure)
+        _measure.measures["Cost"] = orm_measure.cost
+        _measure.measures["Reliability"] = orm_measure.beta
+        _measure.parameters["year"] = orm_measure.year
+
+        for _measure_parameter in orm_measure.parameters:
+            _measure.parameters[_measure_parameter.parameter] = _measure_parameter.value
 
         return _measure
 
-    def _import_diaphragm_wall_measure(self, orm_measure: Measure) -> DiaphragmWallMeasure:
-        _measure = DiaphragmWallMeasure()
-        self._set_standard_measure_values(_measure, orm_measure.standard_measure)
-        _measure.parameters["Type"] = "Diaphragm Wall"
-
-    def _import_stability_screen_measure(self, orm_measure: Measure) -> StabilityScreenMeasure:
-        _measure = StabilityScreenMeasure()
-        self._set_standard_measure_values(_measure, orm_measure.standard_measure)
-        _measure.parameters["Type"] = "Stability Screen"
-
-    def _import_vertical_geotextile_measure(self, orm_measure: Measure) -> VerticalGeotextileMeasure:
-        _measure = VerticalGeotextileMeasure()
-        self._set_standard_measure_values(_measure, orm_measure.standard_measure)
-        _measure.parameters["Type"] = "Vertical Geotextile"
-
-    def _import_custom_measure(self) -> CustomMeasure:
-        raise NotImplementedError()
-
     def _import_standard_measure(self, orm_measure: StandardMeasure) -> MeasureBase:
-        _measure_type = orm_measure.measure.measure_type.name.lower()
-        if _measure_type == "Soil Reinforcement":
-            return self._import_soil_reinforcement_measure(orm_measure)
-        elif _measure_type == "Diaphragm Wall":
-            return self._import_diaphragm_wall_measure(orm_measure)
-        elif _measure_type == "Stability Screen":
-            return self._import_stability_screen_measure(orm_measure)
-        elif _measure_type == "Vertical Geotextile":
-            return self._import_vertical_geotextile_measure(orm_measure)
-        else:
-            raise NotImplementedError("No import available for {}.".format(_measure_type))
+        _mapping_types = {
+            "Soil Reinforcement": SoilReinforcementMeasure,
+            "Diaphragm Wall": DiaphragmWallMeasure,
+            "Stability Screen": StabilityScreenMeasure,
+            "Vertical Geotextile": VerticalGeotextileMeasure,
+        }
+
+        _found_type =_mapping_types.get(orm_measure.measure.measure_type.name, None)
+        if not _found_type:
+            raise NotImplementedError("No import available for {}.".format(orm_measure.measure.measure_type.name))
+        
+        return self._get_standard_measure(_found_type, orm_measure)
 
     def import_orm(self, orm_model: Measure) -> MeasureBase:
         _measure_type = orm_model.measure_type.name.lower()
