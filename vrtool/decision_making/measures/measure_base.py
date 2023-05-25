fc926b07c90a24decbf9192f464e2d8d5eae1089
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-25 16:28:30 +0200

    chore: Extracted properties of MeasureBase

--- vrtool/decision_making/measures/measure_base.py
+++ vrtool/decision_making/measures/measure_base.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import numpy as np
 
 from vrtool.common.dike_traject_info import DikeTrajectInfo
@@ -11,6 +12,15 @@ class MeasureBase:
     """Possible change: create subclasses for different measures to make the below code more neat. Can be done jointly with adding outward reinforcement"""
 
     # class to store measures and their reliability. A Measure is a specific Solution (with parameters)
+    parameters: dict
+    config: VrtoolConfig
+    crest_step: float
+    berm_step: list[int]
+    t_0: int
+    geometry_plot: bool
+    unit_costs: dict
+    input_directory: Path
+
     def __init__(self, inputs, config: VrtoolConfig):
         self.parameters = {}
         for i in range(0, len(inputs)):
