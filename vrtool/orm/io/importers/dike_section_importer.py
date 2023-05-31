f49449dd118f9dd687e0bfaa928a05a0de0457d8
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-31 13:44:13 +0200

    chore: Basic connection to import the MechanismReliabilityCollection

--- vrtool/orm/io/importers/dike_section_importer.py
+++ vrtool/orm/io/importers/dike_section_importer.py
@@ -7,15 +7,17 @@ import pandas as pd
 from vrtool.defaults.vrtool_config import VrtoolConfig
 from vrtool.flood_defence_system.dike_section import DikeSection
 from vrtool.orm.io.importers.geometry_importer import GeometryImporter
+from vrtool.orm.io.importers.mechanism_reliability_collection_importer import MechanismReliabilityCollectionImporter
 from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
 from vrtool.orm.models.buildings import Buildings
 from vrtool.orm.models.section_data import SectionData
+from vrtool.orm.models.mechanism_per_section import MechanismPerSection
 
 
 class DikeSectionImporter(OrmImporterProtocol):
     input_directory: Path
     selected_mechanisms: list[str]
-    T: list[int]
+    computation_years: list[int]
     t_0: int
     externals: Path
 
@@ -25,9 +27,10 @@ class DikeSectionImporter(OrmImporterProtocol):
 
         self.input_directory = vrtool_config.input_directory
         self.selected_mechanisms = vrtool_config.mechanisms
-        self.T = vrtool_config.T
+        self.computation_years = vrtool_config.T
         self.t_0 = vrtool_config.t_0
         self.externals = vrtool_config.externals
+        self._config = vrtool_config
 
     def _import_buildings_list(self, buildings_list: list[Buildings]) -> pd.DataFrame:
         _buildings_data = [
@@ -40,6 +43,13 @@ class DikeSectionImporter(OrmImporterProtocol):
         _importer = GeometryImporter()
         return _importer.import_orm(section_data)
 
+    def _get_mechanism_data(self, section_data: SectionData) -> dict[str, MechanismPerSection]:
+        _importer = MechanismReliabilityCollectionImporter(self._config)
+        _mechanism_data = {}
+        for _mechanism_per_section in section_data.mechanisms_per_section:
+            _mechanism_data[_mechanism_per_section.mechanism.name] = _importer.import_orm(_mechanism_per_section)
+        return _mechanism_data
+
     def import_orm(self, orm_model: SectionData) -> DikeSection:
         if not orm_model:
             raise ValueError(f"No valid value given for {SectionData.__name__}.")
@@ -48,8 +58,6 @@ class DikeSectionImporter(OrmImporterProtocol):
         _dike_section.name = orm_model.section_name
         _dike_section.houses = self._import_buildings_list(orm_model.buildings_list)
         _dike_section.InitialGeometry = self._import_geometry(orm_model)
-        _dike_section.mechanism_data = {}
-        for _mechanism_per_section in orm_model.mechanisms_per_section:
-            _dike_section.mechanism_data[_mechanism_per_section.mechanism.name] = ()
-
+        _dike_section.mechanism_data = self._get_mechanism_data(orm_model)        
         return _dike_section
+
