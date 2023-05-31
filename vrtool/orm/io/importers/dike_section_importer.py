1a54617ecc1d6dd57cb31a4b72ee7cda9055e240
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-05-31 15:55:36 +0200

    chore: Extended dike section importer to include import of water level (InputLoad)

--- vrtool/orm/io/importers/dike_section_importer.py
+++ vrtool/orm/io/importers/dike_section_importer.py
@@ -3,12 +3,16 @@ from __future__ import annotations
 from pathlib import Path
 
 import pandas as pd
+from vrtool.common.hydraulic_loads.load_input import LoadInput
 
 from vrtool.defaults.vrtool_config import VrtoolConfig
 from vrtool.flood_defence_system.dike_section import DikeSection
+from vrtool.flood_defence_system.mechanism_reliability_collection import MechanismReliabilityCollection
+from vrtool.flood_defence_system.section_reliability import SectionReliability
 from vrtool.orm.io.importers.geometry_importer import GeometryImporter
 from vrtool.orm.io.importers.mechanism_reliability_collection_importer import MechanismReliabilityCollectionImporter
 from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
+from vrtool.orm.io.importers.water_level_importer import WaterLevelImporter
 from vrtool.orm.models.buildings import Buildings
 from vrtool.orm.models.section_data import SectionData
 from vrtool.orm.models.mechanism_per_section import MechanismPerSection
@@ -43,7 +47,7 @@ class DikeSectionImporter(OrmImporterProtocol):
         _importer = GeometryImporter()
         return _importer.import_orm(section_data)
 
-    def _get_mechanism_data(self, section_data: SectionData) -> dict[str, MechanismPerSection]:
+    def _get_mechanism_data(self, section_data: SectionData) -> dict[str, MechanismReliabilityCollection]:
         _importer = MechanismReliabilityCollectionImporter(self._config)
         _mechanism_data = {}
         for _mechanism_per_section in section_data.mechanisms_per_section:
@@ -54,6 +58,13 @@ class DikeSectionImporter(OrmImporterProtocol):
                 _mechanism_data[_mechanism_per_section.mechanism.name] = _importer.import_orm(_mechanism_per_section)
         return _mechanism_data
 
+    def _get_section_reliability(self, mechanism_collection: dict[str, MechanismReliabilityCollection], section_data: SectionData) -> SectionReliability:
+        _section_reliability = SectionReliability()
+        _section_reliability.load =  WaterLevelImporter(gridpoints=1000).import_orm(section_data)
+
+        for _mechanism_data in mechanism_collection.values():
+            _section_reliability.failure_mechanisms.add_failure_mechanism_reliability_collection(_mechanism_data)
+
     def import_orm(self, orm_model: SectionData) -> DikeSection:
         if not orm_model:
             raise ValueError(f"No valid value given for {SectionData.__name__}.")
@@ -62,6 +73,9 @@ class DikeSectionImporter(OrmImporterProtocol):
         _dike_section.name = orm_model.section_name
         _dike_section.houses = self._import_buildings_list(orm_model.buildings_list)
         _dike_section.InitialGeometry = self._import_geometry(orm_model)
-        _dike_section.mechanism_data = self._get_mechanism_data(orm_model)        
+        # TODO: Not entirely sure mechanism_data is correctly set. Technically should not be needed anymore.
+        _dike_section.mechanism_data = self._get_mechanism_data(orm_model) 
+        _dike_section.section_reliability = self._get_section_reliability(_dike_section.mechanism_data)
+
         return _dike_section
 
