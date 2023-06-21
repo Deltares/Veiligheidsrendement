fe80f07517108b970ee2ac8fbc134a5798152cdb
Edwin Spee <edwin.spee@deltares.nl>
2023-06-21 14:27:50 +0200

    add data class for revetments

---
+++ tests/failure_mechanisms/revetment/test_revetment_dataclass.py
@@ -0,0 +1,64 @@
+import pytest
+
+from vrtool.failure_mechanisms.revetment.revertment_data_class import (
+    SlopePart,
+    RelationGrassRevetment,
+    RelationStoneRevetment,
+    RevetmentDataClass,
+)
+
+
+class TestRevetmentDataClass:
+    def test_slope_part(self):
+        slope = SlopePart(1, 2, 0.333, 20)
+        revetments = RevetmentDataClass()
+        revetments.slope_parts.append(slope)
+
+        assert len(revetments.slope_parts) == 1
+        assert revetments.slope_parts[0].begin_part == 1
+        assert revetments.slope_parts[0].end_part == 2
+        assert revetments.slope_parts[0].tan_alpha == 0.333
+        assert revetments.slope_parts[0].top_layer_type == 20
+
+    def test_relation_grass(self):
+        grass = RelationGrassRevetment(2025, 2.0, 2.1)
+        revetments = RevetmentDataClass()
+        revetments.grass_relations.append(grass)
+
+        assert len(revetments.grass_relations) == 1
+        assert revetments.grass_relations[0].year == 2025
+        assert revetments.grass_relations[0].transition_level == 2.0
+        assert revetments.grass_relations[0].beta == 2.1
+
+    def test_relation_stone(self):
+        stone1 = RelationStoneRevetment(0, 2050, 2.3, 3.2)
+        stone2 = RelationStoneRevetment(1, 2050, 3.2, 2.3)
+        revetments = RevetmentDataClass()
+        revetments.stone_relations.append(stone1)
+        revetments.stone_relations.append(stone2)
+
+        assert len(revetments.stone_relations) == 2
+        assert revetments.stone_relations[0].year == 2050
+        assert revetments.stone_relations[1].slope_part == 1
+
+    def test_current_transition_level(self):
+        revetments = RevetmentDataClass()
+        revetments.slope_parts.append(SlopePart(1, 2, 0.31, 5, 0.1))
+        revetments.slope_parts.append(SlopePart(2, 3, 0.32, 26.1, 0.15))
+        revetments.slope_parts.append(SlopePart(3, 4, 0.33, 20))
+        revetments.slope_parts.append(SlopePart(4, 5, 0.34, 20))
+
+        level = revetments.current_transition_level()
+
+        assert level == 3
+
+    def test_current_transition_level_no_grass(self):
+        revetments = RevetmentDataClass()
+        revetments.slope_parts.append(SlopePart(1, 2, 0.31, 5, 0.1))
+        revetments.slope_parts.append(SlopePart(2, 3, 0.32, 26.1, 0.15))
+
+        with pytest.raises(Exception) as exception_error:
+            revetments.current_transition_level()
+
+        # Assert
+        assert str(exception_error.value) == "No slope part with grass found"
---
+++ vrtool/failure_mechanisms/revetment/revertment_data_class.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+class SlopePart:
+    # Stores data for slope part
+    def __init__(
+        self,
+        begin_part: float,
+        end_part: float,
+        tan_alpha: float,
+        top_layer_type: float,  # note that e.g. 26.1 is a valid type
+        top_layer_thickness=np.NaN,
+    ):
+        self.begin_part = begin_part
+        self.end_part = end_part
+        self.tan_alpha = tan_alpha
+        self.top_layer_type = top_layer_type
+        self.top_layer_thickness = top_layer_thickness
+
+
+class RelationGrassRevetment:
+    # Stores data for relation grass revetment
+    def __init__(self, year: int, transition_level: float, beta: float):
+        self.year = year
+        self.transition_level = transition_level
+        self.beta = beta
+
+
+class RelationStoneRevetment:
+    # Stores data for relation stone revetment
+    def __init__(
+        self, slope_part: int, year: int, top_layer_thickness: float, beta: float
+    ):
+        self.year = year
+        self.top_layer_thickness = top_layer_thickness
+        self.beta = beta
+        self.slope_part = slope_part
+
+
+class RevetmentDataClass:
+
+    GRASS_TYPE = 20.0
+
+    def __init__(self):
+        self.slope_parts = []  # for a list of SlopePart
+        self.grass_relations = []  # for a list of RelationGrassRevetment
+        self.stone_relations = []  # for a list of RelationStoneRevetment
+
+    def current_transition_level(self) -> float:
+        min_value_grass = 1e99
+        for self.slope_part in self.slope_parts:
+            if self.slope_part.top_layer_type == self.GRASS_TYPE:
+                if self.slope_part.begin_part < min_value_grass:
+                    min_value_grass = self.slope_part.begin_part
+
+        if min_value_grass == 1e99:
+            raise Exception("No slope part with grass found")
+
+        return min_value_grass
