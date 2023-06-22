9fc84ea79f55a1c939feb60c4a9de276e37ecf9d
Edwin Spee <edwin.spee@deltares.nl>
2023-06-22 14:30:07 +0200

    cleanup: change revetment_data_class into a dataclass

--- tests/failure_mechanisms/revetment/test_revetment_dataclass.py
+++ tests/failure_mechanisms/revetment/test_revetment_dataclass.py
@@ -11,6 +11,16 @@ from vrtool.failure_mechanisms.revetment.relation_stone_revetment import (
 
 
 class TestRevetmentDataClass:
+    def test_basic_usage(self):
+        revetments = RevetmentDataClass()
+        revetments.slope_parts.append(SlopePart(1, 2, 0.31, 5, 0.1))
+        revetments.grass_relations.append(RelationGrassRevetment(2030, 2.4, 4.5))
+        revetments.stone_relations.append(RelationStoneRevetment(0, 2050, 1.2, 3.1))
+
+        assert len(revetments.slope_parts) == 1
+        assert len(revetments.grass_relations) == 1
+        assert len(revetments.stone_relations) == 1
+
     def test_current_transition_level(self):
         revetments = RevetmentDataClass()
         revetments.slope_parts.append(SlopePart(1, 2, 0.31, 5, 0.1))
@@ -18,7 +28,7 @@ class TestRevetmentDataClass:
         revetments.slope_parts.append(SlopePart(3, 4, 0.33, 20))
         revetments.slope_parts.append(SlopePart(4, 5, 0.34, 20))
 
-        level = revetments.current_transition_level()
+        level = revetments.current_transition_level
 
         assert level == 3
 
@@ -28,7 +38,7 @@ class TestRevetmentDataClass:
         revetments.slope_parts.append(SlopePart(2, 3, 0.32, 26.1, 0.15))
 
         with pytest.raises(ValueError) as exception_error:
-            revetments.current_transition_level()
+            revetments.current_transition_level
 
         # Assert
         assert str(exception_error.value) == "No slope part with grass found"
--- vrtool/failure_mechanisms/revetment/revetment_data_class.py
+++ vrtool/failure_mechanisms/revetment/revetment_data_class.py
@@ -1,20 +1,32 @@
-# from vrtool.failure_mechanisms.revetment.slope_part import SlopePart
+from __future__ import annotations
+
+import sys
+from dataclasses import dataclass, field
+
+from vrtool.failure_mechanisms.revetment.slope_part import SlopePart
+from vrtool.failure_mechanisms.revetment.relation_grass_revetment import (
+    RelationGrassRevetment,
+)
+from vrtool.failure_mechanisms.revetment.relation_stone_revetment import (
+    RelationStoneRevetment,
+)
 
 
+@dataclass
 class RevetmentDataClass:
-    def __init__(self):
-        self.slope_parts = []  # for a list of SlopePart
-        self.grass_relations = []  # for a list of RelationGrassRevetment
-        self.stone_relations = []  # for a list of RelationStoneRevetment
+    slope_parts: list[SlopePart] = field(default_factory=lambda: [])
+    grass_relations: list[RelationGrassRevetment] = field(default_factory=lambda: [])
+    stone_relations: list[RelationStoneRevetment] = field(default_factory=lambda: [])
 
+    @property
     def current_transition_level(self) -> float:
-        min_value_grass = 1e99
+        min_value_grass = sys.float_info.max
         for slope_part in self.slope_parts:
             if slope_part.is_grass:
                 if slope_part.begin_part < min_value_grass:
                     min_value_grass = slope_part.begin_part
 
-        if min_value_grass == 1e99:
+        if min_value_grass == sys.float_info.max:
             raise ValueError("No slope part with grass found")
 
         return min_value_grass
