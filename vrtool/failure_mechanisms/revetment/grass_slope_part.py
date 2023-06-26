332a1e5a66bd48760fd4f22c001f682170c84be3
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-06-26 16:25:25 +0200

    chore: Added missing decorators to slope part model definitions

--- vrtool/failure_mechanisms/revetment/grass_slope_part.py
+++ vrtool/failure_mechanisms/revetment/grass_slope_part.py
@@ -1,9 +1,11 @@
 from vrtool.failure_mechanisms.revetment.slope_part import SlopePartProtocol
+from dataclasses import dataclass
 
 
 GRASS_TYPE = 20.0
 
 
+@dataclass
 class GrassSlopePart(SlopePartProtocol):
     def is_valid(self) -> bool:
         return self.top_layer_type == GRASS_TYPE
--- vrtool/failure_mechanisms/revetment/stone_slope_part.py
+++ vrtool/failure_mechanisms/revetment/stone_slope_part.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from vrtool.failure_mechanisms.revetment.slope_part import SlopePartProtocol
 
 
@@ -5,6 +6,7 @@ MIN_BLOCK = 26.0
 MAX_BLOCK = 27.9
 
 
+@dataclass
 class StoneSlopePart(SlopePartProtocol):
     def is_valid(self) -> bool:
         return self.top_layer_type >= MIN_BLOCK and self.top_layer_type <= MAX_BLOCK
