8bf1ea38626d6ce8a6ff3cd8f8eff39f01af3581
Edwin Spee <edwin.spee@deltares.nl>
2023-06-28 15:36:36 +0200

    clean up
    add _FindGivenYears()

--- vrtool/failure_mechanisms/revetment/revetment_calculation_assessment.py
+++ vrtool/failure_mechanisms/revetment/revetment_calculation_assessment.py
@@ -14,12 +14,33 @@ from vrtool.probabilistic_tools.probabilistic_functions import beta_to_pf
 
 class RevetmentCalculation(FailureMechanismCalculatorProtocol):
     def __init__(self, revetment: RevetmentDataClass) -> None:
-        self._r = revetment
-        self.given_years = set()
-        for _slope_part in revetment.slope_parts:
-            if isinstance(_slope_part, StoneSlopePart):
-                for rel in _slope_part.slope_part_relations:
-                    self.given_years.add(rel.year)
+        self._revetment = revetment
+        self._given_years = self._FindGivenYears(revetment)
+
+    def calculate(self, year: int) -> tuple[float, float]:
+
+        betaPerYear = []
+        for given_year in self._given_years:
+            beta_zst = []
+            beta_gebu = np.nan
+            for _slope_part in self._revetment.slope_parts:
+                if isinstance(_slope_part, StoneSlopePart):
+                    beta_zst.append(self._evaluate_block(_slope_part, given_year))
+                elif isinstance(_slope_part, GrassSlopePart) and np.isnan(beta_gebu):
+                    beta_zst.append(np.nan)
+                    beta_gebu = self._evaluate_grass(given_year)
+                else:
+                    beta_zst.append(np.nan)
+            betaPerYear.append(self._beta_comb(beta_zst, beta_gebu))
+
+        if len(self._given_years) == 1:
+            return betaPerYear[0], beta_to_pf(betaPerYear[0])
+        else:
+            intBeta = interp1d(
+                self._given_years, betaPerYear, fill_value=("extrapolate")
+            )
+            finalBeta = intBeta(year)
+            return finalBeta, beta_to_pf(finalBeta)
 
     def _beta_comb(self, betaZST: list[float], betaGEBU: float) -> float:
         if np.all(np.isnan(betaZST)):
@@ -35,31 +56,6 @@ class RevetmentCalculation(FailureMechanismCalculatorProtocol):
         probComb = probZST + probGEBU
         betaComb = -ndtri(probComb)
         return betaComb
-
-    def calculate(self, year: int) -> tuple[float, float]:
-
-        betaPerYear = []
-        for given_year in self.given_years:
-            beta_zst = []
-            beta_gebu = np.nan
-            for _slope_part in self._r.slope_parts:
-                if isinstance(_slope_part, StoneSlopePart):
-                    beta_zst.append(self._evaluate_block(_slope_part, given_year))
-                elif isinstance(_slope_part, GrassSlopePart) and np.isnan(beta_gebu):
-                    beta_zst.append(np.nan)
-                    beta_gebu = self._evaluate_grass(given_year)
-                else:
-                    beta_zst.append(np.nan)
-            betaPerYear.append(self._beta_comb(beta_zst, beta_gebu))
-
-        if len(self.given_years) == 1:
-            return betaPerYear[0], beta_to_pf(betaPerYear[0])
-        else:
-            intBeta = interp1d(
-                list(self.given_years), betaPerYear, fill_value=("extrapolate")
-            )
-            finalBeta = intBeta(year)
-            return finalBeta, beta_to_pf(finalBeta)
 
     def _evaluate_block(self, slope_part: StoneSlopePart, year: int):
         D_opt = []
@@ -77,12 +73,21 @@ class RevetmentCalculation(FailureMechanismCalculatorProtocol):
     def _evaluate_grass(self, year: int):
         transitions = []
         betaFailure = []
-        for rel in self._r.grass_relations:
+        for rel in self._revetment.grass_relations:
             if rel.year == year:
                 transitions.append(rel.transition_level)
                 betaFailure.append(rel.beta)
 
         fgrass = interp1d(transitions, betaFailure, fill_value=("extrapolate"))
-        beta = fgrass(self._r.current_transition_level)
+        beta = fgrass(self._revetment.current_transition_level)
 
         return beta
+
+    def _FindGivenYears(self, revetment: RevetmentDataClass) -> set[int]:
+        given_years = set()
+        for _slope_part in revetment.slope_parts:
+            if isinstance(_slope_part, StoneSlopePart):
+                for rel in _slope_part.slope_part_relations:
+                    given_years.add(rel.year)
+                break
+        return list(given_years)
