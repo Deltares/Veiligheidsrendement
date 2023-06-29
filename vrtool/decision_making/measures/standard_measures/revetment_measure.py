040a65617110f4ede87663ce0a0226fcb9db4625
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-06-29 21:13:20 +0200

    chore: Slight code cleanup

--- vrtool/decision_making/measures/standard_measures/revetment_measure.py
+++ vrtool/decision_making/measures/standard_measures/revetment_measure.py
@@ -99,8 +99,6 @@ class RevetmentMeasure(MeasureProtocol):
         if not StoneSlopePart.is_stone_slope_part(top_layer_type):
             return _top_layer_thickness, _recalculated_beta, _is_reinforced
 
-        # only for steen
-
         stone_interpolation = interpolate.interp1d(
             top_layer_thickness,
             np.array(revetment_beta) - calculated_beta,
@@ -123,7 +121,7 @@ class RevetmentMeasure(MeasureProtocol):
 
         return _top_layer_thickness, _recalculated_beta, _is_reinforced
 
-    def get_stone_revetment_measure_data(
+    def _get_stone_revetment_measure_data(
         self, slope_part: SlopePartProtocol, stone_revetment: RelationStoneRevetment
     ) -> RevetmentMeasureData:
         (
@@ -137,20 +135,19 @@ class RevetmentMeasure(MeasureProtocol):
             slope_part.top_layer_type,
             slope_part.top_layer_thickness,
         )
-        _revetment_measure_data = RevetmentMeasureData()
+        return RevetmentMeasureData(
+            begin_part=slope_part.begin_part,
+            end_part=slope_part.end_part,
+            top_layer_type=slope_part.top_layer_type,
+            previous_top_layer_type=slope_part.top_layer_type,
+            top_layer_thickness=_new_top_layer_thickness,
+            beta_block_revetment=_new_beta_block_revetment,
+            beta_grass_revetment=float("nan"),
+            reinforce=_is_reinforced,
+            tan_alpha=slope_part.tan_alpha,
+        )
 
-        _revetment_measure_data.begin_part = slope_part.begin_part
-        _revetment_measure_data.end_part = slope_part.end_part
-        _revetment_measure_data.top_layer_type = slope_part.top_layer_type
-        _revetment_measure_data.previous_top_layer_type = slope_part.top_layer_type
-        _revetment_measure_data.top_layer_thickness = _new_top_layer_thickness
-        _revetment_measure_data.beta_block_revetment = _new_beta_block_revetment
-        _revetment_measure_data.beta_grass_revetment = float("nan")
-        _revetment_measure_data.reinforce = _is_reinforced
-        _revetment_measure_data.tan_alpha = slope_part.tan_alpha
-        return _revetment_measure_data
-
-    def get_combined_revetment_data(
+    def _get_combined_revetment_data(
         self,
         slope_part: SlopePartProtocol,
         stone_revetment: RelationStoneRevetment,
@@ -167,50 +164,48 @@ class RevetmentMeasure(MeasureProtocol):
             slope_part.top_layer_type,
             slope_part.top_layer_thickness,
         )
-        _stone_rmd = RevetmentMeasureData()
-        _stone_rmd.begin_part = slope_part.begin_part
-        _stone_rmd.end_part = transition_level
-        _stone_rmd.top_layer_type = slope_part.top_layer_type
-        _stone_rmd.previous_top_layer_type = slope_part.top_layer_type
-        _stone_rmd.top_layer_thickness = _new_top_layer_thickness
-        _stone_rmd.beta_block_revetment = _new_beta_block_revetment
-        _stone_rmd.beta_grass_revetment = float("nan")
-        _stone_rmd.reinforce = _is_reinforced
-        _stone_rmd.tan_alpha = slope_part.tan_alpha
-
-        _grass_rmd = RevetmentMeasureData()
-        _grass_rmd.begin_part = transition_level
-        _grass_rmd.end_part = slope_part.end_part
-        _grass_rmd.top_layer_type = GRASS_TYPE
-        _grass_rmd.previous_top_layer_type = slope_part.top_layer_type
-        _grass_rmd.top_layer_thickness = float("nan")
-        _grass_rmd.beta_block_revetment = float("nan")
-        _grass_rmd.beta_grass_revetment = self._evaluate_grass_revetment_data(
-            evaluation_year
+        _stone_rmd = RevetmentMeasureData(
+            begin_part=slope_part.begin_part,
+            end_part=transition_level,
+            top_layer_type=slope_part.top_layer_type,
+            previous_top_layer_type=slope_part.top_layer_type,
+            top_layer_thickness=_new_top_layer_thickness,
+            beta_block_revetment=_new_beta_block_revetment,
+            beta_grass_revetment=float("nan"),
+            reinforce=_is_reinforced,
+            tan_alpha=slope_part.tan_alpha,
         )
-        _grass_rmd.reinforce = True
-        _grass_rmd.tan_alpha = slope_part.tan_alpha
+
+        _grass_rmd = RevetmentMeasureData(
+            begin_part=transition_level,
+            end_part=slope_part.end_part,
+            top_layer_type=GRASS_TYPE,
+            previous_top_layer_type=slope_part.top_layer_type,
+            top_layer_thickness=float("nan"),
+            beta_block_revetment=float("nan"),
+            beta_grass_revetment=self._evaluate_grass_revetment_data(evaluation_year),
+            reinforce=True,
+            tan_alpha=slope_part.tan_alpha,
+        )
 
         return _stone_rmd, _grass_rmd
 
-    def get_grass_revetment_data(
+    def _get_grass_revetment_data(
         self, slope_part: SlopePartProtocol
     ) -> RevetmentMeasureData:
-        _grass_rmd = RevetmentMeasureData()
-        _grass_rmd.begin_part = slope_part.begin_part
-        _grass_rmd.end_part = slope_part.end_part
-        _grass_rmd.top_layer_type = GRASS_TYPE
-        _grass_rmd.previous_top_layer_type = slope_part.top_layer_type
-        _grass_rmd.top_layer_thickness = float("nan")
-        _grass_rmd.beta_block_revetment = float("nan")
-        _grass_rmd.beta_grass_revetment = self._evaluate_grass_revetment_data(
-            evaluation_year
+        return RevetmentMeasureData(
+            begin_part=slope_part.begin_part,
+            end_part=slope_part.end_part,
+            top_layer_type=GRASS_TYPE,
+            previous_top_layer_type=slope_part.top_layer_type,
+            top_layer_thickness=float("nan"),
+            beta_block_revetment=float("nan"),
+            beta_grass_revetment=self._evaluate_grass_revetment_data(evaluation_year),
+            reinforce=True,
+            tan_alpha=slope_part.tan_alpha,
         )
-        _grass_rmd.reinforce = True
-        _grass_rmd.tan_alpha = slope_part.tan_alpha
-        return _grass_rmd
 
-    def evaluate_revetment_measure(
+    def _evaluate_revetment_measure(
         self,
         revetment_data_class: RevetmentDataClass,
         beta,
@@ -220,14 +215,16 @@ class RevetmentMeasure(MeasureProtocol):
         _evaluated_measures = []
         for _slope_part in revetment_data_class.slope_parts:
             if _slope_part.end_part <= transition_level:
-                _evaluated_measures.append(self.get_stone_revetment_measure_data(beta))
+                _evaluated_measures.append(self._get_stone_revetment_measure_data(beta))
             elif (
                 _slope_part.begin_part < transition_level
                 and _slope_part.end_part > transition_level
             ):
-                _evaluated_measures.extend(list(self.get_combined_revetment_data(beta)))
+                _evaluated_measures.extend(
+                    list(self._get_combined_revetment_data(beta))
+                )
             elif _slope_part.begin_part >= transition_level:
-                _evaluated_measures.append(self.get_grass_revetment_data(beta))
+                _evaluated_measures.append(self._get_grass_revetment_data(beta))
             # TODO: Check, raise an error or just log? Original script raises nothing.
             raise ValueError("Can't evaluate revetment measure.")
 
@@ -260,157 +257,13 @@ class RevetmentMeasure(MeasureProtocol):
         preserve_slope: bool,
     ):
         self.measures = {}
+        _revetment_measures_collection = self._evaluate_revetment_measure(
+            self.revetment_mechanism_calculator._revetment, ..., ..., ...
+        )
+        _revetment_measures_collection_as_dictionaries = [
+            rm.__dict__ for rm in _revetment_measures_collection
+        ]
         self.measures["Reliability"] = self._calculate_section_reliability()
         self.measures["Cost"] = self.revetment_mechanism_calculator.calculate_cost(
             dike_section.Length, year
         )
-
-    def _evaluate_stone_slope(
-        self, slope_part: SlopePartProtocol, d_opt, beta, beta_failure
-    ):
-
-        fsteen = interpolate.interp1d(
-            d_opt, np.array(beta_failure) - beta, fill_value=("extrapolate")
-        )
-        try:
-            topd = bisection(fsteen, 0.0, 1.0, 0.01)
-            topd = math.ceil(topd / 0.05) * 0.05
-            betares = beta
-            reinforce = "yes"
-        except:
-            topd = np.nan
-            betares = np.nan
-            reinforce = "no"
-
-        if topd <= D:
-            warnings.warn("Design D is <= than the current D")
-            topd = D
-            betares = evaluate_steen(topd, D_opt, betaFalen)
-            reinforce = "no"
-        measure["Zo"].append(dataZST["Zo"][i])
-        measure["Zb"].append(dataZST["Zb"][i])
-        measure["toplaagtype"].append(dataZST["toplaagtype"][i])
-        measure["previous toplaagtype"].append(dataZST["toplaagtype"][i])
-        measure["D"].append(topd)
-        measure["betaZST"].append(betares)
-        measure["betaGEBU"].append(np.nan)
-        measure["reinforce"].append(reinforce)
-        measure["tana"].append(dataZST["tana"][i])
-
-    def _evaluate_measure_karolina_script(self):
-        measure = {
-            "Zo": list(),
-            "Zb": list(),
-            "toplaagtype": list(),
-            "D": list(),
-            "betaZST": list(),
-            "betaGEBU": list(),
-            "previous toplaagtype": list(),
-            "reinforce": list(),
-            "tana": list(),
-        }
-
-        count = 0
-        for i in range(0, dataZST["aantal deelvakken"]):
-
-            if dataZST["Zb"][i] <= h_onder:
-
-                count += 1
-                # steen
-                topd, betares, reinforce = design_steen(
-                    beta,
-                    dataZST[f"deelvak {i}"]["D_opt"],
-                    dataZST[f"deelvak {i}"]["betaFalen"],
-                    dataZST["toplaagtype"][i],
-                    dataZST["D huidig"][i],
-                )
-
-                measure["Zo"].append(dataZST["Zo"][i])
-                measure["Zb"].append(dataZST["Zb"][i])
-                measure["toplaagtype"].append(dataZST["toplaagtype"][i])
-                measure["previous toplaagtype"].append(dataZST["toplaagtype"][i])
-                measure["D"].append(topd)
-                measure["betaZST"].append(betares)
-                measure["betaGEBU"].append(np.nan)
-                measure["reinforce"].append(reinforce)
-                measure["tana"].append(dataZST["tana"][i])
-
-            elif (
-                dataZST["Zo"][i] < h_onder and dataZST["Zb"][i] > h_onder
-            ):  # part is steen and part is gras
-
-                count += 2
-                # steen
-                topd, betares, reinforce = design_steen(
-                    beta,
-                    dataZST[f"deelvak {i}"]["D_opt"],
-                    dataZST[f"deelvak {i}"]["betaFalen"],
-                    dataZST["toplaagtype"][i],
-                    dataZST["D huidig"][i],
-                )
-
-                measure["Zo"].append(dataZST["Zo"][i])
-                measure["Zb"].append(h_onder)
-                measure["toplaagtype"].append(dataZST["toplaagtype"][i])
-                measure["previous toplaagtype"].append(dataZST["toplaagtype"][i])
-                measure["D"].append(topd)
-                measure["betaZST"].append(betares)
-                measure["betaGEBU"].append(np.nan)
-                measure["reinforce"].append(reinforce)
-                measure["tana"].append(dataZST["tana"][i])
-
-                # gras
-                measure["Zo"].append(h_onder)
-                measure["Zb"].append(dataZST["Zb"][i])
-                measure["toplaagtype"].append(20.0)
-                measure["previous toplaagtype"].append(dataZST["toplaagtype"][i])
-                measure["D"].append(np.nan)
-                measure["betaZST"].append(np.nan)
-                measure["betaGEBU"].append(
-                    evaluate_gras(
-                        h_onder, dataGEBU["grasbekleding_begin"], dataGEBU["betaFalen"]
-                    )
-                )
-                measure["reinforce"].append("yes")
-                measure["tana"].append(dataZST["tana"][i])
-
-            elif dataZST["Zo"][i] >= h_onder:  # gras
-
-                count += 1
-                # gras
-                measure["Zo"].append(dataZST["Zo"][i])
-                measure["Zb"].append(dataZST["Zb"][i])
-                measure["toplaagtype"].append(20.0)
-                measure["previous toplaagtype"].append(dataZST["toplaagtype"][i])
-                measure["D"].append(np.nan)
-                measure["betaZST"].append(np.nan)
-                measure["betaGEBU"].append(
-                    evaluate_gras(
-                        h_onder, dataGEBU["grasbekleding_begin"], dataGEBU["betaFalen"]
-                    )
-                )
-                measure["reinforce"].append("yes")
-                measure["tana"].append(dataZST["tana"][i])
-
-        if h_onder >= np.max(dataZST["Zb"]):
-
-            if h_onder >= kruinhoogte:
-                raise ValueError("Overgang >= kruinhoogte")
-
-            count += 1
-            # gras
-            measure["Zo"].append(h_onder)
-            measure["Zb"].append(kruinhoogte)
-            measure["toplaagtype"].append(20.0)
-            measure["previous toplaagtype"].append(np.nan)
-            measure["D"].append(np.nan)
-            measure["betaZST"].append(np.nan)
-            measure["betaGEBU"].append(
-                evaluate_gras(
-                    h_onder, dataGEBU["grasbekleding_begin"], dataGEBU["betaFalen"]
-                )
-            )
-            measure["reinforce"].append("yes")
-            measure["tana"].append(dataZST["tana"][i])
-
-        return count, measure
