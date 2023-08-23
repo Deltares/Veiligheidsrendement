2a27a6f0afee08254a8c8745de7a063c3b219ddf
MatthiasHauth <matthias.hauth@deltares.nl>
2023-08-23 14:25:49 +0200

    cleanup

---
+++ vrtool/orm/models/assessment_mechanism_results.py
@@ -0,0 +1,13 @@
+from peewee import ForeignKeyField, FloatField
+
+from vrtool.orm.models.mechanism_per_section import MechanismPerSection
+from vrtool.orm.models.orm_base_model import OrmBaseModel, _get_table_name
+
+
+class AssessmentMechanismResults(OrmBaseModel):
+    beta = FloatField()
+    time = FloatField()
+    mechanism_per_section_id = ForeignKeyField(MechanismPerSection, backref="assessment_results")
+
+    class Meta:
+        table_name = _get_table_name(__qualname__)
