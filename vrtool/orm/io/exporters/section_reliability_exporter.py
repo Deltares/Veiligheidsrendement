6eba3b903cf46f0f1e37fd8eecf160bd335c4a51
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-08-24 09:39:44 +0200

    chore: Added basic logic for exporting from dom to orm

---
+++ vrtool/orm/io/exporters/section_reliability_exporter.py
@@ -0,0 +1,27 @@
+from typing import Any
+from vrtool.flood_defence_system.dike_section import DikeSection
+from vrtool.flood_defence_system.section_reliability import SectionReliability
+from vrtool.orm.io.exporters.orm_exporter_protocol import OrmExporterProtocol
+from vrtool.orm.models.assessment_section_results import AssessmentSectionResults
+from vrtool.orm.models.orm_base_model import OrmBaseModel
+from vrtool.orm.models.section_data import SectionData
+
+
+class SectionReliabilityExporter(OrmExporterProtocol):
+    _section_data: SectionData
+
+    def __init__(self, section_data: SectionData) -> None:
+        self._section_data = section_data
+
+    @staticmethod
+    def get_related_section_data(dike_section: DikeSection) -> SectionData:
+        pass
+
+    def export_dom(
+        self, dom_model: SectionReliability
+    ) -> list[AssessmentSectionResults]:
+        for reliability_df in dom_model.SectionReliability:
+            AssessmentSectionResults.create(
+                beta=...,
+                time=...,
+            )
