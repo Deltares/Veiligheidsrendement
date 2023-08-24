9c73bb35f0725abd0507ab86aa9f805ed8623400
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-08-24 10:01:49 +0200

    chore: Updated field (without _id)

--- vrtool/orm/models/assessment_section_results.py
+++ vrtool/orm/models/assessment_section_results.py
@@ -7,7 +7,7 @@ from vrtool.orm.models.section_data import SectionData
 class AssessmentSectionResults(OrmBaseModel):
     beta = FloatField()
     time = FloatField()
-    section_id = ForeignKeyField(SectionData, backref="assessment_section_results")
+    section_data = ForeignKeyField(SectionData, backref="assessment_section_results")
 
     class Meta:
         table_name = _get_table_name(__qualname__)
