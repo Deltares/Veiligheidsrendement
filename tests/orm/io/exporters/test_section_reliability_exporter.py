f0f8635c0d7586104f640ae10b857aa6c6d29a8e
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-08-24 14:17:21 +0200

    chore: Added `get_releated_section_data` function

--- tests/orm/io/exporters/test_section_reliability_exporter.py
+++ tests/orm/io/exporters/test_section_reliability_exporter.py
@@ -35,3 +35,20 @@ class TestSectionReliabilityExporter:
         # 3. Verify expectations.
         assert isinstance(_related_section_data, SectionData)
         assert _test_section_data == _related_section_data
+
+    def test_get_related_section_data_returns_none_for_different_traject(
+        self, empty_db_fixture
+    ):
+        # 1. Define test data.
+        _test_section_data = get_basic_section_data()
+        _dike_section = DikeSection()
+        _dike_section.name = _test_section_data.section_name
+        _dike_section.TrajectInfo = DikeTrajectInfo("not_the_traject_in_the_orm")
+
+        # 2. Run test.
+        _related_section_data = SectionReliabilityExporter.get_related_section_data(
+            _dike_section
+        )
+
+        # 3. Verify expectations.
+        assert _related_section_data is None
--- vrtool/orm/io/exporters/section_reliability_exporter.py
+++ vrtool/orm/io/exporters/section_reliability_exporter.py
@@ -3,6 +3,7 @@ from vrtool.flood_defence_system.dike_section import DikeSection
 from vrtool.flood_defence_system.section_reliability import SectionReliability
 from vrtool.orm.io.exporters.orm_exporter_protocol import OrmExporterProtocol
 from vrtool.orm.models.assessment_section_results import AssessmentSectionResults
+from vrtool.orm.models.dike_traject_info import DikeTrajectInfo
 from vrtool.orm.models.orm_base_model import OrmBaseModel
 from vrtool.orm.models.section_data import SectionData
 
@@ -22,9 +23,17 @@ class SectionReliabilityExporter(OrmExporterProtocol):
             dike_section (DikeSection): DOM dike section saved in the database.
 
         Returns:
-            SectionData: ORM instance representing the given `DikeSection`.
+            SectionData: ORM instance representing the given `DikeSection` or `None` when no match was found.
         """
-        pass
+        return (
+            SectionData.select()
+            .join(DikeTrajectInfo)
+            .where(
+                SectionData.dike_traject.traject_name
+                == dike_section.TrajectInfo.traject_name
+            )
+            .get_or_none()
+        )
 
     def export_dom(
         self, dom_model: SectionReliability
