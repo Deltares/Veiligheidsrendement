75f1d0cfdfdf92a1e44e765bd2a318e22b86fcb2
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-08-25 10:32:04 +0200

    chore: Adapted code after merge

--- vrtool/orm/io/exporters/section_reliability_exporter.py
+++ vrtool/orm/io/exporters/section_reliability_exporter.py
@@ -1,7 +1,7 @@
 from vrtool.flood_defence_system.dike_section import DikeSection
 from vrtool.flood_defence_system.section_reliability import SectionReliability
 from vrtool.orm.io.exporters.orm_exporter_protocol import OrmExporterProtocol
-from vrtool.orm.models.assessment_section_results import AssessmentSectionResults
+from vrtool.orm.models.assessment_section_result import AssessmentSectionResult
 from vrtool.orm.models.dike_traject_info import DikeTrajectInfo
 from vrtool.orm.models.section_data import SectionData
 
@@ -35,13 +35,13 @@ class SectionReliabilityExporter(OrmExporterProtocol):
 
     def export_dom(
         self, dom_model: SectionReliability
-    ) -> list[AssessmentSectionResults]:
+    ) -> list[AssessmentSectionResult]:
         _added_assessments = []
         for col_idx, beta_value in enumerate(
             dom_model.SectionReliability.loc["Section"]
         ):
             _added_assessments.append(
-                AssessmentSectionResults.create(
+                AssessmentSectionResult.create(
                     beta=beta_value,
                     time=float(dom_model.SectionReliability.columns[col_idx]),
                     section_data=self._section_data,
