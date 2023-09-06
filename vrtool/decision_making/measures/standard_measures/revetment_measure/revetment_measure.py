eeb01aa308f3a6d5320b058e9967c78ecaf76eab
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-09-06 08:12:16 +0200

    chore: Small code correction for better readability

--- vrtool/decision_making/measures/standard_measures/revetment_measure/revetment_measure.py
+++ vrtool/decision_making/measures/standard_measures/revetment_measure/revetment_measure.py
@@ -50,8 +50,8 @@ class RevetmentMeasure(MeasureProtocol):
         _mech_reliability_collection = dike_section.section_reliability.failure_mechanisms.get_mechanism_reliability_collection(
             "Revetment"
         )
-        _years = map(int, _mech_reliability_collection.Reliability.keys())
-        return _mech_reliability_collection.Reliability[str(min(_years))].Beta
+        _min_reliability_year = str(min(map(int, _mech_reliability_collection.Reliability.keys())))
+        return _mech_reliability_collection.Reliability[_min_reliability_year].Beta
 
     def _get_beta_target_vector(self, min_beta: float, p_max: float) -> list[float]:
         _max_beta = pf_to_beta(p_max / self.max_pf_factor_block)
