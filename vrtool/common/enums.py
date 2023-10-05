18ec10a0567e67b88c554df2d162cc7c7f8bbbf8
Ardt Klapwijk <ardt.klapwijk@gmail.com>
2023-10-05 15:03:00 +0200

    feature: create enum for Mechanism

--- vrtool/common/enums.py
+++ vrtool/common/enums.py
@@ -2,23 +2,33 @@ from enum import Enum
 from re import sub
 
 
+def _normalize_name(in_name) -> str:
+    """Convert string to match naming convention (upper with _)"""
+    return sub(r"(?<!^)(?=[A-Z])", "_", in_name).upper()
+
+
 class MechanismEnum(Enum):
     OVERFLOW = 1
     STABILITY_INNER = 2
     PIPING = 3
     REVETMENT = 4
 
+    def __str__(self) -> str:
+        return self.name
+
     @classmethod
     def get_enum(cls, enum_name: str) -> None | Enum:
         """Return matching enum for name"""
 
-        def _convert_name(in_name) -> str:
-            """Convert string to match naming convention (upper with _)"""
-            return sub(r"(?<!^)(?=[A-Z])", "_", in_name).upper()
+        if enum_name.isupper():
+            try:
+                return cls[enum_name]
+            except KeyError:
+                return None
 
         try:
             return next(
-                _enum for _enum in list(cls) if _convert_name(enum_name) == _enum.name
+                _enum for _enum in list(cls) if _normalize_name(enum_name) == _enum.name
             )
         except StopIteration:
             return None
