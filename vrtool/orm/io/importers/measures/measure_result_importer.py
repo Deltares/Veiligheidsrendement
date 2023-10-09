e1af4b64f298d18728a1a6f9702e03b8e923e58a
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-10-09 10:28:34 +0200

    chore: added in MMeasureResultImporter a static method to import the reliability as a DataFrame

--- vrtool/orm/io/importers/measures/measure_result_importer.py
+++ vrtool/orm/io/importers/measures/measure_result_importer.py
@@ -1,5 +1,6 @@
 from vrtool.flood_defence_system.section_reliability import SectionReliability
 from vrtool.orm.io.importers.orm_importer_protocol import OrmImporterProtocol
+from vrtool.orm.models.measure_result.measure_result import MeasureResult
 from vrtool.orm.models.measure_result.measure_result_mechanism import (
     MeasureResultMechanism,
 )
@@ -11,12 +12,21 @@ from vrtool.orm.models.orm_base_model import OrmBaseModel
 
 
 class MeasureResultImporter(OrmImporterProtocol):
-    def import_orm(self, measure_result: OrmBaseModel) -> dict:
-        _imported_parameters = dict(
-            (mrp.name.lower(), mrp.value)
-            for mrp in measure_result.measure_result_parameters
-        )
-        _cost = float("nan")
+    @staticmethod
+    def import_measure_reliability_df(measure_result: MeasureResult) -> pd.DataFrame:
+        """
+        Imports all the reliability values of a given `MeasureResult` into a
+        `pd.DataFrame`. Said `pd.DataFrame` will have its columns representing
+        the available `time` of a `MeasureResult`, the rows as the different
+        `Mechanism`, as well as `SectionData` and the values being their resulting
+         `beta`.
+
+        Args:
+            measure_result (MeasureResult): _description_
+
+        Returns:
+            pd.DataFrame: _description_
+        """
         _columns = []
         _section_reliability_dict = defaultdict(list)
         for _smr in measure_result.sections_measure_result.order_by(
@@ -24,7 +34,6 @@ class MeasureResultImporter(OrmImporterProtocol):
         ):
             _columns.append(_smr.time)
             _section_reliability_dict["Section"].append(_smr.beta)
-            _cost = _smr.cost
             for _mrm in measure_result.measure_result_mechanisms.where(
                 MeasureResultMechanism.time == _smr.time
             ):
@@ -32,14 +41,27 @@ class MeasureResultImporter(OrmImporterProtocol):
                     _mrm.mechanism_per_section.mechanism.name
                 ].append(_mrm.beta)
 
-        # Set attributes.
-        _section_reliability_df = pd.DataFrame.from_dict(
+        return pd.DataFrame.from_dict(
             _section_reliability_dict, columns=_columns, orient="index"
         )
 
+    def import_orm(self, measure_result: OrmBaseModel) -> dict:
+        _cost = float("nan")
+        if any(measure_result.sections_measure_result):
+            # The measure cost has the same value regardless of the time.
+            _cost = measure_result.section_measure_result[0].cost
         _section_reliability = SectionReliability()
-        _section_reliability.SectionReliability = _section_reliability_df
+        _section_reliability.SectionReliability = self.import_measure_reliability_df(
+            measure_result
+        )
 
+        # Get measure parameters (dberm, dcrest, target_beta, transition_level, ...).
+        _imported_parameters = dict(
+            (mrp.name.lower(), mrp.value)
+            for mrp in measure_result.measure_result_parameters
+        )
+
+        # Set attributes.
         return dict(
             measure_result_id=measure_result.get_id(),
             measure_id=measure_result.measure_per_section.measure.get_id(),
