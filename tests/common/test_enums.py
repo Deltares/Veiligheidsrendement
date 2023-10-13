264ac72ea1a35188971cfd28733449d250897fd4
Ardt Klapwijk <ardt.klapwijk@gmail.com>
2023-10-13 10:27:53 +0200

    feature: extend enum with tests

--- tests/common/test_enums.py
+++ tests/common/test_enums.py
@@ -1,9 +1,27 @@
 import pytest
 
-from vrtool.common.enums import MechanismEnum
+from vrtool.common.enums import MeasureTypeEnum, MechanismEnum
 
 
-class TestVrtoolEnums:
+class TestVrtoolEnum:
+    @pytest.mark.parametrize(
+        "enum_name",
+        [
+            pytest.param("Soil reinforcement", id="VALID With space"),
+            pytest.param("SOIL_REINFORCEMENT", id="VALID UPPER_SNAKE"),
+            pytest.param(" Soil reinforcement", id="VALID space before"),
+            pytest.param("Soil reinforcement ", id="VALID space after"),
+        ],
+    )
+    def test_get_valid_measure_type_enum(self, enum_name: str):
+        # 1. Setup
+
+        # 2. Call
+        _measure_type = MeasureTypeEnum.get_enum(enum_name)
+
+        # 3. Assert
+        assert _measure_type.name == "SOIL_REINFORCEMENT"
+
     @pytest.mark.parametrize(
         "enum_name",
         [
@@ -11,33 +29,66 @@ class TestVrtoolEnums:
             pytest.param("STABILITY_INNER", id="VALID UPPER_SNAKE"),
             pytest.param("stability_inner", id="VALID lower_snake"),
             pytest.param(" StabilityInner", id="VALID space before"),
-            pytest.param("stability inner", id="VALID space within"),
             pytest.param("StabilityInner ", id="VALID space after"),
         ],
     )
-    def test_get_valid_enum(self, enum_name: str):
+    def test_get_valid_mechanism_enum(self, enum_name: str):
         # 1. Setup
 
         # 2. Call
-        _mech = MechanismEnum.get_enum(enum_name)
+        _mechanism = MechanismEnum.get_enum(enum_name)
 
         # 3. Assert
-        assert _mech.name == "STABILITY_INNER"
+        assert _mechanism.name == "STABILITY_INNER"
+
+    @pytest.mark.parametrize(
+        "enum_name",
+        [
+            pytest.param("Soilreinforcement", id="INVALID Withoutspace"),
+            pytest.param("SoilReinforcement", id="INVALID CamelCase"),
+        ],
+    )
+    def test_get_invalid_measure_type_enum(self, enum_name: str):
+        # 1. Setup
+
+        # 2. Call
+        _measure_type = MeasureTypeEnum.get_enum(enum_name)
+
+        # 3. Assert
+        assert _measure_type.name == "INVALID"
 
     @pytest.mark.parametrize(
         "enum_name",
         [
             pytest.param("stabilityinner", id="INVALID camelcase"),
+            pytest.param("Stability Inner", id="INVALID space within"),
         ],
     )
-    def test_get_invalid_enum(self, enum_name: str):
+    def test_get_invalid_mechanism_enum(self, enum_name: str):
         # 1. Setup
 
         # 2. Call
-        _mech = MechanismEnum.get_enum(enum_name)
+        _mechanism = MechanismEnum.get_enum(enum_name)
 
         # 3. Assert
-        assert _mech.name == "INVALID"
+        assert _mechanism.name == "INVALID"
+
+    @pytest.mark.parametrize(
+        "enum_name, expected",
+        [
+            pytest.param("SOIL_REINFORCEMENT", "Soil reinforcement", id="VALID SOIL"),
+            pytest.param("STABILITY_SCREEN", "Stability Screen", id="VALID STABILITY"),
+        ],
+    )
+    def test_get_valid_old_measure_tpe_name(self, enum_name: str, expected: str):
+        # 1. Setup
+        _measure_type = MeasureTypeEnum.get_enum(enum_name)
+
+        # 2. Call
+        _measure_type_name = _measure_type.get_old_name()
+
+        # 3. Assert
+        assert _measure_type_name == expected
 
     @pytest.mark.parametrize(
         "enum_name, expected",
@@ -46,7 +97,7 @@ class TestVrtoolEnums:
             pytest.param("STABILITY_INNER", "StabilityInner", id="VALID UPPER_SNAKE"),
         ],
     )
-    def test_get_valid_old_name(self, enum_name: str, expected: str):
+    def test_get_valid_old_mechanism_name(self, enum_name: str, expected: str):
         # 1. Setup
         _mechanism = MechanismEnum.get_enum(enum_name)
 
--- vrtool/common/enums.py
+++ vrtool/common/enums.py
@@ -6,17 +6,71 @@ class VrtoolEnum(Enum):
     def __str__(self) -> str:
         return self.name
 
+
+class MeasureTypeEnum(VrtoolEnum):
+    SOIL_REINFORCEMENT = 1
+    SOIL_REINFORCEMENT_WITH_STABILITY_SCREEN = 2
+    STABILITY_SCREEN = 3
+    VERTICAL_GEOTEXTILE = 4
+    DIAPHRAGM_WALL = 5
+    REVETMENT = 6
+    CUSTOM = 7
+    INVALID = 99
+
     # TODO: delete this method after rationalizing the testdata (VRTOOL-296)
     def get_old_name(self) -> str:
-        """Get name according to old naming convention (CamelCase)"""
-        return "".join(x.lower().capitalize() or "_" for x in self.name.split("_"))
+        """Get name according to old naming convention"""
+        if self.name.find("REINFORCEMENT") > 0:  # Space separated
+            return self.name.lower().replace("_", " ").capitalize()
+        else:  # Space Separated
+            return self.name.lower().replace("_", " ").title()
 
     @classmethod
     def get_enum(cls, enum_name: str) -> Enum:
         """Return matching enum for name"""
 
         def _normalize_name(in_name: str) -> str:
-            """Convert string to match naming convention (upper snake)"""
+            """Convert string to match naming convention (UPPER SNAKE)"""
+            if not in_name:
+                return cls.INVALID.name
+            return in_name.strip().replace(" ", "_").upper()
+
+        try:
+            # Default: enum name exists
+            return cls[enum_name]
+        except KeyError:
+            # Fallback:
+            # -> enum name needs to be normalized first
+            # -> if still no match: INVALID is returned
+            return next(
+                (
+                    _enum
+                    for _enum in list(cls)
+                    if _normalize_name(enum_name) == _enum.name
+                ),
+                cls.INVALID,
+            )
+
+
+class MechanismEnum(VrtoolEnum):
+    OVERFLOW = 1
+    STABILITY_INNER = 2
+    PIPING = 3
+    REVETMENT = 4
+    HYDRAULIC_STRUCTURES = 5
+    INVALID = 99
+
+    # TODO: delete this method after rationalizing the testdata (VRTOOL-296)
+    def get_old_name(self) -> str:
+        """Get name according to old naming convention (CamelCase)"""
+        return self.name.lower().title().replace("_", "")
+
+    @classmethod
+    def get_enum(cls, enum_name: str) -> Enum:
+        """Return matching enum for name"""
+
+        def _normalize_name(in_name: str) -> str:
+            """Convert string to match naming convention (UPPER SNAKE)"""
             if not in_name:
                 return cls.INVALID.name
             return sub(
@@ -38,23 +92,3 @@ class VrtoolEnum(Enum):
                 ),
                 cls.INVALID,
             )
-
-
-class MeasureTypeEnum(VrtoolEnum):
-    SOIL_REINFORCEMENT = 1
-    SOIL_REINFORCEMENT_WITH_STABILITY_SCREEN = 2
-    STABILITY_SCREEN = 3
-    VERTICAL_GEOTEXTILE = 4
-    DIAPHRAGM_WALL = 5
-    REVETMENT = 6
-    CUSTOM = 7
-    INVALID = 99
-
-
-class MechanismEnum(VrtoolEnum):
-    OVERFLOW = 1
-    STABILITY_INNER = 2
-    PIPING = 3
-    REVETMENT = 4
-    HYDRAULIC_STRUCTURES = 5
-    INVALID = 99
