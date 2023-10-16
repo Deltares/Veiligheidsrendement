f332e0a4beff78f96428eb66efada83029116165
Edwin Spee <edwin.spee@deltares.nl>
2023-10-16 14:15:39 +0200

    fix test test_init_with_valid_data

--- tests/run_workflows/optimization_workflow/test_run_optimization.py
+++ tests/run_workflows/optimization_workflow/test_run_optimization.py
@@ -12,7 +12,8 @@ class TestRunOptimization:
         _results_measures.vr_config = "sth"
         _results_measures.solutions_dict = 123
         _results_measures.selected_traject = 456
-        _results_measures.ids_to_import = [1]
+        _results_measures.ids_to_import = [[1,20]]
+        _results_measures._optimization_selected_measure_ids = [1]
 
         # 2. Run test.
         _run = RunOptimization(_results_measures)
