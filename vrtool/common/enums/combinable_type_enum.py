b72c295bc765749bc662a05848dd2c4ee2bd7677
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-06 16:00:05 +0100

    chore: add get_old_name overwrite

--- vrtool/common/enums/combinable_type_enum.py
+++ vrtool/common/enums/combinable_type_enum.py
@@ -7,3 +7,8 @@ class CombinableTypeEnum(VrtoolEnum):
     PARTIAL = 3
     REVETMENT = 4
     INVALID = 99
+
+    # TODO: delete this method after rationalizing the testdata (VRTOOL-296)
+    def get_old_name(self) -> str:
+        """Get name according to old naming convention"""
+        return self.name.lower()
