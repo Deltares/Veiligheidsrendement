a70bf40a7be363e6ebc794790c7887955d5adc8d
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-06 15:06:03 +0100

    chore: add combinable_type enum

--- vrtool/common/enums/__init__.py
+++ vrtool/common/enums/__init__.py
@@ -1,6 +1,7 @@
+from vrtool.common.enums.combinable_type_enum import CombinableTypeEnum
 from vrtool.common.enums.measure_type_enum import MeasureTypeEnum
 from vrtool.common.enums.mechanism_enum import MechanismEnum
 
 
 def get_enums() -> list:
-    return [MeasureTypeEnum, MechanismEnum]
+    return [CombinableTypeEnum, MeasureTypeEnum, MechanismEnum]
---
+++ vrtool/common/enums/combinable_type_enum.py
@@ -0,0 +1,9 @@
+from vrtool.common.enums.vrtool_enum import VrtoolEnum
+
+
+class CombinableTypeEnum(VrtoolEnum):
+    FULL = 1
+    COMBINABLE = 2
+    PARTIAL = 3
+    REVETMENT = 4
+    INVALID = 99
