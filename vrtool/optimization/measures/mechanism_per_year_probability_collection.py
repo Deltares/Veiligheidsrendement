9d120de3075ef44d9b1a9d5178bb0a3b6afeda00
Edwin Spee <edwin.spee@deltares.nl>
2024-02-07 13:55:02 +0100

    implement interpolation of the probabilities for a given set of years

--- vrtool/optimization/measures/mechanism_per_year_probability_collection.py
+++ vrtool/optimization/measures/mechanism_per_year_probability_collection.py
@@ -1,3 +1,5 @@
+from scipy.interpolate import interp1d
+
 from vrtool.common.enums.mechanism_enum import MechanismEnum
 from vrtool.optimization.measures.mechanism_per_year import MechanismPerYear
 
@@ -13,3 +15,28 @@ class MechanismPerYearProbabilityCollection:
             if p.mechanism == mechanism and p.year == year:
                 return p.probability
         raise ValueError("mechanism/year not found")
+    
+    def _get_mechanisms(self) -> {MechanismEnum}:
+        myset = set()
+        for p in self._probabilities:
+            myset.add(p.mechanism)
+        return myset
+    
+    def _add_year_mechanism(self, mechanism: MechanismEnum, added_years: list[int]) -> None:
+        years = []
+        probs = []
+        for p in self._probabilities:
+            if p.mechanism == mechanism:
+                years.append(p.year)
+                probs.append(p.probability)
+
+        prob_interp = interp1d(years, probs, fill_value=("extrapolate"))(added_years)
+        for i in range(len(added_years)):
+            mechPerYr = MechanismPerYear(mechanism, added_years[i], float(prob_interp[i]))
+            self._probabilities.append(mechPerYr)
+
+    def add_years(self, years: list[int]) -> None:
+        mechanisms = self._get_mechanisms()
+        for m in mechanisms:
+            self._add_year_mechanism(m, years)
+
