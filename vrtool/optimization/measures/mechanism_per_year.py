fc4d24dfc712055d253ec459e7c54af30c37c693
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-07 19:29:06 +0100

    chore: add beta

--- vrtool/optimization/measures/mechanism_per_year.py
+++ vrtool/optimization/measures/mechanism_per_year.py
@@ -2,8 +2,10 @@ from dataclasses import dataclass
 
 from vrtool.common.enums.mechanism_enum import MechanismEnum
 
+
 @dataclass
 class MechanismPerYear:
     mechanism: MechanismEnum
     year: int
     probability: float
+    beta: float
