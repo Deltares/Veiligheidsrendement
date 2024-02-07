97eebc96c4ea4d79a49d411831864cca2d4ef4d8
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-07 15:48:21 +0100

    chore: reformatted imports

--- vrtool/optimization/measures/measure_as_input_protocol.py
+++ vrtool/optimization/measures/measure_as_input_protocol.py
@@ -1,8 +1,11 @@
 from typing import Protocol, runtime_checkable
 
-from mechanism_per_year_probability_collection import MechanismPerYearProbabilityCollection
-from vrtool.common.enums.measure_type_enum import MeasureTypeEnum
 from vrtool.common.enums.combinable_type_enum import CombinableTypeEnum
+from vrtool.common.enums.measure_type_enum import MeasureTypeEnum
+from vrtool.optimization.measures.mechanism_per_year_probability_collection import (
+    MechanismPerYearProbabilityCollection,
+)
+
 
 @runtime_checkable
 class MeasureAsInputProtocol(Protocol):
