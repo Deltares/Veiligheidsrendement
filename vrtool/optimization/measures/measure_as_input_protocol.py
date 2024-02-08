4f2e0d43baba04b861d8ed1a217ecdd2c788f6aa
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-08 11:52:49 +0100

    chore: improve mechanism check

--- vrtool/optimization/measures/measure_as_input_protocol.py
+++ vrtool/optimization/measures/measure_as_input_protocol.py
@@ -2,6 +2,7 @@ from typing import Protocol, runtime_checkable
 
 from vrtool.common.enums.combinable_type_enum import CombinableTypeEnum
 from vrtool.common.enums.measure_type_enum import MeasureTypeEnum
+from vrtool.common.enums.mechanism_enum import MechanismEnum
 from vrtool.optimization.measures.mechanism_per_year_probability_collection import (
     MechanismPerYearProbabilityCollection,
 )
@@ -17,3 +18,11 @@ class MeasureAsInputProtocol(Protocol):
     year: int
     lcc: float
     mechanism_year_collection: MechanismPerYearProbabilityCollection
+
+    @classmethod
+    def is_mechanism_allowed(cls, mechanism: MechanismEnum) -> bool:
+        pass
+
+    @classmethod
+    def get_allowed_mechanisms(cls) -> list[MechanismEnum]:
+        pass
