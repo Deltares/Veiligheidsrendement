fix: Corrected logic for creation of input object and beta calculation

vrtool/failure_mechanisms/stability_inner/stability_inner_simple_input.py
@@ -13,18 +13,29 @@ from vrtool.failure_mechanisms.stability_inner.reliability_calculation_method im
 
 @dataclass
 class StabilityInnerSimpleInput:
+    # TODO: VRTOOL-340. This does not support multiple scenarios.
+    # We can remove everything from 2025 / 2075 (discussed with PO)
+
     safety_factor_2025: np.ndarray
     safety_factor_2075: np.ndarray
 
     beta_2025: np.ndarray
     beta_2075: np.ndarray
-    beta: float
+
+    beta: np.ndarray
+    scenario_probability: np.ndarray
+    probability_of_failure: np.ndarray
 
     failure_probability_with_elimination: np.ndarray
     failure_probability_elimination: np.ndarray
 
     is_eliminated: bool
     reliability_calculation_method: ReliabilityCalculationMethod
+
+    def get_failure_probability_from_scenarios(self) -> float:
+        return np.sum(
+            np.multiply(self.probability_of_failure, self.scenario_probability)
+        )
 
     @classmethod
     def from_mechanism_input(
@@ -62,11 +73,11 @@ class StabilityInnerSimpleInput:
             _beta = None
 
             _reliability_calculation_method = ReliabilityCalculationMethod.BETA_RANGE
-        elif _beta:
+        elif isinstance(_beta, np.ndarray) or _beta:
             _safety_factor_2025 = None
             _beta_2025 = None
-            if isinstance(_beta, np.ndarray):
-                _beta = _beta[0]
+            # if isinstance(_beta, np.ndarray):
+            #     _beta = _beta[0]
             _reliability_calculation_method = ReliabilityCalculationMethod.BETA_SINGLE
         else:
             raise Exception("Warning: No input values SF or Beta StabilityInner")
@@ -89,6 +100,10 @@ class StabilityInnerSimpleInput:
             beta_2025=_beta_2025,
             beta_2075=_beta_2075,
             beta=_beta,
+            scenario_probability=mechanism_input.input.get(
+                "P_scenario", np.ndarray([])
+            ),
+            probability_of_failure=mechanism_input.input.get("Pf", np.ndarray([])),
             reliability_calculation_method=_reliability_calculation_method,
             failure_probability_with_elimination=_failure_probability_with_elimination,
             failure_probability_elimination=_failure_probability_elimination,

vrtool/orm/io/importers/piping_importer.py
@@ -43,9 +43,9 @@ class PipingImporter(OrmImporterProtocol):
 
         index = 0
 
-        computation_scenarios: list[
-            ComputationScenario
-        ] = orm_model.computation_scenarios.select()
+        computation_scenarios: list[ComputationScenario] = (
+            orm_model.computation_scenarios.select()
+        )
         nr_of_scenarios = len(computation_scenarios)
         scenario_probablity_key = "P_scenario"
         mechanism_input.input[scenario_probablity_key] = np.zeros(nr_of_scenarios)
@@ -63,6 +63,7 @@ class PipingImporter(OrmImporterProtocol):
             mechanism_input.input[scenario_probablity_key][
                 index
             ] = _c_scenario.scenario_probability
+            # TODO: VRTOOL-340. This does not support multiple scenarios.
             mechanism_input.input["Pf"] = _c_scenario.probability_of_failure
             index += 1
 

vrtool/orm/io/importers/stability_inner_simple_importer.py
@@ -30,10 +30,30 @@ class StabilityInnerSimpleImporter(OrmImporterProtocol):
             )
 
         mechanism_input = MechanismInput(MechanismEnum.STABILITY_INNER)
-        for _computation_scenario in orm_model.computation_scenarios:
+        _scenario_key = "Scenario"
+        _scenario_probablity_key = "P_scenario"
+        _probability_of_failure = "Pf"
+
+        mechanism_input.input[_scenario_key] = []
+        mechanism_input.input[_scenario_probablity_key] = np.array([])
+        mechanism_input.input[_probability_of_failure] = np.array([])
+
+        def _append_to_numpy_input(input_key: str, value: float) -> None:
+            mechanism_input.input[input_key] = np.append(
+                mechanism_input.input[input_key], value
+            )
+
+        for _c_scenario in orm_model.computation_scenarios:
             self._set_parameters(
                 mechanism_input,
-                _computation_scenario.computation_scenario_parameters.select(),
+                _c_scenario.computation_scenario_parameters.select(),
+            )
+            mechanism_input.input[_scenario_key].append(_c_scenario.scenario_name)
+            _append_to_numpy_input(
+                _scenario_probablity_key, _c_scenario.scenario_probability
+            )
+            _append_to_numpy_input(
+                _probability_of_failure, _c_scenario.probability_of_failure
             )
 
         return mechanism_input