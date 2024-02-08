067199d7f77fb0f63db53c16b0627877680d8c87
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-08 12:40:48 +0100

    test: add rudimentary test for input mapping

--- vrtool/optimization/measures/section_as_input.py
+++ vrtool/optimization/measures/section_as_input.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from vrtool.optimization.measures.measure_as_input_protocol import (
     MeasureAsInputProtocol,
 )
+from vrtool.optimization.measures.sg_measure import SgMeasure
+from vrtool.optimization.measures.sh_measure import ShMeasure
 
 
 @dataclass
@@ -10,3 +12,11 @@ class SectionAsInput:
     section_name: str
     traject_name: str
     measures: list[MeasureAsInputProtocol]
+
+    @property
+    def sh_measures(self):
+        return [m for m in self.measures if isinstance(m, ShMeasure)]
+
+    @property
+    def sg_measures(self):
+        return [m for m in self.measures if not isinstance(m, SgMeasure)]
