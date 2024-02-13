e1c2b79f1887788d349aeb7e6a1c76cc77a09d20
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-13 11:00:28 +0100

    chore: correct methods to staticmethod

--- vrtool/optimization/measures/sh_measure.py
+++ vrtool/optimization/measures/sh_measure.py
@@ -23,18 +23,18 @@ class ShMeasure(MeasureAsInputProtocol):
     transition_level: float
     dcrest: float
 
-    @classmethod
-    def is_mechanism_allowed(cls, mechanism: MechanismEnum) -> bool:
-        return mechanism in cls.get_allowed_mechanisms()
+    @staticmethod
+    def is_mechanism_allowed(mechanism: MechanismEnum) -> bool:
+        return mechanism in ShMeasure.get_allowed_mechanisms()
 
-    @classmethod
-    def get_allowed_mechanisms(cls) -> list[MechanismEnum]:
+    @staticmethod
+    def get_allowed_mechanisms() -> list[MechanismEnum]:
         return [MechanismEnum.OVERFLOW, MechanismEnum.REVETMENT]
 
-    @classmethod
-    def get_allowed_measure_combinations(
-        cls,
-    ) -> dict[CombinableTypeEnum, list[CombinableTypeEnum | None]]:
+    @staticmethod
+    def get_allowed_measure_combinations() -> (
+        dict[CombinableTypeEnum, list[CombinableTypeEnum | None]]
+    ):
         return {
             CombinableTypeEnum.REVETMENT: [None],
             CombinableTypeEnum.COMBINABLE: [None, CombinableTypeEnum.REVETMENT],
@@ -44,6 +44,7 @@ class ShMeasure(MeasureAsInputProtocol):
     def __post_init__(self):
         """
         Set LCC to 0 for Sh to avoid double counting with Sg
+        TODO: move this logic to the aggregate function (or add set/get methods for lcc)
         """
         if self.measure_type in [
             MeasureTypeEnum.DIAPHRAGM_WALL,
