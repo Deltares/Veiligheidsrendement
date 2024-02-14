42a83c72e0120326aebccaf3cc7c9bc24d055db0
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-14 15:16:52 +0100

    test: add test for sh/sg measure

--- tests/optimization/measures/test_sg_measure.py
+++ tests/optimization/measures/test_sg_measure.py
@@ -3,34 +3,33 @@ import pytest
 from vrtool.common.enums.combinable_type_enum import CombinableTypeEnum
 from vrtool.common.enums.measure_type_enum import MeasureTypeEnum
 from vrtool.common.enums.mechanism_enum import MechanismEnum
-from vrtool.optimization.measures.sh_measure import ShMeasure
+from vrtool.optimization.measures.sg_measure import SgMeasure
 
 
 class TestShMeasure:
 
-    def _create_sh_measure(
+    def _create_sg_measure(
         self, measure_type: MeasureTypeEnum, combinable_type: CombinableTypeEnum
-    ) -> ShMeasure:
-        _measure = ShMeasure(
+    ) -> SgMeasure:
+        _measure = SgMeasure(
             measure_type=measure_type,
             combine_type=combinable_type,
             cost=10.5,
             year=10,
             mechanism_year_collection=None,
-            beta_target=1.1,
-            transition_level=0.5,
-            dcrest=0.1,
+            dcrest=0.3,
+            dberm=0.1,
+            lcc=20.3,
         )
-        _measure.lcc = 20.3
         return _measure
 
-    def test_create_sh_measure(self):
+    def test_create_sg_measure(self):
         # 1. Define input
         _measure_type = MeasureTypeEnum.SOIL_REINFORCEMENT
         _combine_type = CombinableTypeEnum.COMBINABLE
 
         # 2. Run test
-        _measure = self._create_sh_measure(_measure_type, _combine_type)
+        _measure = self._create_sg_measure(_measure_type, _combine_type)
 
         # 3. Verify expectations
         assert _measure.measure_type == _measure_type
@@ -39,26 +38,13 @@ class TestShMeasure:
         assert _measure.year == 10
         assert _measure.lcc == 20.3
         assert _measure.mechanism_year_collection is None
-        assert _measure.beta_target == 1.1
-        assert _measure.transition_level == 0.5
-        assert _measure.dcrest == 0.1
-
-    def test_sh_measure_with_type_sets_lcc_0(self):
-        # 1. Define input
-        _measure = self._create_sh_measure(
-            MeasureTypeEnum.DIAPHRAGM_WALL, CombinableTypeEnum.FULL
-        )
-
-        # 2. Run test
-        _lcc_after = _measure.lcc
-
-        # 3. Verify expectations
-        assert _lcc_after == 0
+        assert _measure.dcrest == 0.3
+        assert _measure.dberm == 0.1
 
     def test_get_allowed_mechanism(self):
         # 1./2. Define input & Run test
-        _expected_mechanisms = [MechanismEnum.OVERFLOW, MechanismEnum.REVETMENT]
-        _allowed_mechanisms = ShMeasure.get_allowed_mechanisms()
+        _expected_mechanisms = [MechanismEnum.STABILITY_INNER, MechanismEnum.PIPING]
+        _allowed_mechanisms = SgMeasure.get_allowed_mechanisms()
 
         # 3. Verify expectations
         assert all(x == y for x, y in zip(_expected_mechanisms, _allowed_mechanisms))
@@ -66,20 +52,20 @@ class TestShMeasure:
     @pytest.mark.parametrize(
         "mechanism, expected",
         [
-            pytest.param(MechanismEnum.OVERFLOW, True, id="VALID OVERFLOW"),
-            pytest.param(MechanismEnum.PIPING, False, id="INVALID PIPING"),
+            pytest.param(MechanismEnum.PIPING, True, id="VALID PIPING"),
+            pytest.param(MechanismEnum.OVERFLOW, False, id="INVALID OVERFLOW"),
         ],
     )
     def test_is_mechanism_allowed(self, mechanism: MechanismEnum, expected: bool):
         # 1./2. Define input & Run test
-        _results = ShMeasure.is_mechanism_allowed(mechanism)
+        _results = SgMeasure.is_mechanism_allowed(mechanism)
 
         # 3. Verify expectations
         assert _results == expected
 
     def test_get_allowed_measure_combination(self):
         # 1./2. Define input & Run test
-        _allowed_combinations = ShMeasure.get_allowed_measure_combinations()
+        _allowed_combinations = SgMeasure.get_allowed_measure_combinations()
 
         # 3. Verify expectations
         assert isinstance(_allowed_combinations, dict)
