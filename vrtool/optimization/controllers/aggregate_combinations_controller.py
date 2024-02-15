eb9c425609eca975290e1a7991f916a8e9223781
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-15 20:41:53 +0100

    chore: process review comments

--- vrtool/optimization/controllers/aggregate_combinations_controller.py
+++ vrtool/optimization/controllers/aggregate_combinations_controller.py
@@ -11,37 +11,39 @@ class AggregateCombinationsController:
     def __init__(self, section: SectionAsInput) -> None:
         self._section = section
 
-    def aggregate(self) -> list[AggregatedMeasureCombination]:
-        def _create_aggregates(
-            sh_combinations: list[CombinedMeasure],
-            sg_combinations: list[CombinedMeasure],
-        ) -> list[AggregatedMeasureCombination]:
+    def _create_aggregates(
+        self,
+        sh_combinations: list[CombinedMeasure],
+        sg_combinations: list[CombinedMeasure],
+    ) -> list[AggregatedMeasureCombination]:
 
-            def primaries_match(
-                aggregation: tuple[CombinedMeasure, CombinedMeasure]
-            ) -> bool:
-                # Check if the primary measures in both commbinations match
-                _sh_comb, _sg_comb = aggregation
-                return (
-                    _sh_comb.primary.year == _sg_comb.primary.year
-                    and _sh_comb.primary.measure_type == _sg_comb.primary.measure_type
-                )
-
-            def make_aggregate(
-                aggregation: tuple[CombinedMeasure, CombinedMeasure]
-            ) -> AggregatedMeasureCombination:
-                _sh_comb, _sg_comb = aggregation
-                return AggregatedMeasureCombination(
-                    _sh_comb, _sg_comb, _sh_comb.primary.year
-                )
-
-            return list(
-                map(
-                    make_aggregate,
-                    filter(primaries_match, product(sh_combinations, sg_combinations)),
-                )
+        def primaries_match(
+            aggregation: tuple[CombinedMeasure, CombinedMeasure]
+        ) -> bool:
+            # Check if the primary measures in both commbinations match
+            _sh_comb, _sg_comb = aggregation
+            return (
+                _sh_comb.primary.year == _sg_comb.primary.year
+                and _sh_comb.primary.measure_type == _sg_comb.primary.measure_type
             )
 
-        return _create_aggregates(
+        def make_aggregate(
+            aggregation: tuple[CombinedMeasure, CombinedMeasure]
+        ) -> AggregatedMeasureCombination:
+            _sh_comb, _sg_comb = aggregation
+            return AggregatedMeasureCombination(
+                _sh_comb, _sg_comb, _sh_comb.primary.year
+            )
+
+        return list(
+            map(
+                make_aggregate,
+                filter(primaries_match, product(sh_combinations, sg_combinations)),
+            )
+        )
+
+    def aggregate(self) -> list[AggregatedMeasureCombination]:
+
+        return self._create_aggregates(
             self._section.sh_combinations, self._section.sg_combinations
         )
