f163ccdb3f43bc7db6f9938f7b9a012f4b22e88a
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-02-27 08:50:42 +0100

    chore: build options on aggregates instead of combinations

--- vrtool/optimization/strategy_input/strategy_input_target_reliability.py
+++ vrtool/optimization/strategy_input/strategy_input_target_reliability.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
+
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Any
+
 import numpy as np
 import pandas as pd
-from vrtool.optimization.measures.section_as_input import SectionAsInput
 
+from vrtool.optimization.measures.section_as_input import SectionAsInput
 from vrtool.optimization.strategy_input.strategy_input_protocol import (
     StrategyInputProtocol,
 )
@@ -47,31 +49,45 @@ class OldMappingHelper:
         _options_dict: dict[tuple, Any] = defaultdict(list)
         _years = [*range(section.min_year, section.max_year)]
 
-        for i, _comb in enumerate(section.combined_measures):
-            _options_dict[("ID", "")].append(_comb.combined_id)
-            _options_dict[("type", "")].append(_comb.combined_measure_type)
-            _options_dict[("class", "")].append(_comb.measure_class)
-            _options_dict[("year", "")].append(_comb.year)
-            _options_dict[("yes/no", "")].append(_comb.yesno)
-            _options_dict[("dcrest", "")].append(_comb.dcrest)
-            _options_dict[("dberm", "")].append(_comb.dberm)
-            _options_dict[("transition_level", "")].append(_comb.transition_level)
-            _options_dict[("beta_target", "")].append(_comb.beta_target)
+        for i, _comb in enumerate(section.aggregated_measure_combinations):
+            _options_dict[("ID", "")].append(_comb.sg_combination.combined_id)
+            _options_dict[("type", "")].append(
+                _comb.sg_combination.combined_measure_type
+            )
+            _options_dict[("class", "")].append(_comb.sg_combination.measure_class)
+            _options_dict[("year", "")].append(_comb.sg_combination.year)
+            _options_dict[("yes/no", "")].append(_comb.sg_combination.yesno)
+            _options_dict[("dcrest", "")].append(_comb.sh_combination.dcrest)
+            _options_dict[("dberm", "")].append(_comb.sg_combination.dberm)
+            _options_dict[("transition_level", "")].append(
+                _comb.sh_combination.transition_level
+            )
+            _options_dict[("beta_target", "")].append(_comb.sh_combination.beta_target)
             _options_dict[("cost", "")].append(_comb.lcc)
-            _options_dict[("combined_db_index", "")].append(_comb.combined_db_index)
+            _options_dict[("combined_db_index", "")].append(
+                _comb.sg_combination.combined_db_index
+            )
 
             # Get betas for all years
             for _mech in section.mechanisms:
-                _betas = _comb.mechanism_year_collection.get_betas(_mech, _years)
+                _betas = _comb.sh_combination.mechanism_year_collection.get_betas(
+                    _mech, _years
+                )
+                if len(_betas) == 0:
+                    _betas = _comb.sg_combination.mechanism_year_collection.get_betas(
+                        _mech, _years
+                    )
                 for y, _beta in enumerate(_betas):
                     if (_mech.name, _years[y]) not in _options_dict.keys():
                         _options_dict[(_mech.name, _years[y])] = np.zeros(
-                            len(section.combined_measures)
+                            len(section.aggregated_measure_combinations)
                         )
                     _options_dict[(_mech.name, _years[y])][i] = _beta
 
         # Add section for all years
         for _year in _years:
-            _options_dict[("Section", _year)] = np.zeros(len(section.combined_measures))
+            _options_dict[("Section", _year)] = np.zeros(
+                len(section.aggregated_measure_combinations)
+            )
 
         return pd.DataFrame(_options_dict)
