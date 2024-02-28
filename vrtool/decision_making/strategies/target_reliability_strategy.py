ab736c813f001d1f08d5fc0ed728e759c051642a
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-02-28 15:40:52 +0100

    chore: Removed unused dependencies

--- vrtool/decision_making/strategies/target_reliability_strategy.py
+++ vrtool/decision_making/strategies/target_reliability_strategy.py
@@ -1,12 +1,10 @@
 import copy
 import logging
-from typing import Dict
 from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
 from vrtool.common.enums.mechanism_enum import MechanismEnum
-from vrtool.decision_making.solutions import Solutions
 from vrtool.decision_making.strategies.strategy_protocol import StrategyProtocol
 from vrtool.decision_making.strategy_evaluation import (
     calc_tc,
