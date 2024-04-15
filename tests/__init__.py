9464662c5fc0e023f14563c2b908566ade6a959a
Carsopre <Carsopre@users.noreply.github.com>
2024-04-15 11:23:12 +0000

    chore: autoformat isort & black

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,10 @@
 import hashlib
 import shutil
+from pathlib import Path
+
+from pytest import FixtureRequest
 
 from vrtool.defaults.vrtool_config import VrtoolConfig
-from pathlib import Path
-from pytest import FixtureRequest
 
 test_data = Path(__file__).parent / "test_data"
 test_results = Path(__file__).parent / "test_results"
