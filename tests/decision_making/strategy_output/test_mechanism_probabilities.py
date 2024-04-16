6d770770e85358e451430ab9cbea0fc1e281bf9e
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-04-16 12:56:36 +0200

    test: fix assert

--- tests/decision_making/strategy_output/test_mechanism_probabilities.py
+++ tests/decision_making/strategy_output/test_mechanism_probabilities.py
@@ -14,8 +14,8 @@ class TestMechanismProbabilities:
     def test_initialize_from_strategy_input(self):
         # 1. Define test data
         _mech = MechanismEnum.OVERFLOW
-        _prob_section1 = [0.1, 0.2, 0.3, 0.4]
-        _prob_section2 = [0.5, 0.6, 0.7, 0.8]
+        _prob_section1 = np.array([0.1, 0.2, 0.3, 0.4])
+        _prob_section2 = np.array([0.5, 0.6, 0.7, 0.8])
         _section_probabilities = np.array([_prob_section1, _prob_section2])
         _mechanism_probabilities = np.array(
             [_section_probabilities, _section_probabilities]
@@ -32,7 +32,9 @@ class TestMechanismProbabilities:
         assert isinstance(_mp, MechanismProbabilities)
         assert _mp.mechanism == _mech
         assert len(_mp.section_probabilities) == 2
-        assert _mp.section_probabilities[0].probabilities == _prob_section1
+        assert np.array_equal(
+            _mp.section_probabilities[0].probabilities, _prob_section1
+        )
 
     @pytest.mark.parametrize(
         "mechanism",
