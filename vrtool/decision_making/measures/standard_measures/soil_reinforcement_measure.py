6698cfba3f7d7e033fe7ea1d9f3f5441ca433000
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-04-17 15:41:15 +0200

    chore: disabled new code to avoid commiting changes out of scope

--- vrtool/decision_making/measures/standard_measures/soil_reinforcement_measure.py
+++ vrtool/decision_making/measures/standard_measures/soil_reinforcement_measure.py
@@ -4,7 +4,6 @@ import logging
 import numpy as np
 
 from vrtool.common.dike_traject_info import DikeTrajectInfo
-from vrtool.common.enums.direction_enum import DirectionEnum
 from vrtool.common.enums.mechanism_enum import MechanismEnum
 from vrtool.decision_making.measures.common_functions import (
     determine_costs,
@@ -15,10 +14,6 @@ from vrtool.decision_making.measures.common_functions import (
 from vrtool.decision_making.measures.measure_protocol import MeasureProtocol
 from vrtool.decision_making.measures.modified_dike_geometry_measure_input import (
     ModifiedDikeGeometryMeasureInput,
-)
-from vrtool.decision_making.measures.standard_measures.soil_reinforcement_measure_calculator import (
-    MeasureUnitCosts,
-    SoilReinforcementMeasureCalculator,
 )
 from vrtool.flood_defence_system.dike_section import DikeSection
 from vrtool.flood_defence_system.mechanism_reliability_collection import (
@@ -38,10 +33,9 @@ class SoilReinforcementMeasure(MeasureProtocol):
         # To be added: year property to distinguish the same measure in year 2025 and 2045
         # Measure.__init__(self,inputs)
         # self. parameters = measure.parameters
+        _measure_type = self.parameters["Type"]
         if self.parameters["StabilityScreen"] == "yes":
             self.parameters["Depth"] = self._get_depth(dike_section)
-
-        _measure_unit_costs = MeasureUnitCosts.from_unknown_dict(self.unit_costs)
 
         def get_measure_data(
             modified_measure: ModifiedDikeGeometryMeasureInput,
@@ -53,20 +47,37 @@ class SoilReinforcementMeasure(MeasureProtocol):
             _modified_measure["dberm"] = modified_measure.d_berm
             _modified_measure["StabilityScreen"] = self.parameters["StabilityScreen"]
 
-            _modified_measure["Cost"] = SoilReinforcementMeasureCalculator(
-                section_name=dike_section.name,
-                unit_costs=_measure_unit_costs,
-                length=dike_section.Length,
-                depth=self.parameters.get("Depth", float("nan")),
+            # TODO: The following commented code will phase out the current state.
+            # apply during a readability issue please :)
+            # Define the dictionary outside `get_measure_data` to avoid extra running costs.
+            # _measure_unit_costs = MeasureUnitCosts.from_unknown_dict(self.unit_costs)
+            # _modified_measure["Cost"] = SoilReinforcementMeasureCalculator(
+            #     section_name=dike_section.name,
+            #     unit_costs=_measure_unit_costs,
+            #     length=dike_section.Length,
+            #     depth=self.parameters.get("Depth", float("nan")),
+            #     dcrest=modified_measure.d_crest,
+            #     dberm_in=modified_measure.d_house,
+            #     housing=dike_section.houses,
+            #     area_extra=modified_measure.area_extra,
+            #     area_excavated=modified_measure.area_excavated,
+            #     direction=DirectionEnum.get_enum(self.parameters["Direction"]),
+            # ).calculate_total_cost()
+
+            _modified_measure["Cost"] = determine_costs(
+                self.parameters,
+                _measure_type,
+                dike_section.Length,
+                self.parameters.get("Depth", float("nan")),
+                self.unit_costs,
                 dcrest=modified_measure.d_crest,
                 dberm_in=int(modified_measure.d_house),
                 housing=dike_section.houses,
                 area_extra=modified_measure.area_extra,
                 area_excavated=modified_measure.area_excavated,
-                with_stability_screen=_modified_measure["StabilityScreen"].lower().strip() == "yes",
-                direction=DirectionEnum.get_enum(self.parameters["Direction"]),
-            ).calculate_total_cost()
-
+                direction=self.parameters["Direction"],
+                section=dike_section.name,
+            )
             _modified_measure["Reliability"] = self._get_configured_section_reliability(
                 dike_section, traject_info, _modified_measure
             )
