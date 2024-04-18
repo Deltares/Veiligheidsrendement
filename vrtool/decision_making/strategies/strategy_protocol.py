e76b3dfc22ef1b98597e38324945f609fb0c58e7
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-04-18 13:07:12 +0200

    chore: align protocol with strategies

--- vrtool/decision_making/strategies/strategy_protocol.py
+++ vrtool/decision_making/strategies/strategy_protocol.py
@@ -1,11 +1,20 @@
 from typing import Protocol
 
+import numpy as np
 from typing_extensions import runtime_checkable
+
+from vrtool.common.enums.mechanism_enum import MechanismEnum
+from vrtool.optimization.measures.section_as_input import SectionAsInput
 
 
 @runtime_checkable
 class StrategyProtocol(Protocol):
     design_method: str
+    sections: list[SectionAsInput]
+    time_periods: list[int]
+    measures_taken: list[tuple[int, int, int]]
+    total_risk_per_step: list[float]
+    probabilities_per_step: dict[MechanismEnum, np.ndarray]
 
     def evaluate(self, *args, **kwargs):
         """
@@ -13,17 +22,3 @@ class StrategyProtocol(Protocol):
         TODO: For now the arguments are not specific as we do not have a clear view
         on which generic input structures will be used across the different strategies.
         """
-        pass
-
-    def get_total_lcc_and_risk(self, step_number: int) -> tuple[float, float]:
-        """
-        _summary_
-        THIS METHOD WILL BE DEPRECATED
-
-        Args:
-            step_number (int): _description_
-
-        Returns:
-            tuple[float, float]: _description_
-        """
-        pass
