c57f4a92eae0c58b539fc73ec451f8d88a266cf1
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-04-25 10:31:24 +0200

    test: first adjustement unittest

--- tests/decision_making/measures/standard_measures/wall_measures/test_anchored_sheetpile_measure.py
+++ tests/decision_making/measures/standard_measures/wall_measures/test_anchored_sheetpile_measure.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pytest
 
 from vrtool.common.enums.mechanism_enum import MechanismEnum
@@ -13,47 +14,20 @@ from vrtool.flood_defence_system.mechanism_reliability_collection import (
     MechanismReliabilityCollection,
 )
 
-piping_properties_cases = [
-    dict(
-        d_cover=[8.333550637, 4.833550637, 2.833550637],
-        h_exit=[4.0, 4.0, 4.0],
-    ),
-    dict(
-        d_cover=[9.347514637, 5.847514637, 3.847514637],
-        h_exit=[3.012, 3.012, 3.012],
-    ),
-    dict(
-        d_cover=[2.3],
-        h_exit=[6.0],
-    ),
-]
-
 valid_dike_section_cases = [
     dict(
         crest_height=9.98,
         section_length=397,
-        piping_properties=piping_properties_cases[0],
     ),
     dict(
         crest_height=9.93,
         section_length=286,
-        piping_properties=piping_properties_cases[1],
     ),
     dict(
         crest_height=9,
         section_length=397,
-        piping_properties=piping_properties_cases[2],
     ),
 ]
-
-
-def make_case_invalid(dike_section_case: dict) -> dict:
-    _new_dict = dike_section_case | dict()
-    _new_dict["piping_properties"] = dict(invalid_values=[4.2, 2.4, 42])
-    return _new_dict
-
-
-invalid_dike_section_cases = list(map(make_case_invalid, valid_dike_section_cases))
 
 
 class TestAnchoredSheetpileMeasure:
@@ -76,10 +50,11 @@ class TestAnchoredSheetpileMeasure:
 
         _custom_section = CustomDikeSection()
         _custom_section.crest_height = _dike_section_properties["crest_height"]
+        _custom_section.cover_layer_thickness = 7.0
+        _custom_section.InitialGeometry = pd.DataFrame.from_dict(
+            {"BIT": {"z": 3.444}}
+        )  # 3.913 for section 2
         _custom_section.Length = _dike_section_properties["section_length"]
-        _custom_section.piping_properties = _dike_section_properties[
-            "piping_properties"
-        ]
 
         # Create custom MechanismReliabilityCollection
         _custom_reliability_collection = MechanismReliabilityCollection(
@@ -90,91 +65,7 @@ class TestAnchoredSheetpileMeasure:
             measure_year=0,
         )
 
-        # We only need to define year 0
-        _custom_reliability_collection.Reliability[
-            "0"
-        ].Input.input = _custom_section.piping_properties
-        _custom_section.section_reliability.failure_mechanisms.add_failure_mechanism_reliability_collection(
-            _custom_reliability_collection
-        )
-
         return _custom_section
-
-    @pytest.mark.parametrize(
-        "indirect_dike_section",
-        [
-            pytest.param(valid_dike_section_cases[0], id="Section 1"),
-            pytest.param(valid_dike_section_cases[1], id="Section 2"),
-        ],
-        indirect=True,
-    )
-    def test_get_dcover_with_valid_data(self, indirect_dike_section: DikeSection):
-        # 1. Define test data.
-        _measure = AnchoredSheetpileMeasure()
-
-        # 2. Run test.
-        _dcover = _measure._get_dcover(indirect_dike_section)
-
-        # 3. Verify expectations.
-        assert _dcover == max(indirect_dike_section.piping_properties["d_cover"])
-
-    @pytest.mark.parametrize(
-        "indirect_dike_section",
-        [
-            pytest.param(invalid_dike_section_cases[0], id="Section 1 - invalid"),
-            pytest.param(invalid_dike_section_cases[1], id="Section 2 - invalid"),
-        ],
-        indirect=True,
-    )
-    def test_get_dcover_with_invalid_data_returns_default_value(
-        self, indirect_dike_section: DikeSection
-    ):
-        # 1. Define test data.
-        _measure = AnchoredSheetpileMeasure()
-
-        # 2. Run test.
-        _dcover = _measure._get_dcover(indirect_dike_section)
-
-        # 3. Verify expectations.
-        assert _dcover == 1.0
-
-    @pytest.mark.parametrize(
-        "indirect_dike_section",
-        [
-            pytest.param(valid_dike_section_cases[0], id="Section 1"),
-            pytest.param(valid_dike_section_cases[1], id="Section 2"),
-        ],
-        indirect=True,
-    )
-    def test_get_maaiveld_with_valid_data(self, indirect_dike_section: DikeSection):
-        # 1. Define test data.
-        _measure = AnchoredSheetpileMeasure()
-
-        # 2. Run test.
-        _maaiveld = _measure._get_maaiveld(indirect_dike_section)
-
-        # 3. Verify expectations.
-        assert _maaiveld == min(indirect_dike_section.piping_properties["h_exit"])
-
-    @pytest.mark.parametrize(
-        "indirect_dike_section",
-        [
-            pytest.param(invalid_dike_section_cases[0], id="Section 1 - invalid"),
-            pytest.param(invalid_dike_section_cases[1], id="Section 2 - invalid"),
-        ],
-        indirect=True,
-    )
-    def test_get_maaiveld_with_invalid_data_returns_default_value(
-        self, indirect_dike_section: DikeSection
-    ):
-        # 1. Define test data.
-        _measure = AnchoredSheetpileMeasure()
-
-        # 2. Run test.
-        _maaiveld = _measure._get_maaiveld(indirect_dike_section)
-
-        # 3. Verify expectations.
-        assert _maaiveld == indirect_dike_section.crest_height - 3
 
     @pytest.mark.parametrize(
         "indirect_dike_section, expected_cost",
