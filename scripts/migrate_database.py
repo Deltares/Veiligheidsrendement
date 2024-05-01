62e1acc493bdd5a62422ac8f10015c979ac4299f
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-05-01 15:11:47 +0200

    ci: Updated script so that it can be run from command line

--- scripts/migrate_database.py
+++ scripts/migrate_database.py
@@ -2,49 +2,79 @@
 import sqlite3
 from pathlib import Path
 
+import click
 
-def apply_database_migration(db_filepath: str, sql_filepath: str):
+
+@click.group()
+def cli():
+    pass
+
+
+class MigrateDb:
+    def migrate_single_db(self, db_filepath: Path, sql_filepath: Path):
+        """
+        Applies all SQL statements from a migration file to the provided
+        database file.
+
+        Can be run with `python -m migrate_db db_filepath sql_filepath`
+
+        Args:
+            database_file (Path): Database file to migrate (`*.db`)
+            migration_file (Path): Migration file to apply (`*.sql`)
+
+        """
+        # Show which migration will be done, expected format `v0_2_0__to__v0_3_0.sql`
+        def format_version(version_value: str) -> str:
+            return version_value.replace("_", ".")
+
+        _from_version, _to_version = tuple(
+            map(format_version, sql_filepath.stem.split("__to__"))
+        )
+        print(
+            f"Migrating database file [{_from_version} to {_to_version}]: {db_filepath}"
+        )
+
+        # Open and read the file as a single buffer
+        with sqlite3.connect(db_filepath) as _db_connection:
+            _db_connection.executescript(sql_filepath.read_text(encoding="utf-8"))
+
+    def migrate_databases_in_dir(self, database_dir: Path, sql_file: Path):
+        """
+        Migrates all existing databases in the given directory (and subdirectories)
+        with the provided migration file.
+
+        Args:
+            database_dir (Path): Directory containing the databases to migrate.
+            sql_file (Path): SQL file with migration statements.
+        """
+        for _db_to_migrate in database_dir.rglob("*.db"):
+            self.migrate_single_db(_db_to_migrate, sql_file)
+
+
+@cli.command(
+    name="migrate_db",
+    help="Applies all SQL statements from a migration file to the provided database file.",
+)
+@click.argument("db_filepath", type=click.Path(exists=True), nargs=1)
+@click.argument("sql_filepath", type=click.Path(exists=True), nargs=1)
+def migrate_db(db_filepath: str, sql_filepath: str):
     """
-    Applies all SQL statements from a migration file to the provided
-     database file.
-
-    Can be run with `poetry run migrate_db 'db_filepath' 'sql_filepath'
-
-    Args:
-        database_file (str): Database file to migrate (`*.db`)
-        migration_file (str): Migration file to apply (`*.sql`)
-
+    Can be run with `python -m migrate_db db_filepath sql_filepath`
     """
-    _migration_filepath = Path(sql_filepath)
-
-    # Show which migration will be done, expected format `v0_2_0__to__v0_3_0.sql`
-    def format_version(version_value: str) -> str:
-        return version_value.replace("_", ".")
-
-    _from_version, _to_version = tuple(
-        map(format_version, _migration_filepath.stem.split("__to__"))
-    )
-    print(f"Migrating database file [{_from_version} to {_to_version}]: {db_filepath}")
-
-    # Open and read the file as a single buffer
-    with sqlite3.connect(db_filepath) as _db_connection:
-        _db_connection.executescript(_migration_filepath.read_text(encoding="utf-8"))
+    MigrateDb().migrate_single_db(Path(db_filepath), Path(sql_filepath))
 
 
+@cli.command(
+    name="migrate_db_dir",
+    help="Applies all SQL statements from a migration file to the provided database file.",
+)
+@click.argument("database_dir", type=click.Path(exists=True), nargs=1)
+@click.argument("sql_file", type=click.Path(exists=True), nargs=1)
 def migrate_databases_in_dir(database_dir: str, sql_file: str):
     """
-    Migrates all existing databases in the given directory (and subdirectories)
-    with the provided migration file.
-
-    Can be run with `poetry run migrate_db_dir`
-
-    Args:
-        database_dir (str): Directory containing the databases to migrate.
-        sql_file (str): SQL file with migration statements.
+    Can be run with `python -m migrate_db_dir database_dir sql_file`
     """
-    _migration_dirpath = Path(database_dir)
-    for _db_to_migrate in _migration_dirpath.rglob("*.db"):
-        apply_database_migration(str(_db_to_migrate), sql_file)
+    MigrateDb().migrate_databases_in_dir(Path(database_dir), Path(sql_file))
 
 
 def migrate_test_databases():
@@ -64,8 +94,8 @@ def migrate_test_databases():
     assert _tests_dir.exists(), "No tests directory found."
 
     # Apply migration.
-    migrate_databases_in_dir(str(_tests_dir), str(_migration_file))
+    MigrateDb().migrate_databases_in_dir(_tests_dir, _migration_file)
 
 
 if __name__ == "__main__":
-    migrate_test_databases()
+    cli()
