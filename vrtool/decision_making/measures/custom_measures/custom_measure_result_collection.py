8c1745745a8429160b455dedb37484633ccb93cf
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-05-03 16:44:46 +0200

    chore: Added missing logic to return the values of the custom measure result for the pandas dataframe

--- vrtool/decision_making/measures/custom_measures/custom_measure_result_collection.py
+++ vrtool/decision_making/measures/custom_measures/custom_measure_result_collection.py
@@ -8,3 +8,16 @@ from vrtool.decision_making.measures.measure_result_collection_protocol import (
 
 class CustomMeasureResultCollection(MeasureResultCollectionProtocol):
     result_collection: list[CustomMeasureResult]
+
+    def __init__(self) -> None:
+        self.result_collection = []
+
+    def get_measure_output_values(
+        self, split_params: bool, beta_columns: list[str]
+    ) -> tuple[list, list]:
+        return zip(
+            *(
+                _bt.get_measure_output_values(split_params, beta_columns)
+                for _bt in self.result_collection
+            )
+        )
