601c56c14f0b48715e96195b9457181330f377ab
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-05-06 09:38:42 +0200

    chore: Updated code to export results based on the known reliability years

--- vrtool/orm/io/exporters/measures/dict_to_custom_measure_exporter.py
+++ vrtool/orm/io/exporters/measures/dict_to_custom_measure_exporter.py
@@ -64,13 +64,24 @@ class DictListToCustomMeasureExporter(OrmExporterProtocol):
 
         return float(interp1d(_times, _betas, fill_value=("extrapolate"))(year))
 
-    def _combine_custom_mechanism_values_to_section(
-        self,
+    @staticmethod
+    def combine_custom_mechanism_values_to_section(
         mechanism_beta_values: list[float],
     ) -> float:
         """
+        Combines the beta values of different mechanisms  for the same
+        computation time to generate the related  `MeasureResultSection.beta`
+        value.
+
         This method belongs in a "future" dataclass representing the
         CsvCustomMeasure File-Object-Model
+
+        Args:
+            mechanism_beta_values (list[float]):
+                List of `MeasureResultMechanism.beta` values.
+
+        Returns:
+            float: The calculated `MeasureResultSection.beta` value.
         """
 
         def exceedance_probability_swap(value: float) -> float:
@@ -212,43 +223,68 @@ class DictListToCustomMeasureExporter(OrmExporterProtocol):
         """
         _measure_result_mechanism_to_add = []
         _measure_result_section_to_add = []
-        for (
-            _year,
-            _added_cm_mechanism_year_beta,
-        ) in custom_measures_by_year.items():
-            _section_mechanism_betas = []
-            _section_mechanism_costs = []
-            for (
-                _mechanism_per_section
-            ) in measure_per_section.section.mechanisms_per_section:
 
-                (
-                    _mechanism_beta,
-                    _mechanism_cost,
-                ) = self._get_beta_cost_for_custom_measure_section(
-                    _mechanism_per_section, _added_cm_mechanism_year_beta, _year
+        # TODO: This should be the argument being given.
+        _invertedcmb = defaultdict(dict)
+        for _year, _mechanism_measures in custom_measures_by_year.items():
+            for _mechanism, _measures in _mechanism_measures.items():
+                _invertedcmb[_mechanism][_year] = _measures
+
+        _mechanism_result_to_add = []
+        _section_betas = defaultdict(list)
+        _section_cost = 0
+        for (
+            _mechanism_per_section
+        ) in measure_per_section.section.mechanisms_per_section:
+            _beta_per_year_dict = {
+                _amr.time: _amr.beta
+                for _amr in _mechanism_per_section.assessment_mechanism_results
+            }
+            if sorted(_beta_per_year_dict.keys()) != sorted(self._reliability_years):
+                _assessment_years_str = ", ".join(_beta_per_year_dict.keys())
+                _reliability_years_str = ", ".join(self._reliability_years)
+                raise ValueError(
+                    f"Export not possible as the assessment years ({_assessment_years_str}) do not much the provided reliability years ({_reliability_years_str})."
                 )
-                _section_mechanism_betas.append(_mechanism_beta)
-                _section_mechanism_costs.append(_mechanism_cost)
-                _measure_result_mechanism_to_add.append(
+            if _mechanism_per_section.mechanism in _invertedcmb.keys():
+                for _year, _custom_measure in sorted(
+                    _invertedcmb[_mechanism_per_section.mechanism].items()
+                ):
+                    # Cost is  supposed to be the same for all CustomMeasures
+                    # with the same MeasurePerSection
+                    # (only expected change in time)
+                    _section_cost = _custom_measure.cost
+
+                    # Replace the values for years that match.
+                    # Because it's sorted we can simply replace the rest of the values.
+                    for _assessment_year in _beta_per_year_dict.keys():
+                        if _year <= _assessment_year:
+                            _beta_per_year_dict[_assessment_year] = _custom_measure.beta
+
+            # Add values to collection.
+            for _amr_time, _amr_beta in _beta_per_year_dict.items():
+                _section_betas[_amr_time].append(_amr_beta)
+                _mechanism_result_to_add.append(
                     dict(
                         measure_result=measure_result,
                         mechanism_per_section=_mechanism_per_section,
-                        time=_year,
-                        beta=_mechanism_beta,
+                        time=_amr_time,
+                        beta=_amr_beta,
                     )
                 )
-
-            # Add `MeasureResultSection` data.
-            _measure_result_section_to_add.append(
+        _measure_result_section_to_add.extend(
+            [
                 dict(
                     measure_result=measure_result,
                     time=_year,
-                    beta=self._combine_custom_mechanism_values_to_section(
+                    beta=self.combine_custom_mechanism_values_to_section(
                         _section_mechanism_betas
                     ),
                     # Costs should be identical
-                    cost=_section_mechanism_costs[0],
+                    cost=_section_cost,
                 )
-            )
+                for _year, _section_mechanism_betas in _section_betas.items()
+            ]
+        )
+
         return _measure_result_section_to_add, _measure_result_mechanism_to_add
