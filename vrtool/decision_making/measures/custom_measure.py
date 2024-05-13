0f3a76c8be9987a315e897a08997a585eeab8458
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-05-13 10:28:17 +0200

    chore: remove logic from custom measure evaluate

--- vrtool/decision_making/measures/custom_measure.py
+++ vrtool/decision_making/measures/custom_measure.py
@@ -1,96 +1,16 @@
-import copy
-from typing import Any
-
 from vrtool.common.dike_traject_info import DikeTrajectInfo
-from vrtool.common.enums.mechanism_enum import MechanismEnum
 from vrtool.decision_making.measures.measure_protocol import MeasureProtocol
 from vrtool.flood_defence_system.dike_section import DikeSection
-from vrtool.flood_defence_system.mechanism_reliability import MechanismReliability
-from vrtool.flood_defence_system.mechanism_reliability_collection import (
-    MechanismReliabilityCollection,
-)
-from vrtool.flood_defence_system.section_reliability import SectionReliability
 
 
 class CustomMeasure(MeasureProtocol):
-    measures: dict[str, Any]
-
     def evaluate_measure(
         self,
         dike_section: DikeSection,
         traject_info: DikeTrajectInfo,
         preserve_slope: bool = False,
     ):
-        self.measures["Reliability"] = self._get_configured_section_reliability(
-            dike_section, traject_info
-        )
-        self.measures["Reliability"].calculate_section_reliability()
-
-    def _get_configured_section_reliability(
-        self, dike_section: DikeSection, traject_info: DikeTrajectInfo
-    ) -> SectionReliability:
-        section_reliability = SectionReliability()
-
-        mechanisms = (
-            dike_section.section_reliability.failure_mechanisms.get_available_mechanisms()
-        )
-        for mechanism in mechanisms:
-            mechanism_reliability_collection = (
-                self._get_configured_mechanism_reliability_collection(
-                    mechanism, dike_section, traject_info
-                )
-            )
-            section_reliability.failure_mechanisms.add_failure_mechanism_reliability_collection(
-                mechanism_reliability_collection
-            )
-
-        return section_reliability
-
-    def _get_configured_mechanism_reliability_collection(
-        self,
-        mechanism: MechanismEnum,
-        dike_section: DikeSection,
-        traject_info: DikeTrajectInfo,
-    ) -> MechanismReliabilityCollection:
-        mechanism_reliability_collection = MechanismReliabilityCollection(
-            mechanism, "DirectInput", self.config.T, self.config.t_0, 0
-        )
-
-        for year_to_calculate in mechanism_reliability_collection.Reliability.keys():
-            mechanism_reliability_collection.Reliability[
-                year_to_calculate
-            ].Input = copy.deepcopy(
-                dike_section.section_reliability.failure_mechanisms.get_mechanism_reliability_collection(
-                    mechanism
-                )
-                .Reliability[year_to_calculate]
-                .Input
-            )
-
-            # TODO VRTOOL-505: this code doesn't work yet
-            mechanism_reliability = mechanism_reliability_collection.Reliability[
-                year_to_calculate
-            ]
-            dike_section_mechanism_reliability = dike_section.section_reliability.failure_mechanisms.get_mechanism_reliability_collection(
-                mechanism
-            ).Reliability[
-                year_to_calculate
-            ]
-            if True is False:
-                pass  # TODO: use custom measure to influence mechanism reliability
-            else:
-                self._copy_results(
-                    mechanism_reliability, dike_section_mechanism_reliability
-                )  # No influence
-
-        mechanism_reliability_collection.generate_LCR_profile(
-            dike_section.section_reliability.load,
-            traject_info=traject_info,
-        )
-
-        return mechanism_reliability_collection
-
-    def _copy_results(
-        self, target: MechanismReliability, source_input: MechanismReliability
-    ) -> None:
-        target.Input = copy.deepcopy(source_input.Input)
+        """
+        As for a custom measure the measure results are already created during import,
+        this method will not do anything.
+        """
