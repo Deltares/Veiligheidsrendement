7e86d91ac970ffa04c8660f4a5aac5b31277a5c4
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-05-14 22:02:06 +0200

    chore: Modified pragmas

--- vrtool/orm/orm_db.py
+++ vrtool/orm/orm_db.py
@@ -1,3 +1,12 @@
 from peewee import SqliteDatabase
 
-vrtool_db = SqliteDatabase(None, pragmas={"foreign_keys": 1})
+vrtool_db = SqliteDatabase(
+    None,
+    pragmas={
+        "journal_mode": "wal",
+        "cache_size": -1 * 64000,  # 64MB
+        "foreign_keys": 1,
+        "ignore_check_constraints": 0,
+        "synchronous": 0,
+    },
+)
