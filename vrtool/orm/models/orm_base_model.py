5ffdcc6ecad62239fae202b70c091e277f04dcc5
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-05-15 13:07:09 +0200

    docs: Added some missing docstrings

--- vrtool/orm/models/orm_base_model.py
+++ vrtool/orm/models/orm_base_model.py
@@ -2,12 +2,15 @@ from peewee import Model
 
 from vrtool.orm.orm_db import vrtool_db
 
+# The following properties and methods are used accross the whole `orm.models`
+# subproject
 _max_char_length = 128
 
 
 def _get_table_name(qual_name: str) -> str:
     """
-    When invoking the Meta inner class we can access the `__qual__` attribute which contains its parent class with the name to be used as a SQLite table
+    When invoking the Meta inner class we can access the `__qual__` attribute
+    which contains its parent class with the name to be used as a SQLite table
 
     Args:
         qual_name (str): Value of the `__qual__` attribute.
@@ -19,5 +22,14 @@ def _get_table_name(qual_name: str) -> str:
 
 
 class OrmBaseModel(Model):
+    """
+    Base class meant to gather all common definitions which should be shared
+    across all different models of our ORM.
+    """
+
     class Meta:
+        """
+        Define which database will be coupled to all models inherting from this.
+        """
+
         database = vrtool_db
--- vrtool/orm/orm_db.py
+++ vrtool/orm/orm_db.py
@@ -1,5 +1,15 @@
+"""
+The purpose of this module is to make project-wide available our definition of the
+`SqliteDatabase`, this is necessary so that all models in `orm.models` have
+their `Meta.database` property linked to this database, which is currently done
+through inheritance of the `OrmBaseModel`.
+"""
+
 from peewee import SqliteDatabase
 
+# Default values based on official Peewee documentation:
+# http://docs.peewee-orm.com/en/latest/peewee/database.html#using-sqlite
+# http://docs.peewee-orm.com/en/latest/peewee/database.html#recommended-settings
 vrtool_db = SqliteDatabase(
     None,
     pragmas={
