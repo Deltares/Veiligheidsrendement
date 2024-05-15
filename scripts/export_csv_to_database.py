c80bbaa30f75ec09b8e622daf6b7cf0faebfdfb1
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-05-15 16:25:54 +0200

    fix: Fixed mapping of mechanism with legacy name

---
+++ scripts/export_csv_to_database.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import click
+import pandas
+
+from vrtool.orm.io.exporters.measures.list_of_dict_to_custom_measure_exporter import (
+    ListOfDictToCustomMeasureExporter,
+)
+from vrtool.orm.orm_controllers import open_database
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(
+    name="export_csv_to_database",
+    help="Exports a csv with custom measures into a database.",
+)
+@click.argument("database_file", type=click.Path(exists=True), nargs=1)
+@click.argument("csv_file", type=click.Path(exists=True), nargs=1)
+def export_csv_to_database(database_file: str, csv_file: str):
+    #     pass
+
+    # def my_method(database_file, csv_file):
+    """
+    Can be run with `python export_csv_to_database database_file csv_file`
+    """
+    _csv_custom_measures = pandas.read_csv(csv_file, delimiter=";").to_dict("records")
+    _db = open_database(Path(database_file))
+
+    ListOfDictToCustomMeasureExporter(_db).export_dom(_csv_custom_measures)
+
+
+# _files_dir = Path(__file__).parent.joinpath("example_csv_to_db")
+# my_method(
+#     str(_files_dir.joinpath("vrtool_input.db")),
+#     str(_files_dir.joinpath("custom_measures.csv")),
+# )
+if __name__ == "__main__":
+    export_csv_to_database()
