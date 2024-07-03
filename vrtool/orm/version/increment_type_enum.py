9664606145b1dcb19fdd3bd8d7fa18dd1707c292
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-07-03 16:53:40 +0200

    chore: add proper enum inheritance

--- vrtool/orm/version/increment_type_enum.py
+++ vrtool/orm/version/increment_type_enum.py
@@ -1,5 +1,14 @@
-class IncrementTypeEnum(Enum):
+from __future__ import annotations
+
+from vrtool.common.enums.vrtool_enum import VrtoolEnum
+
+
+class IncrementTypeEnum(VrtoolEnum):
     MAJOR = 1
     MINOR = 2
     PATCH = 3
     INVALID = 99
+
+    @classmethod
+    def get_enum(cls, enum_name: str) -> IncrementTypeEnum:
+        return super().get_enum(enum_name)
