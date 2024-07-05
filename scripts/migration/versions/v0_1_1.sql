d12cdffd2e4f6408aa922bc53cef0d9e1c37fa49
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-07-05 10:34:35 +0200

    chore: update script

--- scripts/migration/versions/v0_1_1.sql
+++ scripts/migration/versions/v0_1_1.sql
@@ -4,5 +4,7 @@ CREATE TABLE IF NOT EXISTS Version (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     orm_version VARCHAR(128) NOT NULL
 );
--- Add initial version
-INSERT INTO Version (orm_version) VALUES ('0.1.0');
+-- Add initial version if table is empty
+INSERT INTO Version (orm_version)
+SELECT '0.1.0'
+WHERE NOT EXISTS (SELECT * FROM Version); 
