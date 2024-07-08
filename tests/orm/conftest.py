9e975edb49370c0abd1a56bde8b426d26357e8c5
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-07-08 09:07:41 +0200

    chore: add fixture for persisted empty db path

--- tests/orm/conftest.py
+++ tests/orm/conftest.py
@@ -1,4 +1,5 @@
 import shutil
+from pathlib import Path
 from typing import Callable, Iterator, Optional
 
 import pytest
@@ -23,16 +24,12 @@ from vrtool.orm.models.section_data import SectionData
 from vrtool.orm.orm_controllers import open_database
 
 
-@pytest.fixture(name="persisted_database")
-def get_persisted_database_fixture(
+@pytest.fixture(name="persisted_database_path")
+def get_persisted_database_path_fixture(
     request: pytest.FixtureRequest,
-) -> Iterator[SqliteDatabase]:
+) -> Iterator[Path]:
     """
-    Get's an empty database context with a valid scheme
-    in a directory where it will be persisted after the test
-    finalizes. Allowing to inspect its results.
-    This fixture's database is used when the database needs to be opened
-    and closed during multiple times in a test.
+    Gets the path to a persisted empty database to be used during the test.
     """
     # Create a results directory where to persist the database.
     _output_dir = test_results.joinpath(request.node.name)
@@ -44,6 +41,22 @@ def get_persisted_database_fixture(
     # Copy the original `empty_db.db` into the output directory.
     _db_file = test_data.joinpath("test_db", "empty_db.db")
     shutil.copyfile(_db_file, _test_db_file)
+
+    yield _test_db_file
+
+
+@pytest.fixture(name="persisted_database")
+def get_persisted_database_fixture(
+    request: pytest.FixtureRequest,
+) -> Iterator[SqliteDatabase]:
+    """
+    Gets an empty database context with a valid scheme
+    in a directory where it will be persisted after the test
+    finalizes. weAllowing to inspect its results.
+    This fixture's database is used when the database needs to be opened
+    and closed during multiple times in a test.
+    """
+    _test_db_file = get_persisted_database_path_fixture(request)
 
     # Initialized its context.
     _connected_db = open_database(_test_db_file)
