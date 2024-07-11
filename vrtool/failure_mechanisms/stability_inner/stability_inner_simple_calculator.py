chore: corrected calculation of failure probability when mechanism input `is_eliminated`

vrtool/failure_mechanisms/stability_inner/stability_inner_simple_calculator.py
@@ -78,10 +78,7 @@ class StabilityInnerSimpleCalculator(FailureMechanismCalculatorProtocol):
             # addition: should not be more unsafe
             failure_probability = np.min(
                 [
-                    beta_to_pf(beta)
-                    * self._mechanism_input.failure_probability_elimination
-                    + self._mechanism_input.failure_probability_with_elimination
-                    * (1 - self._mechanism_input.failure_probability_elimination),
+                    beta_to_pf(beta) / self._mechanism_input.piping_reduction_factor,
                     beta_to_pf(beta),
                 ]
             )