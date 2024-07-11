282d8ac50a9976a5c71f2987feea7501eb74d6df
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-07-11 14:14:07 +0200

    chore: Removed unrequired parameters

--- tests/failure_mechanisms/stability_inner/test_stability_inner_simple_input.py
+++ tests/failure_mechanisms/stability_inner/test_stability_inner_simple_input.py
@@ -10,62 +10,6 @@ from vrtool.probabilistic_tools.probabilistic_functions import pf_to_beta
 
 
 class TestStabilityInnerSimpleInput:
-    def test_from_mechanism_input_with_safety_factor_range_returns_input_with_safety_factor_range(
-        self,
-    ):
-        # Setup
-        mechanism_input = MechanismInput("")
-        mechanism_input.input["sf_2025"] = np.array([0.1], dtype=float)
-        mechanism_input.input["sf_2075"] = np.array([0.2], dtype=float)
-
-        # Call
-        failure_mechanism_input = StabilityInnerSimpleInput.from_mechanism_input(
-            mechanism_input
-        )
-
-        # Assert
-        assert (
-            failure_mechanism_input.reliability_calculation_method
-            == ReliabilityCalculationMethod.SAFETYFACTOR_RANGE
-        )
-        assert (
-            failure_mechanism_input.safety_factor_2025
-            == mechanism_input.input["sf_2025"]
-        )
-        assert (
-            failure_mechanism_input.safety_factor_2075
-            == mechanism_input.input["sf_2075"]
-        )
-
-        assert failure_mechanism_input.beta_2025 is None
-        assert failure_mechanism_input.beta_2075 is None
-
-        assert failure_mechanism_input.beta is None
-
-    def test_from_mechanism_input_with_beta_range_returns_input_with_beta_range(self):
-        # Setup
-        mechanism_input = MechanismInput("")
-        mechanism_input.input["beta_2025"] = np.array([0.1], dtype=float)
-        mechanism_input.input["beta_2075"] = np.array([0.2], dtype=float)
-
-        # Call
-        failure_mechanism_input = StabilityInnerSimpleInput.from_mechanism_input(
-            mechanism_input
-        )
-
-        # Assert
-        assert (
-            failure_mechanism_input.reliability_calculation_method
-            == ReliabilityCalculationMethod.BETA_RANGE
-        )
-        assert failure_mechanism_input.beta_2025 == mechanism_input.input["beta_2025"]
-        assert failure_mechanism_input.beta_2075 == mechanism_input.input["beta_2075"]
-
-        assert failure_mechanism_input.safety_factor_2025 is None
-        assert failure_mechanism_input.safety_factor_2075 is None
-
-        assert failure_mechanism_input.beta is None
-
     def test_from_mechanism_input_with_beta_returns_input_with_beta(self):
         # Setup
         mechanism_input = MechanismInput("")
@@ -83,12 +27,6 @@ class TestStabilityInnerSimpleInput:
         )
         assert failure_mechanism_input.beta == mechanism_input.input["beta"]
 
-        assert failure_mechanism_input.beta_2025 is None
-        assert failure_mechanism_input.beta_2075 is None
-
-        assert failure_mechanism_input.safety_factor_2025 is None
-        assert failure_mechanism_input.safety_factor_2075 is None
-
     def test_from_mechanism_input_without_any_reliability_or_safety_factor_raises_exception(
         self,
     ):
@@ -104,41 +42,6 @@ class TestStabilityInnerSimpleInput:
             str(exception_error.value)
             == "Warning: No input values SF or Beta StabilityInner"
         )
-
-    def test_from_mechanism_input_with_all_reliability_and_safety_factor_returns_input_with_safety_factor_range(
-        self,
-    ):
-        # Setup
-        mechanism_input = MechanismInput("")
-        mechanism_input.input["sf_2025"] = np.array([0.1], dtype=float)
-        mechanism_input.input["sf_2075"] = np.array([0.2], dtype=float)
-        mechanism_input.input["beta_2075"] = np.array([0.3], dtype=float)
-        mechanism_input.input["beta_2075"] = np.array([0.4], dtype=float)
-        mechanism_input.input["beta"] = np.array([0.5], dtype=float)
-
-        # Call
-        failure_mechanism_input = StabilityInnerSimpleInput.from_mechanism_input(
-            mechanism_input
-        )
-
-        # Assert
-        assert (
-            failure_mechanism_input.reliability_calculation_method
-            == ReliabilityCalculationMethod.SAFETYFACTOR_RANGE
-        )
-        assert (
-            failure_mechanism_input.safety_factor_2025
-            == mechanism_input.input["sf_2025"]
-        )
-        assert (
-            failure_mechanism_input.safety_factor_2075
-            == mechanism_input.input["sf_2075"]
-        )
-
-        assert failure_mechanism_input.beta_2025 is None
-        assert failure_mechanism_input.beta_2075 is None
-
-        assert failure_mechanism_input.beta is None
 
     def test_from_mechanism_input_with_elimination_returns_input_with_elimination(
         self,
@@ -207,10 +110,6 @@ class TestStabilityInnerSimpleInput:
     ):
         # 1. Define test data.
         _dummy_input = StabilityInnerSimpleInput(
-            safety_factor_2025=np.array([]),
-            safety_factor_2075=np.array([]),
-            beta_2025=np.array([]),
-            beta_2075=np.array([]),
             beta=pf_to_beta(np.array(initial_probability_of_failure)),
             scenario_probability=np.array(scenario_probability),
             initial_probability_of_failure=np.array(initial_probability_of_failure),
--- vrtool/failure_mechanisms/stability_inner/stability_inner_simple_input.py
+++ vrtool/failure_mechanisms/stability_inner/stability_inner_simple_input.py
@@ -16,12 +16,6 @@ class StabilityInnerSimpleInput:
     # TODO: VRTOOL-340. This does not support multiple scenarios.
     # We can remove everything from 2025 / 2075 (discussed with PO)
 
-    safety_factor_2025: np.ndarray
-    safety_factor_2075: np.ndarray
-
-    beta_2025: np.ndarray
-    beta_2075: np.ndarray
-
     beta: np.ndarray
     scenario_probability: np.ndarray
     initial_probability_of_failure: np.ndarray
@@ -67,34 +61,11 @@ class StabilityInnerSimpleInput:
             return input_value.lower() == "yes"
 
         _reliability_calculation_method = None
-        _safety_factor_2075 = None
-        _beta_2075 = None
-
-        _safety_factor_2025 = mechanism_input.input.get("sf_2025", None)
-        _beta_2025 = mechanism_input.input.get("beta_2025", None)
         _beta = mechanism_input.input.get("beta", None)
 
         # If all input is defined, the safety factor takes precedence in which
         # reliability calculation method should be used
-        if _safety_factor_2025:
-            _safety_factor_2075 = mechanism_input.input["sf_2075"]
-
-            _beta_2025 = None
-            _beta = None
-
-            _reliability_calculation_method = (
-                ReliabilityCalculationMethod.SAFETYFACTOR_RANGE
-            )
-        elif _beta_2025:
-            _beta_2075 = mechanism_input.input["beta_2075"]
-
-            _safety_factor_2025 = None
-            _beta = None
-
-            _reliability_calculation_method = ReliabilityCalculationMethod.BETA_RANGE
-        elif isinstance(_beta, np.ndarray) or _beta:
-            _safety_factor_2025 = None
-            _beta_2025 = None
+        if isinstance(_beta, np.ndarray) or _beta:
             _reliability_calculation_method = ReliabilityCalculationMethod.BETA_SINGLE
         else:
             raise Exception("Warning: No input values SF or Beta StabilityInner")
@@ -112,10 +83,6 @@ class StabilityInnerSimpleInput:
             raise ValueError("Warning: Elimination defined but not turned on")
 
         _input = cls(
-            safety_factor_2025=_safety_factor_2025,
-            safety_factor_2075=_safety_factor_2075,
-            beta_2025=_beta_2025,
-            beta_2075=_beta_2075,
             beta=_beta,
             scenario_probability=mechanism_input.input.get(
                 "P_scenario", np.ndarray([])
