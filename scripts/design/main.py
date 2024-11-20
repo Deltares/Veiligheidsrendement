1159ebc71eb1e9a74cb679da2807c1379fd6ef9a
hauth <matthias.hauth@deltares.nl>
2024-11-20 11:13:03 +0100

    fix peewee.IntegrityError: UNIQUE constraint failed: OptimizationRun.name

--- scripts/design/main.py
+++ scripts/design/main.py
@@ -4,8 +4,8 @@ import pandas as pd
 from scripts.design.combine_omega_length_effect import run_single_database
 from scripts.design.modify_inputs import copy_database, modify_beta_measure_database
 from scripts.design.run_vrtool_specific import rerun_database
+from scripts.design.sensitivity import create_csv_runs_summary, read_sensitivity_results_and_plot, compare_lenient_requirement_tables
 from vrtool.defaults.vrtool_config import VrtoolConfig
-
 
 _input_model = Path(
     r"C:\Users\hauth\OneDrive - Stichting Deltares\projects\VRTool\databases\38-1\test")
@@ -13,13 +13,12 @@ assert _input_model.exists()
 _vr_config = VrtoolConfig().from_json(Path(_input_model).joinpath("config.json"))
 _vr_config.input_directory = _input_model
 
-
 res_list = []
 N = 1
 for i in range(1, N+1):
     copy_database(_vr_config, f"modified_beta_{i}")
     modify_beta_measure_database(_vr_config, measure_type="soil reinforcement", mechanism="StabilityInner", std_beta=1.0)
-    rerun_database(_vr_config, rerun_all=False)
+    rerun_database(_vr_config, rerun_all=False, run_name=f"modified_beta_{i}")
     res, vrm_optimization_steps, df_combinations_results, p_max = run_single_database(_vr_config.input_directory.joinpath(_vr_config.input_database_name))
     res_list.append(res)
 
@@ -30,17 +29,13 @@ df = pd.DataFrame(res_list)
 df.to_csv(_input_model.joinpath("results_sensitivity_analysis_38-1.csv"))
 
 
-# modify_cost_measure_database(_vr_config, multiplier=2, measure_type="soil reinforcement")
-# modify_initial_beta_stability(_vr_config, std_beta=1.0)
-# rerun_database(_vr_config, rerun_all=True)
-# run_dsn_lenient_and_stringent(_vr_config, run_strict=False)
+
+# run_single_database(_vr_config.input_directory.joinpath(_vr_config.input_database_name), plot=True)
 
 
-# plot_combined_measure_figure(df_combinations=df_combinations_results,
-#                              vrm_optimization_steps=vrm_optimization_steps,
-#                              vrm_optimum_point=(res["vrm_optimal_point_cost"], res["vrm_optimal_point_pf"]),
-#                              least_expensive_combination=(res["cheapest_combination_cost"], res["cheapest_combination_pf"]),
-#                              dsn_point=(res["dsn_point_cost"], res["dsn_point_pf"]),
-#                              p_max=p_max)
-
-
+# create_csv_runs_summary()
+# read_sensitivity_results_and_plot(Path(
+#     r"C:\Users\hauth\OneDrive - Stichting Deltares\projects\VRTool\databases\38-1\automation\results_sensitivity_analysis_38-1.csv"),
+#     Path(r"C:\Users\hauth\OneDrive - Stichting Deltares\projects\VRTool\databases\38-1\automation\38-1_basis_0.db")
+# )
+# compare_lenient_requirement_tables()
--- scripts/design/run_vrtool_specific.py
+++ scripts/design/run_vrtool_specific.py
@@ -2,28 +2,22 @@ from pathlib import Path
 
 from vrtool.api import ApiRunWorkflows
 from vrtool.defaults.vrtool_config import VrtoolConfig
-from vrtool.orm.models import MeasureResultSection, MeasureResult, MeasurePerSection, Measure
-from vrtool.orm.orm_controllers import get_all_measure_results_with_supported_investment_years, open_database
+from vrtool.orm.orm_controllers import get_all_measure_results_with_supported_investment_years
 from time import time
-import shutil
 
 
-def rerun_database(_vr_config: VrtoolConfig, rerun_all: bool = False):
+def rerun_database(_vr_config: VrtoolConfig, rerun_all: bool = False, run_name: str = None):
     if rerun_all:
         _vr_config.design_methods = ["Veiligheidsrendement", "Doorsnede-eisen"]
-
         _vr_config.T = [0, 19, 20, 25, 50, 75, 100]
         api = ApiRunWorkflows(_vr_config)
         t0 = time()
         api.run_all()
         t1 = time()
-        print("IDS to import done: ", t1 - t0)
+        print("Run done: ", t1 - t0)
         return
 
-    # _vr_config.design_methods = ["Specifieke doorsnede-eisen", "Veiligheidsrendement"]
     _vr_config.design_methods = ["Veiligheidsrendement", "Doorsnede-eisen"]
-
-    # _vr_config.input_directory = path_model
     _vr_config.T = [0, 19, 20, 25, 50, 75, 100]
 
     t0 = time()
@@ -31,13 +25,13 @@ def rerun_database(_vr_config: VrtoolConfig, rerun_all: bool = False):
     _ids_to_import = get_all_measure_results_with_supported_investment_years(_vr_config)
     t1 = time()
     print("IDS to import done: ", t1 - t0)
-
-    # Run lenient DSN
-    # _vr_config.requirements_file = _vr_config.input_directory.joinpath("requirements_lenient.csv")
-    api.run_optimization(optimization_name="Basisberekening_mod",
+    if run_name is None:
+        raise ValueError("Please provide a unique run name")
+    api.run_optimization(optimization_name=run_name,
                          selected_measures_id_year=_ids_to_import)
     t2 = time()
-    print("Lenient DSN done: ", t2 - t1)
+    print("done: ", t2 - t1)
+
 
 def run_dsn_herverdeling_omega(_vr_config: VrtoolConfig, new_database_name: str):
     """
@@ -51,7 +45,7 @@ def run_dsn_herverdeling_omega(_vr_config: VrtoolConfig, new_database_name: str)
 
     """
     _vr_config.design_methods = ["Doorsnede-eisen"]
-    _vr_config.input_directory = _input_model
+    # _vr_config.input_directory = _input_model
     _vr_config.input_database_name = new_database_name
     _vr_config.T = [0, 19, 20, 25, 50, 75, 100]
 
@@ -66,7 +60,7 @@ def run_dsn_herverdeling_omega(_vr_config: VrtoolConfig, new_database_name: str)
 
 def run_dsn_lenient_and_stringent(_vr_config: VrtoolConfig, run_strict: bool = False):
     _vr_config.design_methods = ["Specifieke doorsnede-eisen"]
-    _vr_config.input_directory = _input_model
+    # _vr_config.input_directory = _input_model
     _vr_config.T = [0, 19, 20, 25, 50, 75, 100]
 
     t0 = time()
@@ -75,9 +69,9 @@ def run_dsn_lenient_and_stringent(_vr_config: VrtoolConfig, run_strict: bool = F
     t1 = time()
     print("IDS to import done: ", t1 - t0)
 
-
     # Run lenient DSN
-    _vr_config.requirements_file = Path(_input_model).joinpath("requirements_lenient.csv")
+    # _vr_config.requirements_file = Path(_input_model).joinpath("requirements_lenient.csv")
+    _vr_config.requirements_file = _vr_config.input_directory.joinpath("requirements_lenient.csv")
     api.run_optimization(optimization_name="Basisberekening_lenient",
                          selected_measures_id_year=_ids_to_import)
     t2 = time()
@@ -85,12 +79,12 @@ def run_dsn_lenient_and_stringent(_vr_config: VrtoolConfig, run_strict: bool = F
 
     if not run_strict:
         # Run stringent DSN
-        _vr_config.requirements_file = Path(_input_model).joinpath("requirements_strict.csv")
+        # _vr_config.requirements_file = Path(_input_model).joinpath("requirements_strict.csv")
+        _vr_config.requirements_file = _vr_config.input_directory.joinpath("requirements_strict.csv")
         api.run_optimization(optimization_name="Basisberekening_strict",
                              selected_measures_id_year=_ids_to_import)
         t3 = time()
         print("Stringent DSN done: ", t3 - t2)
-
 
 # _input_model = Path(r"C:\Users\hauth\OneDrive - Stichting Deltares\projects\VRTool\databases\41-1_test_automation\cheap")
 # assert _input_model.exists()
