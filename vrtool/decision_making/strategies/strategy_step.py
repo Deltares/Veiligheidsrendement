3e38050fff7767e37223bf73712c37c4839aae0a
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2025-03-20 12:31:42 +0100

    chore: extend class

--- vrtool/decision_making/strategies/strategy_step.py
+++ vrtool/decision_making/strategies/strategy_step.py
@@ -11,9 +11,12 @@ from vrtool.optimization.measures.aggregated_measures_combination import (
 
 @dataclass(kw_only=True)
 class StrategyStep:
-    step_type: StepTypeEnum = StepTypeEnum.SINGLE
-    measure_taken: tuple[int, int, int] = field(default_factory=lambda: (0,0,0))
-    cost: float = 0.0
-    risk: float = 0.0
+    step_number: int = 0
+    step_type: StepTypeEnum = StepTypeEnum.UNKNOWN
+    measure: tuple[int, int, int] = field(default_factory=lambda: (0,0,0))
+    section_idx: int = 0
+    aggregated_measure: AggregatedMeasureCombination = None
     probabilities: dict[MechanismEnum, np.ndarray] = field(default_factory=dict)
-    selected_aggregated_measure: tuple[int, AggregatedMeasureCombination] = field(default_factory=tuple)
+    bc_ratio: float = 0.0
+    total_risk: float = 0.0
+    total_cost: float = 0.0
