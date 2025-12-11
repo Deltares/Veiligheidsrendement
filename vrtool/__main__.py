f634514c065e217d959d3643bb786f59be9069bf
ArdtK <ardt.klapwijk@deltares.nl>
2025-12-11 13:01:28 +0100

    chore: fix conversion to Path in main

--- vrtool/__main__.py
+++ vrtool/__main__.py
@@ -17,29 +17,29 @@ __externals_path: Path | None = None
 
 @click.group()
 @click.version_option(__version__)
-@click.option("--externals", type=click.Path(), help="Path to externals directory.")
-def cli(externals: click.Path | None):
+@click.option("--externals", type=click.Path(path_type=Path), help="Path to externals directory.")
+def cli(externals: Path | None):
     """
     Set of general available calls for VeiligheidsrendementTool.
     """
     global __externals_path
-    __externals_path = Path(externals) if externals else None
+    __externals_path = externals if externals else None
 
 
-def _initialize_log_file(log_dir: click.Path):
+def _initialize_log_file(log_dir: Path):
     if log_dir is None:
         raise ValueError("Log directory cannot be None.")
 
     # Define logging filename and initialize handler
     _current_date = datetime.today().strftime("%Y%m%d_%H%M")
-    _log_file = Path(log_dir).joinpath(f"vrtool_{_current_date}.log")
+    _log_file = log_dir.joinpath(f"vrtool_{_current_date}.log")
     VrToolLogger.init_file_handler(_log_file, logging_level=logging.INFO)
-    logging.info("Start logging vanuit %s", str(_log_file))
+    logging.info("Start logging vanuit %s", _log_file)
 
 
 def _set_externals_path(vr_config: VrtoolConfig):
-    logging.info("Config externals path: %s.", str(vr_config.externals))
-    logging.info("CLI externals path: %s.", str(__externals_path))
+    logging.info("Config externals path: %s.", vr_config.externals)
+    logging.info("CLI externals path: %s.", __externals_path)
     if vr_config.externals is not None or __externals_path is None:
         logging.info("No externals path set, using config value.")
         return
@@ -50,9 +50,9 @@ def _set_externals_path(vr_config: VrtoolConfig):
 @cli.command(
     name="assessment", help="Assesses the model with the given configuration file."
 )
-@click.argument("config_file", type=click.Path(exists=True), nargs=1)
-@click.option("-ld", "--log-dir", type=click.Path())
-def run_step_assessment(config_file: click.Path, log_dir: click.Path | None):
+@click.argument("config_file", type=click.Path(exists=True, path_type=Path), nargs=1)
+@click.option("-ld", "--log-dir", type=click.Path(path_type=Path))
+def run_step_assessment(config_file: Path, log_dir: Path | None):
     """
     Runs the step assessment.
     """
@@ -61,10 +61,10 @@ def run_step_assessment(config_file: click.Path, log_dir: click.Path | None):
         log_dir = config_file.parent
     _initialize_log_file(log_dir)
 
-    logging.info("Start beoordeling met configuratie %s", str(config_file))
+    logging.info("Start beoordeling met configuratie %s", config_file)
 
     # Get the selected Traject.
-    _vr_config = api.get_valid_vrtool_config(Path(config_file))
+    _vr_config = api.get_valid_vrtool_config(config_file)
     _set_externals_path(_vr_config)
     api.run_step_assessment(_vr_config)
 
@@ -73,12 +73,13 @@ def run_step_assessment(config_file: click.Path, log_dir: click.Path | None):
     name="measures",
     help="Calculates the reliability and cost for all measures with the given configuration file.",
 )
-@click.argument("config_file", type=click.Path(exists=True), nargs=1)
-@click.option("-ld", "--log-dir", type=click.Path())
-def run_step_measures(config_file: click.Path, log_dir: click.Path | None):
+@click.argument("config_file", type=click.Path(exists=True, path_type=Path), nargs=1)
+@click.option("-ld", "--log-dir", type=click.Path(path_type=Path))
+def run_step_measures(config_file: Path, log_dir: Path | None):
     """
     Runs step measures.
     """
+      
     # Retrieve parameter and initialize logging.
     if log_dir is None:
         log_dir = config_file.parent
@@ -86,11 +87,11 @@ def run_step_measures(config_file: click.Path, log_dir: click.Path | None):
 
     logging.info(
         "Start berekenen betrouwbaarheid en kosten maatregelen met configuratie %s",
-        str(config_file),
+        config_file,
     )
 
     # Define VrToolConfig and Selected Traject
-    _vr_config = api.get_valid_vrtool_config(Path(config_file))
+    _vr_config = api.get_valid_vrtool_config(config_file)
     _set_externals_path(_vr_config)
     api.run_step_measures(_vr_config)
 
@@ -99,11 +100,11 @@ def run_step_measures(config_file: click.Path, log_dir: click.Path | None):
     name="optimization",
     help="Optimizes the model measures with the given configuration file.",
 )
-@click.argument("config_file", type=click.Path(exists=True), nargs=1)
+@click.argument("config_file", type=click.Path(exists=True, path_type=Path), nargs=1)
 @click.argument("measure_result_ids", type=click.INT, nargs=-1)
-@click.option("-ld", "--log-dir", type=click.Path())
+@click.option("-ld", "--log-dir", type=click.Path(path_type=Path))
 def run_step_optimization(
-    config_file: click.Path, log_dir: click.Path | None, measure_result_ids: tuple[int]
+    config_file: Path, log_dir: Path | None, measure_result_ids: tuple[int]
 ):
     """
     Runs step optimization.
@@ -113,25 +114,24 @@ def run_step_optimization(
         log_dir = config_file.parent
     _initialize_log_file(log_dir)
 
-    _config_file = Path(config_file)
-    logging.info("Start optimalisatie met configuratie %s", str(config_file))
+    logging.info("Start optimalisatie met configuratie %s", config_file)
 
     # Define VrToolConfig and Selected Traject
-    _vr_config = api.get_valid_vrtool_config(_config_file)
+    _vr_config = api.get_valid_vrtool_config(config_file)
     _set_externals_path(_vr_config)
     _measure_result_tuples = []
     if any(measure_result_ids):
         _iterator = iter(measure_result_ids)
         _measure_result_tuples = list(zip(_iterator, _iterator))
-    api.run_step_optimization(_vr_config, _config_file.parent, _measure_result_tuples)
+    api.run_step_optimization(_vr_config, config_file.parent, _measure_result_tuples)
 
 
 @cli.command(
     name="run_full", help="Full run of the model with the given configuration."
 )
-@click.argument("config_file", type=click.Path(exists=True), nargs=1)
-@click.option("-ld", "--log-dir", type=click.Path())
-def run_full(config_file: click.Path, log_dir: click.Path | None):
+@click.argument("config_file", type=click.Path(exists=True, path_type=Path), nargs=1)
+@click.option("-ld", "--log-dir", type=click.Path(path_type=Path))
+def run_full(config_file: Path, log_dir: Path | None):
     """
     Runs all the veiligheidsrendement steps (assessment, measures and optimization).
     """
@@ -139,10 +139,10 @@ def run_full(config_file: click.Path, log_dir: click.Path | None):
     if log_dir is None:
         log_dir = config_file.parent
     _initialize_log_file(log_dir)
-    logging.info("Start volledige berekening met configuratie %s!", str(config_file))
+    logging.info("Start volledige berekening met configuratie %s!", config_file)
 
     # Define VrToolConfig and Selected Traject
-    _vr_config = api.get_valid_vrtool_config(Path(config_file))
+    _vr_config = api.get_valid_vrtool_config(config_file)
     _set_externals_path(_vr_config)
     api.run_full(_vr_config)
 
@@ -157,7 +157,7 @@ def migrate_db(db_filepath: str):
         db_filepath (str): Database file location to migrate.
     """
     logging.info("Migreren van database %s.", db_filepath)
-    MigrateDatabaseController(default_scripts_dir).migrate_single_db(Path(db_filepath))
+    MigrateDatabaseController(default_scripts_dir).migrate_single_db(db_filepath)
 
 
 @cli.command(
@@ -173,7 +173,7 @@ def migrate_databases_in_dir(database_dir: str):
     """
     logging.info("Migreren van databases in %s.", database_dir)
     MigrateDatabaseController(default_scripts_dir).migrate_databases_in_dir(
-        Path(database_dir)
+        database_dir
     )
 
 
